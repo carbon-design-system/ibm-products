246da66738e890b35fa5da23ea0daff6199a9722
Simon Finney <SIMONFIN@ie.ibm.com>
2020-07-02 14:40:34 +0100

    docs(storybook): add `Decorator` status icons

--- packages/security/src/Decorator/index.js
+++ packages/security/src/Decorator/index.js
@@ -5,6 +5,4 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export { ComboButton, ComboButtonItem } from './ComboButton';
-export { Decorator } from './Decorator';
-export { ErrorPage } from './ErrorPage';
+export { Decorator } from '@carbon/ibm-security';

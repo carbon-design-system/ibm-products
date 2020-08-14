ba22653f0545e53c109ca7fc381996be08151052
Lee Chase <lee.chase@uk.ibm.com>
2020-08-14 12:44:59 +0100

    feat: add experimental package

---
+++ packages/experimental-package/src/components/ExampleComponent/ExampleComponent.stories.js
@@ -0,0 +1,39 @@
+//
+// Copyright IBM Corp. 2020, 2020
+//
+// This source code is licensed under the Apache-2.0 license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+import React from 'react';
+
+import { ExampleComponent } from '.';
+
+import styles from './_index.scss'; // import index in case more files are added later.
+
+export default {
+  title: 'Example/ExampleComponent',
+  component: ExampleComponent,
+  argTypes: {
+    borderColor: { control: 'color' },
+  },
+  parameters: { styles },
+};
+
+const Template = (args) => <ExampleComponent {...args} />;
+
+// const parameters = { styles };
+
+export const BoxedSet = Template.bind({});
+BoxedSet.args = {
+  borderColor: '#141414',
+  boxedBorder: true,
+  disabled: false,
+};
+
+export const ShadowSet = Template.bind({});
+ShadowSet.args = {
+  borderColor: '#141414',
+  boxedBorder: false,
+  disabled: false,
+};

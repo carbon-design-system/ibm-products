fc77cf5b2940dd01dae882774b610a16825cca0e
Lee Chase <lee.chase@uk.ibm.com>
2020-08-15 12:51:01 +0100

    feat: modified tabs first pass

---
+++ packages/experimental-package/src/components/ModifiedTabs/index.js
@@ -0,0 +1,1 @@
+export { ModifiedTabs } from './ModifiedTabs';
--- packages/experimental-package/src/index.js
+++ packages/experimental-package/src/index.js
@@ -5,4 +5,4 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export { ExampleComponent } from './components';
+export { ExampleComponent, ModifiedTabs } from './components';

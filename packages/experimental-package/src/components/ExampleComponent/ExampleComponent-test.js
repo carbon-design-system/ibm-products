448e6ac054ab9d3c414d39c3c8d097f215630788
David Menendez <dmenend@us.ibm.com>
2020-09-15 10:38:11 -0500

    fix: adds additional testing for ExampleComponent

--- packages/experimental-package/src/components/ExampleComponent/ExampleComponent-test.js
+++ packages/experimental-package/src/components/ExampleComponent/ExampleComponent-test.js
@@ -3,7 +3,29 @@ import { shallow } from 'enzyme';
 import { ExampleComponent } from './ExampleComponent';
 
 describe('ExampleComponent', () => {
-  it('renders', () => {
-    shallow(<ExampleComponent />);
+  let wrapper;
+  const primaryClickMock = jest.fn();
+  const secondaryClickMock = jest.fn();
+
+  beforeEach(() => {
+    wrapper = shallow(<ExampleComponent />);
+  });
+
+  it('primary and secondary clicks not called', () => {
+    wrapper.find('Button').at(0).props().onClick();
+    expect(secondaryClickMock).not.toHaveBeenCalled();
+    wrapper.find('Button').at(1).props().onClick();
+    expect(primaryClickMock).not.toHaveBeenCalled();
+  });
+
+  it('primary and secondary clicks called', () => {
+    wrapper.setProps({
+      onPrimaryClick: primaryClickMock,
+      onSecondaryClick: secondaryClickMock,
+    });
+    wrapper.find('Button').at(0).props().onClick();
+    expect(secondaryClickMock).toHaveBeenCalled();
+    wrapper.find('Button').at(1).props().onClick();
+    expect(primaryClickMock).toHaveBeenCalled();
   });
 });

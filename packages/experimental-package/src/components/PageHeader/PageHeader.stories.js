9edb7023ca3b840b9e2fabfe7d4b506ba9b825ce
Lee Chase <lee.chase@uk.ibm.com>
2020-09-24 13:11:20 +0100

    feat: further flesh out page header

--- packages/experimental-package/src/components/PageHeader/PageHeader.stories.js
+++ packages/experimental-package/src/components/PageHeader/PageHeader.stories.js
@@ -12,14 +12,57 @@ import { PageHeader } from '.';
 
 import styles from './_storybook-styles.scss'; // import index in case more files are added later.
 
+import { BreadcrumbItem } from 'carbon-components-react';
+
+import { Lightning20 } from '@carbon/icons-react';
+
+import { ActionBar } from './ActionBar';
+
 export default {
   title: 'Experimental/PageHeader',
   component: PageHeader,
   parameters: { styles, layout: 'fullscreen' },
 };
 
+const breadcrumbItems = [
+  { label: 'Homepage', href: '/' },
+  { label: 'Secondary page', href: '/secondary' },
+];
+
 const Template = (args) => {
-  return <PageHeader className="fish cake" {...args} />;
+  return (
+    <PageHeader
+      className="fish cake"
+      {...args}
+      breadcrumbItems={breadcrumbItems.map((item, index) => {
+        return (
+          <BreadcrumbItem href={item.href} key={`${index}-${item.label}`}>
+            {item.label}
+          </BreadcrumbItem>
+        );
+      })}
+      actionBar={
+        <ActionBar
+          actions={[
+            {
+              description: 'Action 1',
+              icon: Lightning20,
+              handleClick: () => {
+                console.log('click 1');
+              },
+            },
+            {
+              description: 'Action 2',
+              icon: Lightning20,
+              handleClick: () => {
+                console.log('click 2');
+              },
+            },
+          ]}
+        />
+      }
+    />
+  );
 };
 
 export const Minimal = Template.bind({});

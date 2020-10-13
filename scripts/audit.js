444f50b7a2086e4a42c5655c84602c28e847d9c4
vladbalanescu <vlad.balanescu@uk.ibm.com>
2020-10-13 12:14:48 +0100

    Audit script for prod dependencies (#140)
    * Initial thoughts
    
    * Correct level flag for yarn and actually failing the command
    
    * Seggregated audit and test scripts
    
    * Fixed all high vulnerabilities
    
    * Right copyright header in the new file

---
+++ scripts/audit.js
@@ -0,0 +1,51 @@
+//
+// Copyright IBM Corp. 2020, 2020
+//
+// This source code is licensed under the Apache-2.0 license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+'use strict';
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const argv = process.argv.slice(2);
+const group = argv[0];
+const level = argv[1];
+let auditLevel;
+
+async function audit() {
+  auditLevel = level ? ' --level ' + level : '';
+  const auditCommand = 'yarn audit --groups ' + group + auditLevel;
+  console.log('Running audit using:', auditCommand);
+  await exec(auditCommand);
+}
+
+audit().catch((err) => {
+  // Failed to contact endpoint
+  if (err && err.message && err.message.includes('code ENOAUDIT')) {
+    console.log(
+      'audit stage has failed because of an error contacting the yarn audit endpoint, this will not fail the build!'
+    );
+    process.exit(0);
+  }
+
+  // Report any vulnerabilities
+  try {
+    // If any entry in the table, this must be reported
+    if (err.stdout.includes('Prototype Pollution')) {
+      console.log(err.stdout);
+      process.exit(1);
+    } else {
+      // If not table reported, we're good
+      process.exit(0);
+    }
+  } catch (e) {
+    console.log(
+      'audit stage failed due to an invalid error body! Error was: \n',
+      e
+    );
+  }
+
+  process.exit(1);
+});

762060acf708ea6d19d9f48d6520d05ef3e43836
Lee Chase <leechase@live.com>
2020-11-23 16:29:30 +0000

    feat: add show all to tag-set (#222)
    * feat: add show all to tag-set
    
    * chore: remove block class export
    
    Co-authored-by: Lee Chase <lee.chase@uk.ibm.com>

--- packages/experimental/src/components/TagSet/TagSet.js
+++ packages/experimental/src/components/TagSet/TagSet.js
@@ -13,8 +13,7 @@ import cx from 'classnames';
 // import { settings } from 'carbon-components';
 // const { prefix } = settings;
 
-import { Tag, Tooltip } from 'carbon-components-react';
-
+import { Link, Modal, Search, Tag, Tooltip } from 'carbon-components-react';
 import { expPrefix } from '../../global/js/settings';
 
 import ReactResizeDetector from 'react-resize-detector';
@@ -27,16 +26,24 @@ export const TagSet = ({
   maxVisibleTags,
   rightAlign,
   overflowDirection,
+  showAllModalHeading,
+  showAllSearchLabel,
+  showAllSearchPlaceHolderText,
+  showAllTagsLabel,
 }) => {
   const [displayCount, setDisplayCount] = useState(3);
   const [displayedTags, setDisplayedTags] = useState([]);
   const [overflowTags, setOverflowTags] = useState([]);
   const [allTags, setAllTags] = useState([]);
+  const [filteredAllTags, setFilteredAllTags] = useState([]);
   const [tipOpen, setTipOpen] = useState(false);
+  const [showAllModalOpen, setShowAllModalOpen] = useState(false);
+  const [search, setSearch] = useState('');
   const tagSet = useRef(null);
   const displayedArea = useRef(null);
   const sizingTags = useRef([]);
   const overflowTag = useRef(null);
+  const overflowTagContent = useRef(null);
 
   useEffect(() => {
     setAllTags(
@@ -64,19 +71,48 @@ export const TagSet = ({
           </div>
         );
       } else {
-        newOverflowTags.push(
-          <span
-            key={`overflow-tag-${child.key}`}
-            className={`${blockClass}--overflow-tag`}>
-            {React.cloneElement(child)}
-          </span>
-        );
+        if (newOverflowTags.length < 10) {
+          newOverflowTags.push(
+            <span
+              key={`overflow-tag-${child.key}`}
+              className={`${blockClass}--overflow-tag`}>
+              {React.cloneElement(child)}
+            </span>
+          );
+        }
       }
     });
 
     setDisplayedTags(newDisplayedTags);
     setOverflowTags(newOverflowTags);
   }, [children, displayCount]);
+
+  useEffect(() => {
+    if (showAllModalOpen) {
+      const newFilteredAllTags = [];
+      children.forEach((child) => {
+        const dataSearch = child.props['data-search']?.toLocaleLowerCase();
+        const contentsAsString = child.props.children
+          .toString()
+          .toLocaleLowerCase();
+        if (
+          (dataSearch && dataSearch.indexOf(search) > -1) ||
+          contentsAsString.indexOf(search) > -1
+        ) {
+          newFilteredAllTags.push(
+            <span
+              key={`overflow-tag-${child.key}`}
+              className={`${blockClass}-show-all-tags`}>
+              {React.cloneElement(child)}
+            </span>
+          );
+        }
+      });
+      setFilteredAllTags(newFilteredAllTags);
+    } else {
+      setFilteredAllTags([]);
+    }
+  }, [showAllModalOpen, children, search]);
 
   const checkFullyVisibleTags = () => {
     // how many will fit?
@@ -127,6 +163,43 @@ export const TagSet = ({
     checkFullyVisibleTags();
   };
 
+  const handleModalClose = () => {
+    setShowAllModalOpen(false);
+  };
+
+  const handleShowAllTagsClick = (ev) => {
+    ev.stopPropagation();
+    ev.preventDefault();
+    setTipOpen(false);
+    setShowAllModalOpen(true);
+  };
+
+  const handleSearch = (ev) => {
+    setSearch(ev.target.value);
+  };
+
+  const handleClickOutsideCheck = (ev) => {
+    const tooltipEl = overflowTagContent.current?.parentElement?.parentElement;
+    if (
+      tooltipEl !== undefined &&
+      (tooltipEl === ev.target || tooltipEl.contains(ev.target))
+    ) {
+      // inside click
+      return;
+    }
+    hideTip(ev);
+  };
+
+  useEffect(() => {
+    // Check for a click outside of the tooltip
+    document.addEventListener('mousedown', handleClickOutsideCheck);
+    // remove listener on destroy
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutsideCheck);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <ReactResizeDetector onResize={handleResize}>
       <div className={cx([blockClass, className])} ref={tagSet}>
@@ -148,24 +221,51 @@ export const TagSet = ({
           </div>
 
           <span
-            onMouseEnter={showTip}
-            onMouseLeave={hideTip}
-            onFocus={showTip}
-            onBlur={hideTip}
             aria-hidden={overflowTags === 0}
             className={cx(`${blockClass}--overflow`, {
               [`${blockClass}--overflow--hidden`]: overflowTags.length === 0,
-            })}>
+            })}
+            onFocus={showTip}>
             <Tooltip
               direction={overflowDirection}
-              triggerText={<Tag>+{overflowTags.length}</Tag>}
-              showIcon={false}
               open={tipOpen}
+              triggerText={<Tag>+{children.length - displayedTags.length}</Tag>}
+              showIcon={false}
               ref={overflowTag}>
-              {overflowTags}
+              <div
+                ref={overflowTagContent}
+                className={`${blockClass}--overflow-content`}>
+                {overflowTags}
+                {overflowTags.length >= 10 && (
+                  <Link
+                    className={`${blockClass}--show-all-tags-link`}
+                    href=""
+                    onClick={handleShowAllTagsClick}>
+                    {showAllTagsLabel}
+                  </Link>
+                )}
+              </div>
             </Tooltip>
           </span>
         </div>
+        <Modal
+          className={`${blockClass}--show-all-modal`}
+          open={showAllModalOpen}
+          passiveModal
+          size="sm"
+          modalHeading={showAllModalHeading}
+          onRequestClose={handleModalClose}>
+          <Search
+            className={`${blockClass}--show-all-tags-search`}
+            labelText={showAllSearchLabel}
+            placeHolderText={showAllSearchPlaceHolderText}
+            onChange={handleSearch}
+            size="lg"
+          />
+          <div className={`${blockClass}--show-all-tags-content`}>
+            {filteredAllTags}
+          </div>
+        </Modal>
       </div>
     </ReactResizeDetector>
   );
@@ -192,8 +292,30 @@ TagSet.propTypes = {
    * align tags to right of available space
    */
   rightAlign: PropTypes.bool,
+  /**
+   * heading for the show all modal
+   */
+  showAllModalHeading: PropTypes.string,
+  /**
+   * label text for the show all search
+   */
+  showAllSearchLabel: PropTypes.string,
+  /**
+   * placeholder text for the show all search
+   */
+  showAllSearchPlaceHolderText: PropTypes.string,
+  /**
+   * label for the overflow show all tags link
+   */
+  showAllTagsLabel: PropTypes.string,
 };
 
 TagSet.defaultProps = {
   overflowDirection: 'bottom',
+  showAllModalHeading: 'All tags',
+  showAllSearchLabel: 'Search all tags',
+  showAllSearchPlaceHolderText: 'Search all tags',
+  showAllTagsLabel: 'View all tags',
 };
+
+export default TagSet;

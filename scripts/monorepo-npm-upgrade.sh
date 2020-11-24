0073ecd83d28c94a94e996798ae97968c6a48ce6
Lee Chase <lee.chase@uk.ibm.com>
2020-11-24 11:20:30 +0000

    feat: add monorepo-npm-upgradenpm

---
+++ scripts/monorepo-npm-upgrade.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+################################################################################
+#
+# Helper script run npm-upgrade in all packages
+#
+################################################################################
+
+echo Running npm-upgrade for all packages and root
+
+cd ./packages
+for package in * ; do
+    if [ -d "$package" ]; then
+        echo "Upgrade package '$package' (y/n)?"
+        old_stty_cfg=$(stty -g)
+        stty raw -echo
+        mode=$( while ! head -c 1 | grep -i '[yn]' ;do true ;done )
+        stty $old_stty_cfg
+
+        cd $package
+            if echo "$mode" | grep -iq "^y" ;then
+                npm-upgrade
+            fi
+        cd ..
+    fi
+done
+cd ..
+
+echo "Run npm-upgrade for root package.json (y/n)?"
+old_stty_cfg=$(stty -g)
+stty raw -echo
+mode=$( while ! head -c 1 | grep -i '[yn]' ;do true ;done )
+stty $old_stty_cfg
+
+if echo "$mode" | grep -iq "^y" ;then
+    npm-upgrade
+fi

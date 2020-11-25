861abde211a3d32122fc59735b43e34a2ac57ef0
Simon Finney <SIMONFIN@ie.ibm.com>
2020-11-25 10:23:58 +0000

    build(babel): add export plugin

--- packages/babel-preset-ibm-cloud-cognitive/index.js
+++ packages/babel-preset-ibm-cloud-cognitive/index.js
@@ -23,6 +23,7 @@ module.exports = () => ({
   ],
   plugins: [
     '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-export-default-from',
     [
       '@babel/plugin-transform-runtime',
       {

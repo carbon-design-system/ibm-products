35b351776107d23f6dafd30b9d4a24d626a9d9ec
Simon Finney <simonjfinney@gmail.com>
2021-01-29 10:30:13 +0000

    fix(combo-button): add truncation (#308)
    * fix(combo-button): add appropriate string truncation
    
    * fix(combo-button): add appropriate string truncation
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/core/.storybook/preview.js
+++ packages/core/.storybook/preview.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2020, 2020
+ * Copyright IBM Corp. 2020, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -40,9 +40,12 @@ const decorators = [
 ];
 
 const parameters = {
+  controls: { expanded: true, hideNoControlsWarning: true },
   layout: 'centered',
+
+  // Optional default Carbon theme.
   carbonTheme: {
-    theme: 'g10', // optional default carbon theme
+    theme: 'g10',
   },
 };
 

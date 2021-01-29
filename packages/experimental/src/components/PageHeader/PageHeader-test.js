41135b75a81169181e4661973b88270ca76d5083
Lee Chase <lee.chase@uk.ibm.com>
2021-01-29 15:52:54 +0000

    chore: remove unused code

--- packages/experimental/src/components/PageHeader/PageHeader-test.js
+++ packages/experimental/src/components/PageHeader/PageHeader-test.js
@@ -7,7 +7,6 @@
 
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 import { pkgPrefix /*, carbonPrefix */ } from '../../global/js/settings';
 

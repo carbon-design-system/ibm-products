70309a6d8467391a578b3951d6536e75282bcd1e
Alexander-Melo <alexander.melo@ibm.com>
2021-02-11 15:50:14 -0500

    feat(web-terminal): added web terminal default props and propTypes

--- packages/experimental/src/components/WebTerminal/WebTerminal.js
+++ packages/experimental/src/components/WebTerminal/WebTerminal.js
@@ -34,28 +34,30 @@ const WebTerminal = ({
       ])}>
       <header className={`${pkgPrefix}-web-terminal__bar`}>
         <div className={`${pkgPrefix}-web-terminal__actions`}>
-          <div className={`${pkgPrefix}-web-terminal__bar-icon-container`}>
-            <Help className={`${pkgPrefix}-web-terminal__bar-icon`} />
-            <ul className={`${pkgPrefix}-web-terminal__bar-icon-dropdown`}>
-              {documentationLinks.map(
-                ({ label, onClick, href = null, openInNewTab = true }) => (
-                  <li
-                    className={`${pkgPrefix}-web-terminal__bar-icon-dropdown-item`}>
-                    <a
-                      className={`${pkgPrefix}-web-terminal__bar-icon-dropdown-link`}
-                      onClick={(event) =>
-                        onDocumentationLinkClick(event, onClick)
-                      }
-                      href={href}
-                      target={openInNewTab ? '_blank' : null}
-                      rel="noreferrer noopener">
-                      {label}
-                    </a>
-                  </li>
-                )
-              )}
-            </ul>
-          </div>
+          {showDocumentationLinks && (
+            <div className={`${pkgPrefix}-web-terminal__bar-icon-container`}>
+              <Help className={`${pkgPrefix}-web-terminal__bar-icon`} />
+              <ul className={`${pkgPrefix}-web-terminal__bar-icon-dropdown`}>
+                {documentationLinks.map(
+                  ({ label, onClick, href = null, openInNewTab = true }) => (
+                    <li
+                      className={`${pkgPrefix}-web-terminal__bar-icon-dropdown-item`}>
+                      <a
+                        className={`${pkgPrefix}-web-terminal__bar-icon-dropdown-link`}
+                        onClick={(event) =>
+                          onDocumentationLinkClick(event, onClick)
+                        }
+                        href={href}
+                        target={openInNewTab ? '_blank' : null}
+                        rel="noreferrer noopener">
+                        {label}
+                      </a>
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
+          )}
         </div>
         <div
           className={`${pkgPrefix}-web-terminal__bar-icon-container`}
@@ -68,6 +70,29 @@ const WebTerminal = ({
       <div className={`${pkgPrefix}-web-terminal__body`}>{children}</div>
     </div>
   ) : null;
+};
+
+WebTerminal.defaultProps = {
+  documentationLinks: [],
+  className: '',
+};
+
+WebTerminal.propTypes = {
+  open: PropTypes.bool.isRequired,
+  closeTerminal: PropTypes.func.isRequired,
+  documentationLinks: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      href: PropTypes.string,
+      onClick: PropTypes.func,
+      openInNewTab: PropTypes.bool,
+    })
+  ),
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+  className: PropTypes.string,
 };
 
 export default WebTerminal;

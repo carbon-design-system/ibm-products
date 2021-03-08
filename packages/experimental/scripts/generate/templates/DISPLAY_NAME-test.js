047f0c6371a7d0f23deba8b62cfef4cf53f787a1
Lee Chase <leechase@live.com>
2021-03-08 10:24:33 +0000

    Update packages/experimental/scripts/generate/templates/DISPLAY_NAME-test.js
    Co-authored-by: Dave Clark <dave@dcwarwick.org.uk>

--- packages/experimental/scripts/generate/templates/DISPLAY_NAME-test.js
+++ packages/experimental/scripts/generate/templates/DISPLAY_NAME-test.js
@@ -9,7 +9,7 @@ import { render } from '@testing-library/react'; // https://testing-library.com/
 import React from 'react';
 
 import { pkg } from '../../settings';
-import '../../enable-all-canary'; // must come before component is imported
+import '../../enable-all'; // must come before component is imported
 
 const blockClass = `${pkg.prefix}-STYLE_NAME`;
 

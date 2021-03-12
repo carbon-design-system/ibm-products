2b3c2a91802631aebcafc1571c9ccff2032c75c8
James Thompson <jamesthompson@jamess-mbp.attlocal.net>
2021-03-12 11:57:13 -0600

    refactored tests

--- packages/experimental/src/components/UserProfileImage/UserProfileImage-test.js
+++ packages/experimental/src/components/UserProfileImage/UserProfileImage-test.js
@@ -14,14 +14,39 @@ import React from 'react';
 
 import { UserProfileImage } from '.';
 
-const {
-  defaultProps: { backgroundColor, icon, size },
-  name,
-} = UserProfileImage;
+// const {
+//   defaultProps: { backgroundColor, icon, size, theme },
+//   name,
+// } = UserProfileImage;
+
+const { backgroundColor, icon, size, theme } = UserProfileImage;
 
 jest.setTimeout(15000);
 
 describe(name, () => {
+  test('should return a circle with background color', () => {
+    const { container } = render(
+      <UserProfileImage backgroundColor="light-blue" />
+    );
+    const element = container.querySelector(
+      `.${expPrefix}-user-profile-avatar`
+    );
+    const hasBackgroundColor = element.className.includes('light-blue');
+    expect(hasBackgroundColor).toBeTruthy();
+  });
+
+  test('should return an icon for the avatar image', () => {
+    const { container } = render(<UserProfileImage icon="user" />);
+    const renderedSVG = container.querySelector('svg');
+    expect(renderedSVG).toBeTruthy();
+  });
+
+  test('should render image for the avatar image', () => {
+    const { container } = render(<UserProfileImage image="path_to_image.jpg" />);
+    const imagePath = container.querySelector('img').getAttribute('src');
+    expect(typeof imagePath).toBe('string');
+  });
+
   test('should return appropriately size circle based on size prop', () => {
     const { container } = render(<UserProfileImage size="x-large" />);
     const element = container.querySelector(
@@ -31,20 +56,12 @@ describe(name, () => {
     expect(hasSizeClass).toBeTruthy();
   });
 
-  test('should return an icon for the avatar image', () => {
-    const { container } = render(<UserProfileImage icon="user" />);
-    const renderedSVG = container.querySelector('svg');
-    expect(renderedSVG).toBeTruthy();
-  });
-
-  test('should return a circle with background color', () => {
-    const { container } = render(
-      <UserProfileImage backgroundColor="#0f62fe" />
-    );
+  test('should recognize theme setting', () => {
+    const { container } = render(<UserProfileImage theme="light" />);
     const element = container.querySelector(
       `.${expPrefix}-user-profile-avatar`
     );
-    const hasBackgroundColor = element.style.backgroundColor;
-    expect(hasBackgroundColor).toBeTruthy();
+    const hasThemeClass = element.className.includes('light');
+    expect(hasThemeClass).toBeTruthy();
   });
 });

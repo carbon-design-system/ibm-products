b9f4ba3e5e6d166dea0e8857ace4bd529879b427
David Menendez <dmenend@us.ibm.com>
2021-03-18 10:39:04 -0500

    feat: saving

---
+++ packages/experimental/src/components/Saving/Saving.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import cx from 'classnames';
+import { Button } from 'carbon-components-react';
+import PropTypes from 'prop-types';
+import { Canary } from '../_Canary';
+import { pkg } from '../../settings';
+const componentName = 'Saving';
+
+export const Saving = !pkg.isComponentEnabled(componentName)
+  ? // Return canary if not released or flag not set
+    () => <Canary component={componentName} />
+  : // Main component code...
+    ({
+      className,
+      defaultIcon,
+      defaultText,
+      failIcon,
+      failText,
+      inProgressIcon,
+      inProgressText,
+      onClick,
+      status,
+      successIcon,
+      successText,
+      type,
+    }) => {
+      const getStatusText = () => {
+        if (status === 'default') return defaultText;
+        else if (status === 'inprogress') return inProgressText;
+        else if (status === 'success') return successText;
+        return failText;
+      };
+
+      const getStatusIcon = () => {
+        if (status === 'default') return defaultIcon;
+        else if (status === 'inprogress') return inProgressIcon;
+        else if (status === 'success') return successIcon;
+        return failIcon;
+      };
+
+      return (
+        <div className={cx(`${pkg.prefix}-saving`, className)}>
+          {type === 'auto' ? (
+            <p>{getStatusText()}</p>
+          ) : (
+            <Button
+              onClick={onClick}
+              kind="primary"
+              renderIcon={getStatusIcon()}>
+              {getStatusText()}
+            </Button>
+          )}
+        </div>
+      );
+    };
+
+Saving.propTypes = {
+  /**
+   * Optinal class
+   */
+  className: PropTypes.string,
+  /**
+   * Icon for default state
+   */
+  defaultIcon: PropTypes.object,
+  /**
+   * Text for default state
+   */
+  defaultText: PropTypes.string,
+  /**
+   * Icon for failure state
+   */
+  failIcon: PropTypes.object,
+  /**
+   * Text for failure state
+   */
+  failText: PropTypes.string,
+  /**
+   * Icon for in progress state
+   */
+  inProgressIcon: PropTypes.object,
+  /**
+   * Text for in progress state
+   */
+  inProgressText: PropTypes.string,
+  /**
+   * Function handler for button
+   */
+  onClick: PropTypes.func,
+  /**
+   * Save state
+   */
+  status: PropTypes.oneOf(['default', 'inprogress', 'success', 'fail']),
+  /**
+   * Icon for success state
+   */
+  successIcon: PropTypes.object,
+  /**
+   * Text for success state
+   */
+  successText: PropTypes.string,
+  /**
+   * Design style. See usage guidelines for additional information
+   */
+  type: PropTypes.oneOf(['manual', 'auto']),
+};
+
+Saving.defaultProps = {
+  className: '',
+  defaultIcon: null,
+  defaultText: '',
+  failIcon: null,
+  failText: '',
+  inProgressIcon: null,
+  inProgressText: '',
+  onClick: () => {},
+  status: '',
+  successIcon: null,
+  successText: '',
+  type: '',
+};
+
+Saving.displayName = componentName;
---
+++ packages/experimental/src/components/Saving/Saving.test.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright IBM Corp. 2020, 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import { Saving } from '.';
+import '../../enable-all'; // must come before component is imported (directly or indirectly)
+
+const { name } = Saving;
+const defaultProps = {};
+
+describe(name, () => {
+  test('should render', async () => {
+    render(<Saving {...defaultProps} />);
+  });
+});

2f762e812c9728525721b2480e3bdd86f533bbc0
Matthew Gallo <mdgallo07@gmail.com>
2021-03-26 08:10:09 -0400

    fix(Notifications): new notifications get sorted as expected now (#527)
    * fix(Notifications): new notifications get sorted as expected now
    
    * fix(Notifications): check that array exists before accessing length
    
    Co-authored-by: Matt Gallo <mdgallo@us.ibm.com>

--- packages/cloud-cognitive/src/components/Notifications/Notification.stories.js
+++ packages/cloud-cognitive/src/components/Notifications/Notification.stories.js
@@ -7,6 +7,7 @@
 
 import React, { useState } from 'react';
 import { action } from '@storybook/addon-actions';
+import { Button } from 'carbon-components-react';
 import {
   Header,
   HeaderContainer,
@@ -18,6 +19,7 @@ import { User20, Notification20 } from '@carbon/icons-react';
 import { white } from '@carbon/colors';
 import styles from './_storybook-styles.scss';
 import { pkg } from '../../settings';
+import uuidv4 from '../../global/js/utils/uuidv4';
 import '../../enable-all'; // must come before component is imported (directly or indirectly)
 import { Notifications } from '.';
 
@@ -73,6 +75,37 @@ const renderUIShellHeader = (open, setOpen) => (
 
 const Template = (args) => {
   const [open, setOpen] = useState(false);
+  const [notificationsData, setNotificationsData] = useState(data);
+
+  const addNewNotification = () => {
+    const newNotification = {
+      id: uuidv4(),
+      type: 'success',
+      title: 'Deployed app successfully',
+      description: 'Application has been deployed.',
+      timestamp: new Date(),
+      unread: true,
+      onNotificationClick: action(`Clicked on notification`),
+    };
+    setNotificationsData((arr) => [...arr, newNotification]);
+  };
+
+  return (
+    <>
+      {renderUIShellHeader(open, setOpen)}
+      <Button onClick={addNewNotification}>Add new notification</Button>
+      <Notifications
+        {...args}
+        data={notificationsData}
+        open={open}
+        setOpen={setOpen}
+      />
+    </>
+  );
+};
+
+const EmptyNotifications = (args) => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       {renderUIShellHeader(open, setOpen)}
@@ -83,7 +116,6 @@ const Template = (args) => {
 
 export const Default = Template.bind({});
 Default.args = {
-  data,
   onDoNotDisturbChange: action('Toggled to do not disturb'),
   onViewAllClick: action('Clicked view all button'),
   onSettingsClick: action('Cliked settings gear'),
@@ -91,7 +123,7 @@ Default.args = {
   onDismissSingleNotification: action('Dismiss single notification'),
 };
 
-export const EmptyState = Template.bind({});
+export const EmptyState = EmptyNotifications.bind({});
 EmptyState.args = {
   data: [],
   onDoNotDisturbChange: action('Toggled to do not disturb'),

cb6313b35e1ed046cdbe562c7f64715721648afa
Dave Clark <dave.clark@uk.ibm.com>
2021-03-30 16:29:24 +0100

    chore: test for stacking depth

--- packages/cloud-cognitive/src/components/Tearsheet/TearsheetShell.js
+++ packages/cloud-cognitive/src/components/Tearsheet/TearsheetShell.js
@@ -130,11 +130,9 @@ export const TearsheetShell = ({
       </ComposedModal>
     );
   } else {
-    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-      console.error(
-        'Tearsheet not rendered - more than 3 levels of tearsheet stacking.'
-      );
-    }
+    console.warn(
+      'Tearsheet not rendered: more than 3 levels of tearsheet stacking.'
+    );
     return null;
   }
 };

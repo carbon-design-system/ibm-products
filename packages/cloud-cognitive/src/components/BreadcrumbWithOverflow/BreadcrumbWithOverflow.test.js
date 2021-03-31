6c70628c49689822876bc77884201e5cda6dea55
Lee Chase <leechase@live.com>
2021-03-31 20:33:00 +0100

    Update packages/cloud-cognitive/src/components/BreadcrumbWithOverflow/BreadcrumbWithOverflow.test.js
    Co-authored-by: Dave Clark <dave@dcwarwick.org.uk>

--- packages/cloud-cognitive/src/components/BreadcrumbWithOverflow/BreadcrumbWithOverflow.test.js
+++ packages/cloud-cognitive/src/components/BreadcrumbWithOverflow/BreadcrumbWithOverflow.test.js
@@ -6,7 +6,7 @@
  */
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import '../../enable-all'; // must come before component is imported (directly or indirectly)
+import '../../utils/enable-all'; // must come before component is imported (directly or indirectly)
 import { BreadcrumbItem } from 'carbon-components-react';
 import { BreadcrumbWithOverflow } from '.';
 

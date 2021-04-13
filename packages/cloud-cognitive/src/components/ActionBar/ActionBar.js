f4e9e08bf51ade290f6925867049eb7a73a87824
Lee Chase <lee.chase@uk.ibm.com>
2021-04-13 12:41:54 +0100

    chore: tidy up action bar tests and coverage

--- packages/cloud-cognitive/src/components/ActionBar/ActionBar.js
+++ packages/cloud-cognitive/src/components/ActionBar/ActionBar.js
@@ -28,23 +28,27 @@ const componentName = 'ActionBar';
 /**
  * The ActionBar is used internally by the PageHeader to wrap ActionBarItems.
  */
-export let ActionBar = ({
-  children,
-  className,
-  maxVisibleActionBarItems,
-  onWidthChange,
-  overflowAriaLabel,
-  rightAlign,
-}) => {
-  const [displayCount, setDisplayCount] = useState(0);
-  const [displayedItems, setDisplayedItems] = useState([]);
-  const internalId = useRef(uuidv4());
-  const [childArray, setChildArray] = useState([]);
-  const refDisplayedItems = useRef(null);
+export let ActionBar = React.forwardRef(
+  (
+    {
+      children,
+      className,
+      maxVisibleActionBarItems,
+      onWidthChange,
+      overflowAriaLabel,
+      rightAlign,
+      // Collect any other property values passed in.
+      ...rest
+    },
+    ref
+  ) => {
+    const [displayCount, setDisplayCount] = useState(0);
+    const [displayedItems, setDisplayedItems] = useState([]);
+    const internalId = useRef(uuidv4());
+    const [childArray, setChildArray] = useState([]);
+    const refDisplayedItems = useRef(null);
 
-  const ActionBarOverflowItems = ({ overflowItems }) => {
-    /* istanbul ignore next if */ // not sure why this is not hit in tests
-    if (displayCount < childArray.length) {
+    const ActionBarOverflowItems = ({ overflowItems }) => {
       return (
         <OverflowMenu
           ariaLabel={overflowAriaLabel}
@@ -77,111 +81,117 @@ export let ActionBar = ({
           })}
         </OverflowMenu>
       );
-    } else {
-      return null;
-    }
-  };
+    };
 
-  // create child array from children which may be a fragment
-  useEffect(() => {
-    setChildArray(unwrapIfFragment(children));
-  }, [children]);
+    ActionBarOverflowItems.propTypes = {
+      /**
+       * overflowItems: items to bre shown in the ActionBar overflow menu
+       */
+      overflowItems: PropTypes.arrayOf(PropTypes.element),
+    };
 
-  // creates displayed items based on displayCount and alignment
-  useEffect(() => {
-    const newDisplayedItems = [];
-    const newOverflowItems = [];
+    // create child array from children which may be a fragment
+    useEffect(() => {
+      setChildArray(unwrapIfFragment(children));
+    }, [children]);
 
-    // add visible items
-    for (let index = 0; index < displayCount; index++) {
-      newDisplayedItems.push(
-        React.cloneElement(childArray[index], {
-          key: `displayed-action-bar-item-${internalId.current}-${index}`,
-        })
-      );
-    }
+    // creates displayed items based on displayCount and alignment
+    useEffect(() => {
+      const newDisplayedItems = [];
+      const newOverflowItems = [];
 
-    // add overflow items
-    for (let index = displayCount; index < childArray.length; index++) {
-      newOverflowItems.push(
-        React.cloneElement(childArray[index], {
-          key: `overflow-action-bar-item-${internalId.current}-${index}`,
-        })
-      );
-    }
-
-    // add overflow menu with items
-    if (newOverflowItems.length) {
-      const overflowMenu = (
-        <ActionBarOverflowItems
-          overflowItems={newOverflowItems}
-          key={`overflow-menu-${internalId.current}`}></ActionBarOverflowItems>
-      );
-      newDisplayedItems.push(overflowMenu);
-    }
-
-    setDisplayedItems(newDisplayedItems);
-  }, [childArray, displayCount]);
-
-  // determine display count based on space available and width of pageActions
-  const checkFullyVisibleItems = () => {
-    const spaceAvailable = refDisplayedItems.current.offsetWidth;
-    const actionBarItemWidth = refDisplayedItems.current.offsetHeight; // short cut measure width
-
-    if (actionBarItemWidth > 0) {
-      const mightFit = spaceAvailable / actionBarItemWidth;
-      // visibleItems may include 1 overflow menu
-      const visibleItems = maxVisibleActionBarItems
-        ? Math.min(childArray.length, maxVisibleActionBarItems)
-        : childArray.length;
-      let willFit = Math.min(Math.floor(mightFit), visibleItems);
-
-      onWidthChange &&
-        onWidthChange({
-          maxWidth: actionBarItemWidth * visibleItems,
-          minWidth: actionBarItemWidth,
-        });
-
-      // action bar items are a fixed width
-      if (willFit < childArray.length) {
-        willFit -= 1; // remove one for overflow menu
+      // add visible items
+      for (let index = 0; index < displayCount; index++) {
+        newDisplayedItems.push(
+          React.cloneElement(childArray[index], {
+            key: `displayed-action-bar-item-${internalId.current}-${index}`,
+          })
+        );
       }
 
-      if (willFit < 1) {
-        setDisplayCount(0);
-      } else {
-        setDisplayCount(willFit);
+      // add overflow items
+      for (let index = displayCount; index < childArray.length; index++) {
+        newOverflowItems.push(
+          React.cloneElement(childArray[index], {
+            key: `overflow-action-bar-item-${internalId.current}-${index}`,
+          })
+        );
       }
-    }
-  };
 
-  useEffect(() => {
-    checkFullyVisibleItems();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [maxVisibleActionBarItems, childArray.length]);
+      // add overflow menu with items
+      if (newOverflowItems.length) {
+        const overflowMenu = (
+          <ActionBarOverflowItems
+            overflowItems={newOverflowItems}
+            key={`overflow-menu-${internalId.current}`}></ActionBarOverflowItems>
+        );
+        newDisplayedItems.push(overflowMenu);
+      }
 
-  const handleResize = () => {
-    // width is the space available for all action bar items horizontally
-    // the action bar items are squares so the height should be one item wide
-    /* istanbul ignore next */
-    checkFullyVisibleItems();
-  };
+      setDisplayedItems(newDisplayedItems);
+    }, [childArray, displayCount]);
 
-  return (
-    <ReactResizeDetector onResize={handleResize}>
-      <div className={cx([blockClass, className])}>
-        <div
-          ref={refDisplayedItems}
-          className={cx([
-            `${blockClass}--displayed-items`,
-            { [`${blockClass}--displayed-items--right`]: rightAlign },
-          ])}>
-          {displayedItems}
+    // determine display count based on space available and width of pageActions
+    const checkFullyVisibleItems = () => {
+      const spaceAvailable = refDisplayedItems.current.offsetWidth;
+      const actionBarItemWidth = refDisplayedItems.current.offsetHeight; // short cut measure width
+
+      /* istanbul ignore next if */
+      if (actionBarItemWidth > 0) {
+        const mightFit = spaceAvailable / actionBarItemWidth;
+        // visibleItems may include 1 overflow menu
+        const visibleItems = maxVisibleActionBarItems
+          ? Math.min(childArray.length, maxVisibleActionBarItems + 1) // + 1 for overflow menu if needed
+          : childArray.length;
+        let willFit = Math.min(Math.floor(mightFit), visibleItems);
+
+        onWidthChange &&
+          onWidthChange({
+            maxWidth: actionBarItemWidth * visibleItems,
+            minWidth: actionBarItemWidth,
+          });
+
+        // action bar items are a fixed width
+        if (willFit < childArray.length) {
+          willFit -= 1; // remove one for overflow menu
+        }
+
+        if (willFit < 1) {
+          setDisplayCount(0);
+        } else {
+          setDisplayCount(willFit);
+        }
+      }
+    };
+
+    useEffect(() => {
+      checkFullyVisibleItems();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [maxVisibleActionBarItems, childArray]);
+
+    const handleResize = () => {
+      // width is the space available for all action bar items horizontally
+      // the action bar items are squares so the height should be one item wide
+      /* istanbul ignore next */ // not sure how to fake window resize
+      checkFullyVisibleItems();
+    };
+
+    return (
+      <ReactResizeDetector onResize={handleResize}>
+        <div {...rest} className={cx([blockClass, className])} ref={ref}>
+          <div
+            ref={refDisplayedItems}
+            className={cx([
+              `${blockClass}--displayed-items`,
+              { [`${blockClass}--displayed-items--right`]: rightAlign },
+            ])}>
+            {displayedItems}
+          </div>
         </div>
-      </div>
-    </ReactResizeDetector>
-  );
-};
+      </ReactResizeDetector>
+    );
+  }
+);
 
 ActionBar.displayName = componentName;
 ActionBar.propTypes = {

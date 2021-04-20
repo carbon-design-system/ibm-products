4005a986be3b52f3c39402d16e015a73db13299e
Syd Rosa <sydabailey@gmail.com>
2021-04-20 10:28:10 -0500

    Epic: Create Modal (#501)
    * feat: create modal initial component structure
    
    * fix: number input border bottom in storybook
    
    * fix: add default title prop
    
    * feat: add dark theme to storybook
    
    * chore: add testing
    
    * chore: update documentation
    
    * fix: update tests
    
    * fix: dropdown caret in stories
    
    * fix: remove weird button styling on number input component
    
    * fix: update documentation
    
    * feat: add additional form validation story
    
    * chore: refactor component to add useRef
    
    * fix: update enable-all path
    
    * fix: update enable-all path
    
    * chore: add blockclass to component
    
    * feat: add testing to Create Modal
    
    * chore: add CreateModal to files per updated code guidelines
    
    * chore: add testing, update component structure
    
    * fix: update story propTypes
    
    * fix: remove global directory --old
    
    * chore: update docgen for create modal proptypes
    
    * Update ImportModal.js
    
    Removing a small change I made while reviewing David's PR
    
    * Update ImportModal.js
    
    Remove additional spacing before bracket
    
    Co-authored-by: Matthew Gallo <mdgallo07@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ packages/cloud-cognitive/src/components/CreateModal/index.js
@@ -0,0 +1,10 @@
+//
+// Copyright IBM Corp. 2020, 2020
+//
+// This source code is licensed under the Apache-2.0 license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+// NOTE: An index file is most useful when you have multiple components
+
+export { CreateModal } from './CreateModal';

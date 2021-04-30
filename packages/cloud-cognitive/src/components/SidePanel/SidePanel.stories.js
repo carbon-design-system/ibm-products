467475f7b8ed03bd7809cef05226c50785d64b68
Matt Gallo <mdgallo@us.ibm.com>
2021-04-30 11:20:00 -0400

    chore(SidePanel): add test coverage to new animateTitle prop changes

--- packages/cloud-cognitive/src/components/SidePanel/SidePanel.stories.js
+++ packages/cloud-cognitive/src/components/SidePanel/SidePanel.stories.js
@@ -438,3 +438,36 @@ WithMinimalContent.args = {
   actions: 0,
   minimalContent: true,
 };
+
+export const WithStaticTitle = SlideOverTemplate.bind({});
+WithStaticTitle.args = {
+  ...defaultStoryProps,
+  actions: 0,
+  animateTitle: false,
+  includeOverlay: true,
+};
+
+export const WithStaticTitleAndActionToolbar = SlideOverTemplate.bind({});
+WithStaticTitleAndActionToolbar.args = {
+  ...defaultStoryProps,
+  actions: 0,
+  animateTitle: false,
+  includeOverlay: true,
+  actionToolbarButtons: [
+    {
+      label: 'Copy',
+      icon: Copy20,
+      onActionToolbarButtonClick: () => {},
+    },
+    {
+      label: 'Settings',
+      icon: Settings20,
+      onActionToolbarButtonClick: () => {},
+    },
+    {
+      label: 'Delete',
+      icon: Delete20,
+      onActionToolbarButtonClick: () => {},
+    },
+  ],
+};

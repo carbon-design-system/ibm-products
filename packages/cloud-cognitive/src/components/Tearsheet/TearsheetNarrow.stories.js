c963120fd5bd3cd098016d32189537844b3a9238
Dave Clark <dave.clark@uk.ibm.com>
2021-04-30 23:50:19 +0100

    Smarten up micro-animation when stacked tearsheets are closed (#689)
    * fix: transition all props for smooth handover when stacking
    
    * chore: improve stories, and consolidate the stacking scenarios
    
    * chore: update snapshot

--- packages/cloud-cognitive/src/components/Tearsheet/TearsheetNarrow.stories.js
+++ packages/cloud-cognitive/src/components/Tearsheet/TearsheetNarrow.stories.js
@@ -84,15 +84,127 @@ const title = 'Title of the tearsheet';
 const Template = ({ actions, ...args }) => {
   const [open, setOpen] = useState(false);
 
+  const wiredActions = Array.prototype.map.call(actions, (action) => {
+    if (action.label === 'Cancel') {
+      const previousClick = action.onClick;
+      return {
+        ...action,
+        onClick: (evt) => {
+          setOpen(false);
+          previousClick(evt);
+        },
+      };
+    }
+    return action;
+  });
+
   return (
     <>
       <style>{`.${pkg.prefix}--tearsheet { opacity: 0 }`};</style>
       <Button onClick={() => setOpen(true)}>Open Tearsheet</Button>
       <TearsheetNarrow
         {...args}
-        {...{ actions, open }}
+        actions={wiredActions}
+        open={open}
         onClose={() => setOpen(false)}>
         {mainContent}
+      </TearsheetNarrow>
+    </>
+  );
+};
+
+// eslint-disable-next-line react/prop-types
+const StackedTemplate = ({ actions, ...args }) => {
+  const [open1, setOpen1] = useState(false);
+  const [open2, setOpen2] = useState(false);
+  const [open3, setOpen3] = useState(false);
+
+  const wiredActions1 = Array.prototype.map.call(actions, (action) => {
+    if (action.label === 'Cancel') {
+      const previousClick = action.onClick;
+      return {
+        ...action,
+        onClick: (evt) => {
+          setOpen1(false);
+          previousClick(evt);
+        },
+      };
+    }
+    return action;
+  });
+
+  const wiredActions2 = Array.prototype.map.call(actions, (action) => {
+    if (action.label === 'Cancel') {
+      const previousClick = action.onClick;
+      return {
+        ...action,
+        onClick: (evt) => {
+          setOpen2(false);
+          previousClick(evt);
+        },
+      };
+    }
+    return action;
+  });
+
+  const wiredActions3 = Array.prototype.map.call(actions, (action) => {
+    if (action.label === 'Cancel') {
+      const previousClick = action.onClick;
+      return {
+        ...action,
+        onClick: (evt) => {
+          setOpen3(false);
+          previousClick(evt);
+        },
+      };
+    }
+    return action;
+  });
+
+  return (
+    <>
+      <style>{`.${pkg.prefix}--tearsheet { opacity: 0 }`};</style>
+      <div
+        style={{
+          display: 'flex',
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          zIndex: 10000,
+        }}>
+        <Button onClick={() => setOpen1(!open1)}>Toggle #1</Button>
+        <Button onClick={() => setOpen2(!open2)}>Toggle #2</Button>
+        <Button onClick={() => setOpen3(!open3)}>Toggle #3</Button>
+      </div>
+      <TearsheetNarrow
+        {...args}
+        actions={wiredActions1}
+        title="Tearsheet #1"
+        open={open1}
+        onClose={() => setOpen1(false)}>
+        <div className="tearsheet-stories__narrow-content-block">
+          Main content 1
+        </div>
+      </TearsheetNarrow>
+      <TearsheetNarrow
+        {...args}
+        actions={wiredActions2}
+        title="Tearsheet #2"
+        open={open2}
+        onClose={() => setOpen2(false)}>
+        <div className="tearsheet-stories__narrow-content-block">
+          Main content 2
+        </div>
+      </TearsheetNarrow>
+      <TearsheetNarrow
+        {...args}
+        actions={wiredActions3}
+        title="Tearsheet #3"
+        open={open3}
+        onClose={() => setOpen3(false)}>
+        <div className="tearsheet-stories__narrow-content-block">
+          Main content 3
+        </div>
       </TearsheetNarrow>
     </>
   );
@@ -119,4 +231,15 @@ fullyLoaded.args = {
   title,
   verticalPosition: 'normal',
   actions: 0,
+};
+
+export const stacked = StackedTemplate.bind({});
+stacked.storyName = 'Stacking narrow tearsheets';
+stacked.args = {
+  closeIconDescription,
+  description,
+  height: 'lower',
+  label,
+  preventCloseOnClickOutside: true,
+  actions: 6,
 };

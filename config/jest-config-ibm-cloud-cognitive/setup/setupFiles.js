d85f3ddd4a250c10828019b07ac6772802d0970c
Dave Clark <dave.clark@uk.ibm.com>
2021-07-01 01:20:55 +0100

    chore: fix pack to cloud-cognitive from jest config

--- config/jest-config-ibm-cloud-cognitive/setup/setupFiles.js
+++ config/jest-config-ibm-cloud-cognitive/setup/setupFiles.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { pkg } from '../../cloud-cognitive/src/settings';
+import { pkg } from '../../../packages/cloud-cognitive/src/settings';
 
 pkg._silenceWarnings(true);
 pkg.setAllComponents(true);

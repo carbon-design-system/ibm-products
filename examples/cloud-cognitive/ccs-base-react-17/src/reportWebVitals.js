b2044039c9de50d84ff8cf71029a3508423a21fc
Dave Clark <dave.clark@uk.ibm.com>
2021-07-16 15:10:49 +0100

    chore: fix the example projects

--- examples/cloud-cognitive/ccs-base-react-17/src/reportWebVitals.js
+++ examples/cloud-cognitive/ccs-base-react-17/src/reportWebVitals.js
@@ -1,3 +1,5 @@
+// cspell:words TTFB
+
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {

5763f3ea7ddf3a6a40f313abbe2c29dbf65e459a
Jared Devine <Jared.Devine@ibm.com>
2021-07-19 10:24:05 -0400

    Removed toolbar imports

--- packages/cloud-cognitive/src/global/js/package-settings.js
+++ packages/cloud-cognitive/src/global/js/package-settings.js
@@ -53,7 +53,6 @@ const defaults = {
     ProductiveCard: false,
     SidePanel: false,
     TagSet: false,
-    // Toolbar: false,
     WebTerminal: false,
     /* new component flags here - comment used by generate CLI */
   },

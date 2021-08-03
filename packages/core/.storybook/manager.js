5017f909238484274dd40775e8b6457bd125b132
Simon Finney <simonjfinney@gmail.com>
2021-08-03 17:01:02 +0100

    docs(storybook): add current version (#1095)
    * docs(storybook): add current version
    
    * docs(storybook): revert variable rename

--- packages/core/.storybook/manager.js
+++ packages/core/.storybook/manager.js
@@ -1,21 +1,24 @@
 /**
- * Copyright IBM Corp. 2020, 2020
+ * Copyright IBM Corp. 2020, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
-
 import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming/create';
 
-import packageInfo from '../package.json';
+import React from 'react';
+
+import packageInfo from '../../cloud-cognitive/package.json';
+
+const { description, version } = packageInfo;
 
 addons.setConfig({
   theme: create({
-    brandTitle: packageInfo.name,
+    brandTitle: `${description} v${version}`,
   }),
+
   sidebar: {
     renderLabel: (item) => {
       const parts = item.name.split('#');

675e06312bd353ab2ea98b5c38bc64f5740d82c9
Simon Finney <SIMONFIN@ie.ibm.com>
2021-08-12 12:20:00 +0100

    chore: sync `ibm-security` with `packages/security`

--- packages/security/setup/setupFilesAfterEnv.js
+++ packages/security/setup/setupFilesAfterEnv.js
@@ -5,10 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { configure } from '@testing-library/dom';
-
-configure({ testIdAttribute: 'data-testid' });
-
 const { fn } = jest;
 
 window.ResizeObserver = fn().mockImplementation(() => ({

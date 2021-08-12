43f88035bdc121db06e39ba55690680d68d930c7
Lee Chase <lee.chase@uk.ibm.com>
2021-08-12 11:16:10 +0100

    fix: code sandbox examples

--- examples/cloud-cognitive/ccs-base-react-17/src/App.test.js
+++ examples/cloud-cognitive/ccs-base-react-17/src/App.test.js
@@ -3,6 +3,5 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  screen.getByText(/Show AboutModal/i);
 });

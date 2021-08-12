59b0f3d5aab49843b8447f64729c5b3b9e45d2c8
Simon Finney <SIMONFIN@ie.ibm.com>
2021-08-12 11:58:34 +0100

    chore: sync `ibm-security` with `packages/security`

--- packages/security/config.js
+++ packages/security/config.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { resolve } from 'path';
+const root = '../..';
+const sectionTitle = 'Security';
 
-export const root = resolve(__dirname, '../..');
-export const sectionTitle = 'Security';
+export { root, sectionTitle };

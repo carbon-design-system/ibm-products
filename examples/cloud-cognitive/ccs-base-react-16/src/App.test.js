ebb4399a6e2cdb579d15b3c0afa29d5a1ab750ec
Lee Chase <lee.chase@uk.ibm.com>
2021-08-20 11:49:08 +0100

    chore: address review concerns

--- examples/cloud-cognitive/ccs-base-react-16/src/App.test.js
+++ examples/cloud-cognitive/ccs-base-react-16/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders an AboutModal as a basic package usage example', () => {
   render(<App />);
   screen.getByText(/Show AboutModal/i);
 });

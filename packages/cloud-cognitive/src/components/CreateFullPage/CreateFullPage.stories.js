0daf67077e48ee98fd99c530b68d4961fd861379
Syd <sydabailey@gmail.com>
2021-08-26 13:14:58 -0500

    chore: remove modal flashing from story

--- packages/cloud-cognitive/src/components/CreateFullPage/CreateFullPage.stories.js
+++ packages/cloud-cognitive/src/components/CreateFullPage/CreateFullPage.stories.js
@@ -241,166 +241,169 @@ const TemplateWithSections = ({ ...args }) => {
   const [simulatedDelay] = useState(750);
 
   return (
-    <CreateFullPage className={`${blockClass}`} {...args}>
-      <CreateFullPageStep
-        title="Partition"
-        subtitle="One or more partitions make up a topic. A partition is an ordered list
+    <>
+      <style>{`.${carbon.prefix}--modal { opacity: 0; }`};</style>
+      <CreateFullPage className={`${blockClass}`} {...args}>
+        <CreateFullPageStep
+          title="Partition"
+          subtitle="One or more partitions make up a topic. A partition is an ordered list
         of messages."
-        description="Partitions are distributed across the brokers in order to increase the
+          description="Partitions are distributed across the brokers in order to increase the
         scalability of your topic. You can also use them to distribute
         messages across the members of a consumer group."
-        onNext={() => {
-          return new Promise((resolve, reject) => {
-            setTimeout(() => {
-              // Example usage of how to prevent the next step if some kind
-              // of error occurred during the `onNext` handler.
-              if (shouldReject) {
-                setHasSubmitError(true);
-                reject();
-              }
-              setIsInvalid(false);
-              resolve();
-            }, simulatedDelay);
-          });
-        }}
-        disableSubmit={!textInput}>
-        <Row>
-          <Column xlg={5} lg={5} md={4} sm={4}>
-            <FormGroup
-              className={`${blockClass}__step-fieldset ${storyClass}__step-fieldset--label`}
-              legendText="Partition">
-              <TextInput
-                id="test-6"
-                invalidText="A valid value is required"
-                labelText="Topic name"
-                placeholder="Enter topic name"
-                onChange={(e) => {
-                  setTextInput(e.target.value);
-                  setIsInvalid(false);
-                }}
-                onBlur={() => {
-                  textInput.length === 0 && setIsInvalid(true);
-                }}
-                invalid={isInvalid}
-              />
-              {hasSubmitError && (
-                <InlineNotification
-                  lowContrast
-                  kind="error"
-                  title="Error"
-                  subtitle="Resolve errors to continue"
-                  onClose={() => setHasSubmitError(false)}
-                />
-              )}
-              <div>
-                <Tooltip
-                  triggerClassName={`${storyClass}__tooltip`}
-                  direction="right"
-                  tabIndex={0}>
-                  <p>
-                    Once toggled on, an inline error notification will appear
-                    upon clicking next. This is an example usage of how to
-                    prevent the next step if some kind of error occurred during
-                    the `onNext` handler.
-                  </p>
-                </Tooltip>
-                <Toggle
-                  className={`${storyClass}__error--toggle`}
-                  id="simulated-error-toggle"
-                  size="sm"
-                  labelText="Simulate error"
-                  onToggle={(event) => setShouldReject(event)}
-                />
-              </div>
-            </FormGroup>
-          </Column>
-        </Row>
-        <span className={`${blockClass}__section-divider`} />
-        <h5 className={`${blockClass}__step-title`}>Core configuration</h5>
-        <h6 className={`${blockClass}__step-subtitle`}>
-          This is how long messages are retained before they are deleted.
-        </h6>
-        <FormGroup
-          className={`${blockClass}__step-fieldset ${storyClass}__step-fieldset--label`}
-          legendText="Core configuration">
-          <p className={`${blockClass}__step-description`}>
-            If your messages are not read by a consumer within this time, they
-            will be missed.
-          </p>
+          onNext={() => {
+            return new Promise((resolve, reject) => {
+              setTimeout(() => {
+                // Example usage of how to prevent the next step if some kind
+                // of error occurred during the `onNext` handler.
+                if (shouldReject) {
+                  setHasSubmitError(true);
+                  reject();
+                }
+                setIsInvalid(false);
+                resolve();
+              }, simulatedDelay);
+            });
+          }}
+          disableSubmit={!textInput}>
           <Row>
             <Column xlg={5} lg={5} md={4} sm={4}>
-              <TextInput
-                id="test-7"
-                invalidText="A valid value is required"
-                labelText="Topic name (optional)"
-                placeholder="Enter topic name"
-              />
+              <FormGroup
+                className={`${blockClass}__step-fieldset ${storyClass}__step-fieldset--label`}
+                legendText="Partition">
+                <TextInput
+                  id="test-6"
+                  invalidText="A valid value is required"
+                  labelText="Topic name"
+                  placeholder="Enter topic name"
+                  onChange={(e) => {
+                    setTextInput(e.target.value);
+                    setIsInvalid(false);
+                  }}
+                  onBlur={() => {
+                    textInput.length === 0 && setIsInvalid(true);
+                  }}
+                  invalid={isInvalid}
+                />
+                {hasSubmitError && (
+                  <InlineNotification
+                    lowContrast
+                    kind="error"
+                    title="Error"
+                    subtitle="Resolve errors to continue"
+                    onClose={() => setHasSubmitError(false)}
+                  />
+                )}
+                <div>
+                  <Tooltip
+                    triggerClassName={`${storyClass}__tooltip`}
+                    direction="right"
+                    tabIndex={0}>
+                    <p>
+                      Once toggled on, an inline error notification will appear
+                      upon clicking next. This is an example usage of how to
+                      prevent the next step if some kind of error occurred
+                      during the `onNext` handler.
+                    </p>
+                  </Tooltip>
+                  <Toggle
+                    className={`${storyClass}__error--toggle`}
+                    id="simulated-error-toggle"
+                    size="sm"
+                    labelText="Simulate error"
+                    onToggle={(event) => setShouldReject(event)}
+                  />
+                </div>
+              </FormGroup>
             </Column>
           </Row>
-          <Row>
-            <Column xlg={3} lg={3} md={3} sm={3}>
-              <NumberInput
-                id="test-8"
-                invalidText="Number is not valid"
-                label="Label (optional)"
-                max={100}
-                min={0}
-                step={10}
-                value={0}
+          <span className={`${blockClass}__section-divider`} />
+          <h5 className={`${blockClass}__step-title`}>Core configuration</h5>
+          <h6 className={`${blockClass}__step-subtitle`}>
+            This is how long messages are retained before they are deleted.
+          </h6>
+          <FormGroup
+            className={`${blockClass}__step-fieldset ${storyClass}__step-fieldset--label`}
+            legendText="Core configuration">
+            <p className={`${blockClass}__step-description`}>
+              If your messages are not read by a consumer within this time, they
+              will be missed.
+            </p>
+            <Row>
+              <Column xlg={5} lg={5} md={4} sm={4}>
+                <TextInput
+                  id="test-7"
+                  invalidText="A valid value is required"
+                  labelText="Topic name (optional)"
+                  placeholder="Enter topic name"
+                />
+              </Column>
+            </Row>
+            <Row>
+              <Column xlg={3} lg={3} md={3} sm={3}>
+                <NumberInput
+                  id="test-8"
+                  invalidText="Number is not valid"
+                  label="Label (optional)"
+                  max={100}
+                  min={0}
+                  step={10}
+                  value={0}
+                />
+                <NumberInput
+                  id="test-9"
+                  invalidText="Number is not valid"
+                  label="Label (optional)"
+                  max={100}
+                  min={0}
+                  step={10}
+                  value={0}
+                />
+              </Column>
+            </Row>
+            <Row>
+              <Column xlg={5} lg={5} md={4} sm={4}>
+                <TextInput
+                  id="test-10"
+                  invalidText="A valid value is required"
+                  labelText="Minimum in-sync replicas (optional)"
+                  placeholder="Enter topic name"
+                />
+              </Column>
+            </Row>
+          </FormGroup>
+        </CreateFullPageStep>
+        <CreateFullPageStep
+          title="Message retention"
+          subtitle="This is how many copies of a topic will be made for high availability"
+          description="The partitions of each topic can be replicated across a configurable number of brokers.">
+          <div>
+            <RadioButtonGroup
+              defaultSelected="standard"
+              legend="Group Legend"
+              name="radio-button-group"
+              valueSelected="standard"
+              orientation="vertical">
+              <RadioButton
+                id="radio-4"
+                labelText="Replication factor: 1"
+                value="standard"
               />
-              <NumberInput
-                id="test-9"
-                invalidText="Number is not valid"
-                label="Label (optional)"
-                max={100}
-                min={0}
-                step={10}
-                value={0}
+              <RadioButton
+                id="radio-5"
+                labelText="Replication factor: 2"
+                value="default-selected"
               />
-            </Column>
-          </Row>
-          <Row>
-            <Column xlg={5} lg={5} md={4} sm={4}>
-              <TextInput
-                id="test-10"
-                invalidText="A valid value is required"
-                labelText="Minimum in-sync replicas (optional)"
-                placeholder="Enter topic name"
+              <RadioButton
+                id="radio-6"
+                labelText="Replication factor: 3"
+                value="disabled"
               />
-            </Column>
-          </Row>
-        </FormGroup>
-      </CreateFullPageStep>
-      <CreateFullPageStep
-        title="Message retention"
-        subtitle="This is how many copies of a topic will be made for high availability"
-        description="The partitions of each topic can be replicated across a configurable number of brokers.">
-        <div>
-          <RadioButtonGroup
-            defaultSelected="standard"
-            legend="Group Legend"
-            name="radio-button-group"
-            valueSelected="standard"
-            orientation="vertical">
-            <RadioButton
-              id="radio-4"
-              labelText="Replication factor: 1"
-              value="standard"
-            />
-            <RadioButton
-              id="radio-5"
-              labelText="Replication factor: 2"
-              value="default-selected"
-            />
-            <RadioButton
-              id="radio-6"
-              labelText="Replication factor: 3"
-              value="disabled"
-            />
-          </RadioButtonGroup>
-        </div>
-      </CreateFullPageStep>
-    </CreateFullPage>
+            </RadioButtonGroup>
+          </div>
+        </CreateFullPageStep>
+      </CreateFullPage>
+    </>
   );
 };
 

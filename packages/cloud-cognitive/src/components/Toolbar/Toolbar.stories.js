76e2ba728b2b314724837d9fac63ecf344dd5569
Simon Finney <SIMONFIN@ie.ibm.com>
2021-08-26 16:10:00 +0100

    feat(toolbar): add active button

--- packages/cloud-cognitive/src/components/Toolbar/Toolbar.stories.js
+++ packages/cloud-cognitive/src/components/Toolbar/Toolbar.stories.js
@@ -65,7 +65,7 @@ function _Toolbar(args) {
   return (
     <Toolbar {...args}>
       <ToolbarGroup>
-        <ToolbarButton iconDescription="Save" renderIcon={Save16} active />
+        <ToolbarButton iconDescription="Save" renderIcon={Save16} />
         <ToolbarButton iconDescription="Share" renderIcon={Share16} />
         <ToolbarButton iconDescription="Upload" renderIcon={Upload16} />
         <ToolbarButton iconDescription="Print" renderIcon={Printer16} />

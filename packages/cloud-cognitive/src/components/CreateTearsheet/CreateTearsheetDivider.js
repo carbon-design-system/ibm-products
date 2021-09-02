18ea69c844ce1ae5cc13195af46199131796f3ff
Matt Gallo <mdgallo@us.ibm.com>
2021-09-02 09:13:43 -0400

    fix(CreateTearsheet): add canary check to divider component

--- packages/cloud-cognitive/src/components/CreateTearsheet/CreateTearsheetDivider.js
+++ packages/cloud-cognitive/src/components/CreateTearsheet/CreateTearsheetDivider.js
@@ -10,11 +10,18 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { pkg } from '../../settings';
 
+const componentName = 'CreateTearsheetDivider';
 const blockClass = `${pkg.prefix}--tearsheet-create__section--divider`;
 export let CreateTearsheetDivider = forwardRef(
   ({ className, ...rest }, ref) => {
     return <span {...rest} ref={ref} className={cx(blockClass, className)} />;
   }
+);
+
+// Return a placeholder if not released and not enabled by feature flag
+CreateTearsheetDivider = pkg.checkComponentEnabled(
+  CreateTearsheetDivider,
+  componentName
 );
 
 CreateTearsheetDivider.propTypes = {

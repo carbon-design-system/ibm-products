3a238dfe714d6dd74072a568f0192399aa59a297
Lee Chase <lee.chase@uk.ibm.com>
2021-09-21 18:08:31 +0100

    fix: keep page header above content

--- packages/cloud-cognitive/src/components/PageHeader/PageHeader.js
+++ packages/cloud-cognitive/src/components/PageHeader/PageHeader.js
@@ -106,7 +106,9 @@ export let PageHeader = React.forwardRef(
     // handle deprecated props - END
 
     const [metrics, setMetrics] = useState({});
-    const [pageHeaderStyles, setPageHeaderStyles] = useState({});
+    const [pageHeaderStyles, setPageHeaderStyles] = useState({
+      ...rest.style,
+    });
 
     // refs
     const localHeaderRef = useRef(null);
@@ -493,8 +495,7 @@ export let PageHeader = React.forwardRef(
       <>
         <div
           className={`${blockClass}--offset-top-measuring-element`}
-          ref={offsetTopMeasuringRef}
-        ></div>
+          ref={offsetTopMeasuringRef}></div>
         <section
           {...rest}
           className={cx([
@@ -509,15 +510,13 @@ export let PageHeader = React.forwardRef(
           ])}
           style={pageHeaderStyles}
           ref={headerRef}
-          {...getDevtoolsProps(componentName)}
-        >
+          {...getDevtoolsProps(componentName)}>
           <Grid
             fullWidth={fullWidthGrid === true || fullWidthGrid === 'xl'}
             narrow={narrowGrid}
             className={cx({
               [`${blockClass}--width--xl`]: fullWidthGrid === 'xl',
-            })}
-          >
+            })}>
             <div className={`${blockClass}__non-navigation-row-content`}>
               {hasBreadcrumbRow ? (
                 <Row
@@ -528,15 +527,13 @@ export let PageHeader = React.forwardRef(
                       breadcrumbs,
                     [`${blockClass}__breadcrumb-row--has-action-bar`]:
                       hasActionBar,
-                  })}
-                >
+                  })}>
                   <div className={`${blockClass}__breadcrumb-row--container`}>
                     <Column
                       className={cx(`${blockClass}__breadcrumb-column`, {
                         [`${blockClass}__breadcrumb-column--background`]:
                           breadcrumbs !== undefined || hasActionBar,
-                      })}
-                    >
+                      })}>
                       {/* keeps actionBar right even if empty */}
 
                       {breadcrumbs !== undefined ? (
@@ -544,8 +541,7 @@ export let PageHeader = React.forwardRef(
                           className={`${blockClass}__breadcrumb`}
                           noTrailingSlash={title !== undefined}
                           overflowAriaLabel={breadcrumbOverflowAriaLabel}
-                          breadcrumbs={breadcrumbsInWithTitle}
-                        >
+                          breadcrumbs={breadcrumbsInWithTitle}>
                           {!breadcrumbsIn ? deprecated_breadcrumbItems : null}
                           {!breadcrumbsIn && title ? (
                             <BreadcrumbItem
@@ -556,8 +552,7 @@ export let PageHeader = React.forwardRef(
                                   [`${blockClass}__breadcrumb-title--pre-collapsed`]:
                                     collapseTitle,
                                 },
-                              ])}
-                            >
+                              ])}>
                               <span>
                                 {titleLoading ? <SkeletonText /> : titleText}
                               </span>
@@ -575,12 +570,10 @@ export let PageHeader = React.forwardRef(
                           [`${blockClass}__action-bar-column--influenced-by-collapse-button`]:
                             spaceForCollapseButton,
                         },
-                      ])}
-                    >
+                      ])}>
                       <div
                         className={`${blockClass}__action-bar-column-content`}
-                        ref={sizingContainerRef}
-                      >
+                        ref={sizingContainerRef}>
                         {hasActionBar ? (
                           // Investigate the responsive  behaviour or this and the title also fix the ActionBar Item and PageAction story css
                           <>
@@ -589,8 +582,7 @@ export let PageHeader = React.forwardRef(
                                 className={cx(`${blockClass}__page-actions`, {
                                   [`${blockClass}__page-actions--in-breadcrumb`]:
                                     pageActionsInBreadcrumbRow,
-                                })}
-                              >
+                                })}>
                                 <ButtonSetWithOverflow
                                   className={`${blockClass}__button-set--in-breadcrumb`}
                                   onWidthChange={handleButtonSetWidthChange}
@@ -630,16 +622,14 @@ export let PageHeader = React.forwardRef(
                       pageActions !== undefined &&
                       actionBarItems === undefined &&
                       hasBreadcrumbRow,
-                  })}
-                >
+                  })}>
                   <Column className={`${blockClass}__title-column`}>
                     {/* keeps page actions right even if empty */}
                     {title !== undefined ? (
                       <div
                         className={cx(`${blockClass}__title`, {
                           [`${blockClass}__title--fades`]: hasBreadcrumbRow,
-                        })}
-                      >
+                        })}>
                         {TitleIcon && !titleLoading ? (
                           <TitleIcon className={`${blockClass}__title-icon`} />
                         ) : null}
@@ -661,8 +651,7 @@ export let PageHeader = React.forwardRef(
                       className={cx(`${blockClass}__page-actions`, {
                         [`${blockClass}__page-actions--in-breadcrumb`]:
                           pageActionsInBreadcrumbRow,
-                      })}
-                    >
+                      })}>
                       <ButtonSetWithOverflow
                         className={`${blockClass}__page-actions-container`}
                         onWidthChange={handleButtonSetWidthChange}
@@ -706,8 +695,7 @@ export let PageHeader = React.forwardRef(
                       [`${blockClass}__last-row-buffer--active`]:
                         lastRowBufferActive,
                     },
-                  ])}
-                ></div>
+                  ])}></div>
               )}
 
               {
@@ -716,14 +704,12 @@ export let PageHeader = React.forwardRef(
                   <Row
                     className={cx(`${blockClass}__navigation-row`, {
                       [`${blockClass}__navigation-row--has-tags`]: tags,
-                    })}
-                  >
+                    })}>
                     <Column
                       className={cx(`${blockClass}__navigation-tags`, {
                         [`${blockClass}__navigation-tags--tags-only`]:
                           navigation === undefined,
-                      })}
-                    >
+                      })}>
                       <TagSet
                         overflowAlign="end"
                         {...{
@@ -748,8 +734,7 @@ export let PageHeader = React.forwardRef(
                     [`${blockClass}__navigation-row--spacing-above-06`]:
                       navigation !== undefined,
                     [`${blockClass}__navigation-row--has-tags`]: tags,
-                  })}
-                >
+                  })}>
                   <Column className={`${blockClass}__navigation-tabs`}>
                     {navigation}
                   </Column>
@@ -758,8 +743,7 @@ export let PageHeader = React.forwardRef(
                       className={cx(`${blockClass}__navigation-tags`, {
                         [`${blockClass}__navigation-tags--tags-only`]:
                           navigation === undefined,
-                      })}
-                    >
+                      })}>
                       <TagSet
                         overflowAlign="end"
                         {...{

519491cadf560cae6194a234e2232eee713a1689
Matt Gallo <mdgallo@us.ibm.com>
2021-09-24 16:28:59 -0400

    fix(EmptyStates): give svg elements unique ids to avoid display issues

--- packages/cloud-cognitive/src/components/EmptyStates/assets/NotFoundIllustration.js
+++ packages/cloud-cognitive/src/components/EmptyStates/assets/NotFoundIllustration.js
@@ -12,11 +12,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { pkg } from '../../../settings';
+import uuidv4 from '../../../global/js/utils/uuidv4';
 
 // The block part of our conventional BEM class names (blockClass__E--M).
 const blockClass = `${pkg.prefix}--empty-state`;
 
 export const NotFoundIllustration = ({ theme, size, ...rest }) => {
+  const svgId = uuidv4();
   return theme === 'dark' ? (
     <svg
       {...rest}
@@ -31,7 +33,7 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
     >
       <defs>
         <linearGradient
-          id="prefix__a"
+          id={`prefix__a_dark_${svgId}`}
           x1={2.6}
           y1={-12.81}
           x2={43.48}
@@ -44,7 +46,7 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#393939" />
         </linearGradient>
         <linearGradient
-          id="prefix__b"
+          id={`prefix__b_dark_${svgId}`}
           x1={43.74}
           y1={57.07}
           x2={69.51}
@@ -55,7 +57,7 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#393939" />
         </linearGradient>
         <linearGradient
-          id="prefix__c"
+          id={`prefix__c_dark_${svgId}`}
           x1={15.1}
           y1={10.36}
           x2={41.51}
@@ -66,7 +68,7 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#262626" />
         </linearGradient>
         <linearGradient
-          id="prefix__d"
+          id={`prefix__d_dark_${svgId}`}
           x1={34.92}
           y1={-46.56}
           x2={34.92}
@@ -79,7 +81,7 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#262626" stopOpacity={0.1} />
         </linearGradient>
         <linearGradient
-          id="prefix__e"
+          id={`prefix__e_dark_${svgId}`}
           x1={24.56}
           y1={9.61}
           x2={50.36}
@@ -96,7 +98,7 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#6f6f6f" />
         </linearGradient>
         <linearGradient
-          id="prefix__f"
+          id={`prefix__f_dark_${svgId}`}
           x1={15.86}
           y1={-20.5}
           x2={56.75}
@@ -112,15 +114,15 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
       <path fill="none" d="M0 0h80v80H0z" />
       <path
         d="M12.69 18c0 12 8.75 26.84 19.56 33.08 5.64 3.26 10.73 3.52 14.3 1.32l4.83-2.79-1.19-1.93c-3.25 1.73-7.72 1.38-12.67-1.48-9.9-5.74-17.92-19.32-17.92-30.35 0-5.09 1.71-8.75 4.53-10.65l-1.2-1.95L18.1 6c-3.35 2-5.41 6.12-5.41 12"
-        fill="url(#prefix__a)"
+        fill={`url(#prefix__a_dark_${svgId})`}
       />
       <path
         d="M67.31 64.92s.11.86-1.89 2.24-2.73 1.18-2.73 1.18L48.57 49.22l4.62-3.42z"
-        fill="url(#prefix__b)"
+        fill={`url(#prefix__b_dark_${svgId})`}
       />
       <path
         d="M37.68 46.05c-9.9-5.71-17.93-19.29-17.93-30.32 0-3.79.95-6.78 2.6-8.85-4.87.76-8.09 5-8.09 12 0 11 8 24.61 17.93 30.32 6.5 3.75 12.19 3.19 15.33-.76-2.82.44-6.21-.31-9.84-2.41"
-        fill="url(#prefix__c)"
+        fill={`url(#prefix__c_dark_${svgId})`}
       />
       <ellipse
         cx={34.92}
@@ -129,7 +131,7 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
         ry={25.2}
         transform="rotate(-30 34.93 27.58)"
         opacity={0.5}
-        fill="url(#prefix__d)"
+        fill={`url(#prefix__d_dark_${svgId})`}
       />
       <path
         d="M67.1 75l-12.84-7.41c1.1-1.15 1.1-2.36-.77-3.44h-.05L26.17 48.4l-.09-.06c-2.59-1.49-5.82-1.05-8.42.45s-3.57 3.48-1 5l27.43 15.79a7.2 7.2 0 005.5.53l13 7.51c1.23.72 5.76-1.89 4.51-2.62z"
@@ -137,12 +139,12 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
       />
       <path
         d="M42.88 50.79a16.42 16.42 0 01-8.06-2.47c-9.88-5.7-17.92-19.63-17.92-31 0-5.57 1.89-9.73 5.32-11.72S30.2 4 35 6.81c9.91 5.71 18 19.64 18 31.05 0 5.57-1.89 9.73-5.32 11.71a9.31 9.31 0 01-4.8 1.22zM27 4.76a9 9 0 00-4.54 1.17c-3.3 1.9-5.12 5.93-5.12 11.35 0 11.28 8 25 17.72 30.68 4.69 2.71 9.09 3.16 12.39 1.25s5.12-5.94 5.12-11.35c0-11.28-7.95-25-17.72-30.69A16 16 0 0027 4.76z"
-        fill="url(#prefix__e)"
+        fill={`url(#prefix__e_dark_${svgId})`}
         opacity={0.6}
       />
       <path
         d="M57 37.5c0-12-8.75-26.85-19.56-33.08C31.82 1.16 26.73.89 23.16 3.09l-4.83 2.78 1.19 1.94c3.25-1.74 7.72-1.38 12.67 1.47C42.09 15 50.11 28.57 50.11 39.6c0 4.86-1.55 8.4-4.11 10.4-.12.1-1.17.73-1.31.82l2.12 1.42 4.83-2.79C55 47.44 57 43.34 57 37.5"
-        fill="url(#prefix__f)"
+        fill={`url(#prefix__f_dark_${svgId})`}
       />
     </svg>
   ) : (
@@ -160,7 +162,7 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
     >
       <defs>
         <linearGradient
-          id="prefix__a_dark"
+          id={`prefix__a_${svgId}`}
           x1={2.6}
           y1={-12.81}
           x2={43.48}
@@ -173,7 +175,7 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#a8a8a8" />
         </linearGradient>
         <linearGradient
-          id="prefix__b_dark"
+          id={`prefix__b_${svgId}`}
           x1={48.57}
           y1={57.07}
           x2={67.31}
@@ -184,7 +186,7 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#c6c6c6" />
         </linearGradient>
         <linearGradient
-          id="prefix__c_dark"
+          id={`prefix__c_${svgId}`}
           x1={15.1}
           y1={10.36}
           x2={41.51}
@@ -195,7 +197,7 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#a3a3a3" />
         </linearGradient>
         <linearGradient
-          id="prefix__d_dark"
+          id={`prefix__d_${svgId}`}
           x1={34.92}
           y1={-46.56}
           x2={34.92}
@@ -209,7 +211,7 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#8c8c8c" stopOpacity={0.1} />
         </linearGradient>
         <linearGradient
-          id="prefix__e_dark"
+          id={`prefix__e_${svgId}`}
           x1={26.4}
           y1={71.67}
           x2={55.54}
@@ -220,7 +222,7 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#171717" stopOpacity={0.1} />
         </linearGradient>
         <linearGradient
-          id="prefix__f_dark"
+          id={`prefix__f_${svgId}`}
           x1={24.56}
           y1={9.61}
           x2={50.36}
@@ -240,26 +242,26 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#fff" />
         </linearGradient>
         <linearGradient
-          id="prefix__g_dark"
+          id={`prefix__g_${svgId}`}
           x1={15.86}
           y1={-20.5}
           x2={56.75}
           y2={50.32}
-          xlinkHref="#prefix__a_dark"
+          xlinkHref={`#prefix__a_${svgId}`}
         />
       </defs>
       <path fill="none" d="M0 0h80v80H0z" />
       <path
         d="M12.69 18c0 12 8.75 26.84 19.56 33.08 5.64 3.26 10.73 3.52 14.3 1.32l4.83-2.79-1.19-1.93c-3.25 1.73-7.72 1.38-12.67-1.48-9.9-5.74-17.92-19.32-17.92-30.35 0-5.09 1.71-8.75 4.53-10.65l-1.2-1.95L18.1 6c-3.35 2-5.41 6.12-5.41 12"
-        fill="url(#prefix__a_dark)"
+        fill={`url(#prefix__a_${svgId})`}
       />
       <path
         d="M67.31 64.92s.11.86-1.89 2.24-2.73 1.18-2.73 1.18L48.57 49.22l4.62-3.42z"
-        fill="url(#prefix__b_dark)"
+        fill={`url(#prefix__b_${svgId})`}
       />
       <path
         d="M37.68 46.05c-9.9-5.71-17.93-19.29-17.93-30.32 0-3.79.95-6.78 2.6-8.85-4.87.76-8.09 5-8.09 12 0 11 8 24.61 17.93 30.32 6.5 3.75 12.19 3.19 15.33-.76-2.82.44-6.21-.31-9.84-2.41"
-        fill="url(#prefix__c_dark)"
+        fill={`url(#prefix__c_${svgId})`}
       />
       <ellipse
         cx={34.92}
@@ -268,20 +270,20 @@ export const NotFoundIllustration = ({ theme, size, ...rest }) => {
         ry={25.2}
         transform="rotate(-30 34.93 27.58)"
         opacity={0.5}
-        fill="url(#prefix__d_dark)"
+        fill={`url(#prefix__d_${svgId})`}
       />
       <path
         d="M67.1 75l-12.84-7.41c1.1-1.15 1.1-2.36-.77-3.44h-.05L26.17 48.4l-.09-.06c-2.59-1.49-5.82-1.05-8.42.45s-3.57 3.48-1 5l27.43 15.79a7.2 7.2 0 005.5.53l13 7.51c1.23.72 5.76-1.89 4.51-2.62z"
-        fill="url(#prefix__e_dark)"
+        fill={`url(#prefix__e_${svgId})`}
       />
       <path
         d="M42.88 50.79a16.42 16.42 0 01-8.06-2.47c-9.88-5.7-17.92-19.63-17.92-31 0-5.57 1.89-9.73 5.32-11.72S30.2 4 35 6.81c9.91 5.71 18 19.64 18 31.05 0 5.57-1.89 9.73-5.32 11.71a9.31 9.31 0 01-4.8 1.22zM27 4.76a9 9 0 00-4.54 1.17c-3.3 1.9-5.12 5.93-5.12 11.35 0 11.28 8 25 17.72 30.68 4.69 2.71 9.09 3.16 12.39 1.25s5.12-5.94 5.12-11.35c0-11.28-7.95-25-17.72-30.69A16 16 0 0027 4.76z"
-        fill="url(#prefix__f_dark)"
+        fill={`url(#prefix__f_${svgId})`}
         opacity={0.6}
       />
       <path
         d="M57 37.5c0-12-8.75-26.85-19.56-33.08C31.82 1.16 26.73.89 23.16 3.09l-4.83 2.78 1.19 1.94c3.25-1.74 7.72-1.38 12.67 1.47C42.09 15 50.11 28.57 50.11 39.6c0 4.86-1.55 8.4-4.11 10.4-.12.1-1.17.73-1.31.82l2.12 1.42 4.83-2.79C55 47.44 57 43.34 57 37.5"
-        fill="url(#prefix__g_dark)"
+        fill={`url(#prefix__g_${svgId})`}
       />
     </svg>
   );
--- packages/cloud-cognitive/src/components/EmptyStates/assets/UnauthorizedIllustration.js
+++ packages/cloud-cognitive/src/components/EmptyStates/assets/UnauthorizedIllustration.js
@@ -12,11 +12,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { pkg } from '../../../settings';
+import uuidv4 from '../../../global/js/utils/uuidv4';
 
 // The block part of our conventional BEM class names (blockClass__E--M).
 const blockClass = `${pkg.prefix}--empty-state`;
 
 export const UnauthorizedIllustration = ({ theme, size, ...rest }) => {
+  const svgId = uuidv4();
   return theme === 'dark' ? (
     <svg
       {...rest}
@@ -32,7 +34,7 @@ export const UnauthorizedIllustration = ({ theme, size, ...rest }) => {
     >
       <defs>
         <linearGradient
-          id="prefix__b"
+          id={`prefix__b_dark_${svgId}`}
           x1={17.33}
           y1={40.68}
           x2={53.57}
@@ -43,7 +45,7 @@ export const UnauthorizedIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#393939" />
         </linearGradient>
         <linearGradient
-          id="prefix__a"
+          id={`prefix__a_dark_${svgId}`}
           x1={37.21}
           y1={61.49}
           x2={71.41}
@@ -54,15 +56,15 @@ export const UnauthorizedIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#393939" />
         </linearGradient>
         <linearGradient
-          id="prefix__c"
+          id={`prefix__c_dark_${svgId}`}
           x1={39.97}
           y1={32.38}
           x2={39.97}
           y2={1.64}
-          xlinkHref="#prefix__a"
+          xlinkHref={`#prefix__a_dark_${svgId}`}
         />
         <linearGradient
-          id="prefix__d"
+          id={`prefix__d_dark_${svgId}`}
           x1={24.58}
           y1={44.68}
           x2={51.62}
@@ -73,7 +75,7 @@ export const UnauthorizedIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#262626" />
         </linearGradient>
         <linearGradient
-          id="prefix__e"
+          id={`prefix__e_dark_${svgId}`}
           x1={32.72}
           y1={45.46}
           x2={38.82}
@@ -91,23 +93,23 @@ export const UnauthorizedIllustration = ({ theme, size, ...rest }) => {
       />
       <path
         d="M61.16 32.9a1.44 1.44 0 00-.5-.51l-1.1-.64-28.48-16.44a.52.52 0 00-.5-.06l-10.11 5.83c-.11.07 2.16 28.77 2.16 28.77l4.3 2.79L61.26 33.1a1.24 1.24 0 00-.1-.2z"
-        fill="url(#prefix__b)"
+        fill={`url(#prefix__b_dark_${svgId})`}
       />
       <path
         d="M61.16 62.45a.5.5 0 00.23-.48V33.64a1.38 1.38 0 00-.13-.54L26.93 52.64l24.12 15.64z"
-        fill="url(#prefix__a)"
+        fill={`url(#prefix__a_dark_${svgId})`}
       />
       <path
         d="M46.88 31.4a.85.85 0 00.5.69 2.69 2.69 0 002.41 0 .85.85 0 00.49-.7V18.27c0-5.39-4.15-12-9.46-15.07-3-1.76-6-2-8.22-.78-1.9 1.09-3 3.19-3 5.89V21.5a.85.85 0 00.5.7 2.67 2.67 0 002.4 0 .87.87 0 00.5-.7V8.31a3.29 3.29 0 011.3-2.95c1.08-.62 2.88-.33 4.82.79 4.21 2.43 7.76 8 7.76 12.12z"
-        fill="url(#prefix__c)"
+        fill={`url(#prefix__c_dark_${svgId})`}
       />
       <path
         d="M50.56 38.22l-1.1-.63L21 21.14a.44.44 0 00-.72.42v28.33a1.49 1.49 0 00.23.74 1.42 1.42 0 00.49.51l28.46 16.45 1.1.63a.5.5 0 00.49.06.49.49 0 00.23-.47V39.47a1.61 1.61 0 00-.72-1.25z"
-        fill="url(#prefix__d)"
+        fill={`url(#prefix__d_dark_${svgId})`}
       />
       <path
         d="M38 41.63a5 5 0 00-2.25-3.9c-1.25-.72-2.26-.13-2.26 1.3a4.73 4.73 0 00.84 2.5l-.84 5.31 4.51 2.6-.84-6.27a1.48 1.48 0 00.84-1.54z"
-        fill="url(#prefix__e)"
+        fill={`url(#prefix__e_dark_${svgId})`}
       />
       <path
         d="M51.41 38.51a1.9 1.9 0 00-.64-.65l-1.1-.63-28.49-16.45-.1-.05-.61.35a.33.33 0 01.17 0 .6.6 0 01.32.1l28.5 16.41 1.1.63a1.5 1.5 0 01.49.51s.05.09.08.14l.36-.21z"
@@ -129,7 +131,7 @@ export const UnauthorizedIllustration = ({ theme, size, ...rest }) => {
     >
       <defs>
         <linearGradient
-          id="prefix__b_dark"
+          id="prefix__b"
           x1={27.98}
           y1={73.72}
           x2={53.65}
@@ -140,7 +142,7 @@ export const UnauthorizedIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopOpacity={0.1} />
         </linearGradient>
         <linearGradient
-          id="prefix__c_dark"
+          id={`prefix__c_${svgId}`}
           x1={17.33}
           y1={40.68}
           x2={53.57}
@@ -152,7 +154,7 @@ export const UnauthorizedIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#c6c6c6" />
         </linearGradient>
         <linearGradient
-          id="prefix__d_dark"
+          id={`prefix__d_${svgId}`}
           x1={28.59}
           y1={16.01}
           x2={58.88}
@@ -168,7 +170,7 @@ export const UnauthorizedIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#8d8d8d" />
         </linearGradient>
         <linearGradient
-          id="prefix__a_dark"
+          id={`prefix__a_${svgId}`}
           x1={26.93}
           y1={50.69}
           x2={61.39}
@@ -179,15 +181,15 @@ export const UnauthorizedIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#c6c6c6" />
         </linearGradient>
         <linearGradient
-          id="prefix__e_dark"
+          id={`prefix__e_${svgId}`}
           x1={39.97}
           y1={32.38}
           x2={39.97}
           y2={1.64}
-          xlinkHref="#prefix__a_dark"
+          xlinkHref={`#prefix__a_${svgId}`}
         />
         <linearGradient
-          id="prefix__f_dark"
+          id={`prefix__f_${svgId}`}
           x1={24.58}
           y1={44.68}
           x2={51.62}
@@ -198,7 +200,7 @@ export const UnauthorizedIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#c6c6c6" />
         </linearGradient>
         <linearGradient
-          id="prefix__g_dark"
+          id={`prefix__g_${svgId}`}
           x1={32.72}
           y1={45.46}
           x2={38.82}
@@ -209,7 +211,7 @@ export const UnauthorizedIllustration = ({ theme, size, ...rest }) => {
           <stop offset={1} stopColor="#8d8d8d" />
         </linearGradient>
         <linearGradient
-          id="prefix__h_dark"
+          id={`prefix__h_${svgId}`}
           x1={20.61}
           y1={20.85}
           x2={52.7}
@@ -231,36 +233,36 @@ export const UnauthorizedIllustration = ({ theme, size, ...rest }) => {
       </defs>
       <path fill="none" d="M0 0h80v80H0z" />
       <path
-        fill="url(#prefix__b_dark)"
+        fill={`url(#prefix__b_${svgId})`}
         d="M20.28 60.39l10.27-5.94 30.8 17.79-10.26 5.93-30.81-17.78z"
       />
       <path
         d="M61.16 32.9a1.44 1.44 0 00-.5-.51l-1.1-.64-28.48-16.44a.52.52 0 00-.5-.06l-10.11 5.83c-.11.07 2.16 28.77 2.16 28.77l4.3 2.79L61.26 33.1a1.24 1.24 0 00-.1-.2z"
-        fill="url(#prefix__c_dark)"
+        fill={`url(#prefix__c_${svgId})`}
       />
       <path
         d="M61.16 62.45a.5.5 0 00.23-.48V33.64a1.38 1.38 0 00-.13-.54L26.93 52.64l24.12 15.64z"
-        fill="url(#prefix__d_dark)"
+        fill={`url(#prefix__d_${svgId})`}
       />
       <path
         d="M61.16 62.45a.5.5 0 00.23-.48V33.64a1.38 1.38 0 00-.13-.54L26.93 52.64l24.12 15.64z"
-        fill="url(#prefix__a_dark)"
+        fill={`url(#prefix__a_${svgId})`}
       />
       <path
         d="M46.88 31.4a.85.85 0 00.5.69 2.69 2.69 0 002.41 0 .85.85 0 00.49-.7V18.27c0-5.39-4.15-12-9.46-15.07-3-1.76-6-2-8.22-.78-1.9 1.09-3 3.19-3 5.89V21.5a.85.85 0 00.5.7 2.67 2.67 0 002.4 0 .87.87 0 00.5-.7V8.31a3.29 3.29 0 011.3-2.95c1.08-.62 2.88-.33 4.82.79 4.21 2.43 7.76 8 7.76 12.12z"
-        fill="url(#prefix__e_dark)"
+        fill={`url(#prefix__e_${svgId})`}
       />
       <path
         d="M50.56 38.22l-1.1-.63L21 21.14a.44.44 0 00-.72.42v28.33a1.49 1.49 0 00.23.74 1.42 1.42 0 00.49.51l28.46 16.45 1.1.63a.5.5 0 00.49.06.49.49 0 00.23-.47V39.47a1.61 1.61 0 00-.72-1.25z"
-        fill="url(#prefix__f_dark)"
+        fill={`url(#prefix__f_${svgId})`}
       />
       <path
         d="M38 41.63a5 5 0 00-2.25-3.9c-1.25-.72-2.26-.13-2.26 1.3a4.73 4.73 0 00.84 2.5l-.84 5.31 4.51 2.6-.84-6.27a1.48 1.48 0 00.84-1.54z"
-        fill="url(#prefix__g_dark)"
+        fill={`url(#prefix__g_${svgId})`}
       />
       <path
         d="M51.41 38.51a1.9 1.9 0 00-.64-.65l-1.1-.63-28.49-16.45-.1-.05-.61.35a.33.33 0 01.17 0 .6.6 0 01.32.1l28.5 16.41 1.1.63a1.5 1.5 0 01.49.51s.05.09.08.14l.36-.21z"
-        fill="url(#prefix__h_dark)"
+        fill={`url(#prefix__h_${svgId})`}
       />
     </svg>
   );

47c640ab05c6fa3dd9374b9933e2fa06930e9fbe
Lee Chase <lee.chase@uk.ibm.com>
2021-09-28 14:32:35 +0100

    fix: correct formatting of generated file

--- examples/cloud-cognitive/example-gallery/src/gallery-config/index.js
+++ examples/cloud-cognitive/example-gallery/src/gallery-config/index.js
@@ -1,1 +1,25 @@
-const config = [{ label: 'React 16 usage', url: 'https://codesandbox.io/s/github/carbon-design-system/ibm-cloud-cognitive/tree/main/examples/cloud-cognitive/ccs-base-react-16', thumbnail: 'url( ' + require('./ccs-base-react-16--thumbnail.png').default + ')' },{ label: 'React 17 usage', url: 'https://codesandbox.io/s/github/carbon-design-system/ibm-cloud-cognitive/tree/main/examples/cloud-cognitive/ccs-base-react-17', thumbnail: 'url( ' + require('./ccs-base-react-17--thumbnail.png').default + ')' },{ label: 'codesandbox', url: 'https://codesandbox.io/s/github/carbon-design-system/ibm-cloud-cognitive/tree/main/examples/cloud-cognitive/codesandbox', thumbnail: 'url( ' + require('./codesandbox--thumbnail.png').default + ')' },{ label: 'PageHeader example', url: 'https://codesandbox.io/s/pageheaderexample-rs5ie', thumbnail: 'url("https://screenshots.codesandbox.io/rs5ie/210.png")' },]; export default config;
+const config = [
+  {
+    label: 'React 16 usage',
+    url: 'https://codesandbox.io/s/github/carbon-design-system/ibm-cloud-cognitive/tree/main/examples/cloud-cognitive/ccs-base-react-16',
+    thumbnail:
+      'url( ' + require('./ccs-base-react-16--thumbnail.png').default + ')',
+  },
+  {
+    label: 'React 17 usage',
+    url: 'https://codesandbox.io/s/github/carbon-design-system/ibm-cloud-cognitive/tree/main/examples/cloud-cognitive/ccs-base-react-17',
+    thumbnail:
+      'url( ' + require('./ccs-base-react-17--thumbnail.png').default + ')',
+  },
+  {
+    label: 'codesandbox',
+    url: 'https://codesandbox.io/s/github/carbon-design-system/ibm-cloud-cognitive/tree/main/examples/cloud-cognitive/codesandbox',
+    thumbnail: 'url( ' + require('./codesandbox--thumbnail.png').default + ')',
+  },
+  {
+    label: 'PageHeader example',
+    url: 'https://codesandbox.io/s/pageheaderexample-rs5ie',
+    thumbnail: 'url("https://screenshots.codesandbox.io/rs5ie/210.png")',
+  },
+];
+export default config;

98752cb764956aaced250b4ff96d91a74b6b9d48
Simon Finney <SIMONFIN@ie.ibm.com>
2021-10-01 11:08:07 +0100

    refactor: add custom devtools matcher

--- packages/cloud-cognitive/src/components/UserProfileImage/UserProfileImage.test.js
+++ packages/cloud-cognitive/src/components/UserProfileImage/UserProfileImage.test.js
@@ -13,9 +13,7 @@ import { pkg, carbon } from '../../settings';
 
 import { UserProfileImage } from '.';
 
-const { devtoolsAttribute, getDevtoolsId, prefix } = pkg;
-
-const blockClass = `${prefix}-user-profile-avatar`;
+const blockClass = `${pkg.prefix}-user-profile-avatar`;
 const dataTestId = uuidv4();
 const kind = 'user';
 const size = 'xlg';
@@ -82,9 +80,8 @@ describe(name, () => {
   it('adds the Devtools attribute to the containing node', () => {
     renderComponent({ 'data-testid': dataTestId });
 
-    expect(screen.getByTestId(dataTestId)).toHaveAttribute(
-      devtoolsAttribute,
-      getDevtoolsId(UserProfileImage.displayName)
+    expect(screen.getByTestId(dataTestId)).toHaveDevtoolsAttribute(
+      UserProfileImage.displayName
     );
   });
 

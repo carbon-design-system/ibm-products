86c28001dca85ec519b7adbcde0fe76f43ca1568
Dave Clark <dave.clark@uk.ibm.com>
2021-10-16 20:19:06 +0100

    chore: test using sass CLI for the styles snapshot test

---
+++ packages/cloud-cognitive/src/__tests__/styles3.test.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright IBM Corp. 2021, 2021
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @jest-environment node
+ */
+
+const { renderSync } = require('sass');
+const { resolve } = require('path');
+const { execSync } = require('child_process');
+
+const name = 'CSS export checks';
+
+const r = (file) => resolve(__dirname, file);
+const includePath1 = r('../../node_modules');
+const includePath2 = r('../../../../node_modules');
+const includePaths = [includePath1, includePath2];
+const scssReleasedOnly = r('../index-without-carbon-released-only.scss');
+const scssAll = r('../index.scss');
+const scssSettings = r('../global/styles/_project-settings.scss');
+
+describe(name, () => {
+  // This test will fail if the generated CSS changes and no longer matches
+  // the snapshot. If a change to the exported CSS for released components is
+  // intended, re-run the tests with -u to update the snapshot, and check the
+  // fresh snapshot in as part of the PR.
+  it("doesn't change the exported CSS for released components", () => {
+    expect(
+      execSync(
+        `sass --style=expanded --load-path ${includePath1} --load-path ${includePath2} ${scssReleasedOnly}`
+      ).toString()
+    ).toMatchSnapshot();
+  });
+
+  // This test will fail if any generated CSS changes.
+  it.skip("doesn't change any of the generated CSS", () => {
+    expect(
+      renderSync({
+        file: scssAll,
+        includePaths,
+        outputStyle: 'expanded',
+      }).css.toString()
+    ).toMatchSnapshot();
+  });
+
+  // This test will fail if the project settings generates any CSS, as this
+  // file should be including definitions and variables only.
+  it("doesn't generate any CSS from the project settings", () => {
+    expect(
+      renderSync({
+        file: scssSettings,
+        includePaths,
+        outputStyle: 'compressed',
+      }).css.toString()
+    ).toHaveLength(0);
+  });
+});

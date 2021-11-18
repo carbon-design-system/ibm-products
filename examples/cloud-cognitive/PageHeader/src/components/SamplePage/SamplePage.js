5104c2266a332c62c91d26a80bb1a7119c457e58
Lee Chase <leechase@live.com>
2021-11-18 19:40:51 +0000

    fix: page header example prop usage (#1463)
    Co-authored-by: Dave Clark <dave.clark@uk.ibm.com>

--- examples/cloud-cognitive/PageHeader/src/components/SamplePage/SamplePage.js
+++ examples/cloud-cognitive/PageHeader/src/components/SamplePage/SamplePage.js
@@ -1,18 +1,12 @@
 import React, { useRef } from 'react';
 import { PageHeader } from '@carbon/ibm-cloud-cognitive';
-import {
-  BreadcrumbItem,
-  Tabs,
-  Tab,
-  Grid,
-  Row,
-  Column,
-} from 'carbon-components-react';
+import { Tabs, Tab, Grid, Row, Column } from 'carbon-components-react';
 import { Lightning16, Bee24 } from '@carbon/icons-react';
 
 import './_sample-page.scss';
 
 const actionBarItems = [1, 2, 3, 4].map((item, index) => ({
+  key: `Action ${index}`,
   renderIcon: index % 2 ? Lightning16 : Bee24,
   iconDescription: `Action ${item}`,
   onClick: () => {
@@ -20,22 +14,23 @@ const actionBarItems = [1, 2, 3, 4].map((item, index) => ({
   },
 }));
 
-const breadcrumbItems = (
-  <>
-    <BreadcrumbItem href="#">Breadcrumb 1</BreadcrumbItem>
-    <BreadcrumbItem href="#">Breadcrumb 2</BreadcrumbItem>
-    <BreadcrumbItem href="#">Breadcrumb 3</BreadcrumbItem>
-    <BreadcrumbItem href="#">Breadcrumb 4</BreadcrumbItem>
-    <BreadcrumbItem href="#">Breadcrumb 5</BreadcrumbItem>
-  </>
+const makeBreadcrumb = (item, title) => ({
+  href: '#',
+  key: `Breadcrumb ${item}`,
+  label: typeof title === 'string' ? title : `Breadcrumb ${item}`,
+});
+const breadcrumbs = Array.from({ length: 5 }, (_, index) =>
+  makeBreadcrumb(index + 1, `Breadcrumb ${index + 1}`)
 );
 
 const pageActions = [
   {
+    key: 'secondary',
     kind: 'secondary',
     label: 'Secondary button',
   },
   {
+    key: 'primary',
     kind: 'primary',
     label: 'Primary button',
   },
@@ -84,47 +79,42 @@ const dummyPageContent = (
   </Grid>
 );
 
-const availableSpace = (
-  <div style={{ display: 'flex' }}>
-    <p style={{ marginRight: '50px', maxWidth: '400px' }}>
-      {/* cspell:disable */}
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor <strong>incididunt ut labore</strong> et dolore magna aliqua. Ut
-      enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-      aliquip ex ea commodo consequat.
-      {/* cspell:enable */}
-    </p>
-    <p>
-      Property: Value
-      <br />
-      Property: Value
-      <br />
-      Property: Value
-    </p>
-  </div>
-);
-
 export const SamplePage = () => {
   const refMain = useRef(null);
   return (
     <>
       <PageHeader
-        availableSpace={availableSpace}
-        background
-        breadcrumbItems={breadcrumbItems}
-        actionBarItems={actionBarItems}
         title="Page title"
-        pageActions={pageActions}
         pageActionsOverflowLabel="Page actions..."
         subtitle="Page sub-title"
         navigation={tabs}
-        scrollTarget={refMain}
-        tags={tags}
         showAllTagsLabel="Show all tags"
         allTagsModalTitle="Filter tags"
         allTagsModalSearchPlaceholderText="Filter tags"
         allTagsModalSearchLabel="Filter tags"
-      />
+        actionBarOverflowAriaLabel="more actions"
+        breadcrumbOverflowAriaLabel="more breadcrumbs"
+        {...{ breadcrumbs, actionBarItems, pageActions, tags }}
+      >
+        {/* some additional content displayed in the page header --> */}
+        <div style={{ display: 'flex' }}>
+          <p style={{ marginRight: '50px', maxWidth: '400px' }}>
+            {/* cspell:disable */}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor <strong>incididunt ut labore</strong> et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat.
+            {/* cspell:enable */}
+          </p>
+          <p>
+            Property: Value
+            <br />
+            Property: Value
+            <br />
+            Property: Value
+          </p>
+        </div>
+      </PageHeader>
       <main className="sample-page__main" ref={refMain}>
         {dummyPageContent}
       </main>

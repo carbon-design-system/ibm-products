3c613539ce6334be13dbf2b5d09b5f7b73f87a20
David Menendez <damenend@gmail.com>
2022-01-12 15:56:40 -0600

    feat: add select component init (#1523)
    * feat: add select component init
    
    * fix: remove test code
    
    * fix: ci fixes
    
    * fix: ci fixes
    
    * fix: ci fix
    
    Co-authored-by: David Menendez <dmenend@us.ibm.com>

--- packages/core/story-structure.js
+++ packages/core/story-structure.js
@@ -32,6 +32,7 @@ const s = [
       {
         n: 'Patterns',
         s: [
+          { n: 'AddSelect', s: ['c/AddSelect'] },
           { n: 'About modal', s: ['c/AboutModal'] },
           {
             n: 'Create flows',

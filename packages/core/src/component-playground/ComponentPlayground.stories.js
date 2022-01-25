5b63de7f50f704e48a1722bbd110a54f1ca5561b
Dave Clark <dave.clark@uk.ibm.com>
2022-01-25 14:00:38 +0000

    chore: add component playground to storybook (#1556)
    * chore: add component playground to storybook
    
    * chore: remove unneeded scss files
    
    * chore: update component playground
    
    Co-authored-by: Matt Gallo <mdgallo@us.ibm.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/core/src/component-playground/ComponentPlayground.stories.js
+++ packages/core/src/component-playground/ComponentPlayground.stories.js
@@ -1,24 +1,23 @@
 /**
- * Copyright IBM Corp. 2020, 2021
+ * Copyright IBM Corp. 2022, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
 import React from 'react';
-import { getPathForComponent } from '../../../../packages/core/story-structure';
-
-import App from './App';
-
+import { getPathForComponent } from '../../story-structure';
 import styles from './_storybook-styles.scss';
+
+import App from './ComponentPlayground';
 
 export default {
   title: getPathForComponent('o', 'Examples'),
   parameters: {
     styles,
+    layout: 'fullscreen',
   },
 };
 
-export const c4pGallery = () => <App />;
-c4pGallery.storyName = 'on CodeSandbox';
-c4pGallery.parameters = { options: { showPanel: false } };
+export const playground = () => <App />;
+playground.storyName = 'Component playground';
---
+++ packages/core/src/component-playground/components/CreateTearsheet/index.js
@@ -0,0 +1,2 @@
+import MultiStepTearsheetWide from './CreateTearsheetWide';
+export { MultiStepTearsheetWide };

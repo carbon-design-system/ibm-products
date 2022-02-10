f47f26116e5a694c8db8df214eab72c0a5230921
Elysia Hwang <Elysia.Hwang@ibm.com>
2022-02-10 12:58:20 -0500

    chore(EmptyState): remove isRequired from variations
    Contributes to #1605

--- packages/cloud-cognitive/src/components/EmptyStates/ErrorEmptyState/ErrorEmptyState.js
+++ packages/cloud-cognitive/src/components/EmptyStates/ErrorEmptyState/ErrorEmptyState.js
@@ -113,7 +113,7 @@ ErrorEmptyState.propTypes = {
   /**
    * Empty state subtitle
    */
-  subtitle: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+  subtitle: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
 
   /**
    * Empty state title

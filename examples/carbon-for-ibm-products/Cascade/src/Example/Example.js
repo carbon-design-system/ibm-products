34f7599257a7f2d86b5f24b741f93272c84369a5
Dave Clark <dave.clark@uk.ibm.com>
2022-02-21 02:19:47 +0000

    chore: update Cascade example

---
+++ examples/carbon-for-ibm-products/Cascade/src/Example/Example.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// ----------------------------------------------------
+// This is an example showing use of the
+// Cascade component
+// from the @carbon/ibm-products component library.
+// ----------------------------------------------------
+
+import { Cascade } from '@carbon/ibm-products';
+
+import './_example.scss';
+
+export const Example = () => (
+  <Cascade style={{'max-width': '728px'}}>
+    <div className="box" />
+    <div className="box" />
+    <div className="box" />
+    <div className="box" />
+    <div className="box" />
+    <div className="box" />
+    <div className="box" />
+    <div className="box" />
+  </Cascade>
+);

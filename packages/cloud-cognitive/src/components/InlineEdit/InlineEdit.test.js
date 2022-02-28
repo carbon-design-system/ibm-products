757dff46c9c8b8889e204303590ee4ea15e69edb
Lee Chase <lee.chase@uk.ibm.com>
2022-02-28 12:07:46 +0000

    chore: inline edit complete test coverage (#1688)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/cloud-cognitive/src/components/InlineEdit/InlineEdit.test.js
+++ packages/cloud-cognitive/src/components/InlineEdit/InlineEdit.test.js
@@ -83,11 +83,17 @@ describe(componentName, () => {
       <InlineEdit {...requiredProps} value={startingValue} />
     );
 
+    // do after render
+    jest.useFakeTimers();
+    jest.spyOn(global, 'setTimeout');
+
     const input = screen.getByRole('textbox');
     // clicks in
     userEvent.click(input);
     const component = container.querySelector(`.${blockClass}`);
     expect(component).toHaveClass(`${blockClass}--editing`);
+
+    expect(setTimeout).not.toHaveBeenCalled();
 
     // tabs out
     userEvent.tab({ shift: true });
@@ -104,6 +110,28 @@ describe(componentName, () => {
     // testing {ctrl}z does not seem to work
     // userEvent.type(input, '{ctrl}z');
     // expect(input).toHaveTextContent(startingValue);
+    jest.spyOn(global, 'setTimeout').mockRestore();
+  });
+
+  it('can start and stop edit with enter', () => {
+    const startingValue = value;
+    const { container } = render(
+      <InlineEdit {...requiredProps} value={startingValue} />
+    );
+
+    const input = screen.getByRole('textbox');
+    // clicks in
+    userEvent.click(input);
+    const component = container.querySelector(`.${blockClass}`);
+    expect(component).toHaveClass(`${blockClass}--editing`);
+
+    // tabs in
+    userEvent.type(input, 'ABC');
+    expect(input).toHaveTextContent(`${startingValue}ABC`);
+
+    userEvent.type(input, '{enter}');
+
+    expect(component).not.toHaveClass(`${blockClass}--editing`);
   });
 
   it('can start and cancel an edit using buttons', () => {
@@ -113,12 +141,20 @@ describe(componentName, () => {
     );
     const component = container.querySelector(`.${blockClass}`);
 
+    // do after render
+    jest.useFakeTimers();
+    jest.spyOn(global, 'setTimeout');
+
     const input = screen.getByRole('textbox');
 
     // starts edit
     const edit = container.querySelector(`.${blockClass}__edit`);
     userEvent.click(edit);
     expect(component).toHaveClass(`${blockClass}--editing`);
+
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+    jest.runOnlyPendingTimers();
+    expect(input.scrollLeft).toEqual(9999); // scroll moved to end
 
     // has edit buttons
     const controls = screen.getAllByRole('button');
@@ -128,6 +164,32 @@ describe(componentName, () => {
     userEvent.click(controls[0]);
     expect(input).toHaveTextContent(startingValue);
     expect(component).not.toHaveClass(`${blockClass}--editing`);
+    expect(input.scrollLeft).toEqual(0); // scroll moved to start
+
+    jest.spyOn(global, 'setTimeout').mockRestore();
+  });
+
+  it('can start edit clicking left on the component (left of input)', () => {
+    const startingValue = value;
+    const { container } = render(
+      <InlineEdit {...requiredProps} value={startingValue} />
+    );
+    const input = screen.getByRole('textbox');
+    const component = container.querySelector(`.${blockClass}`);
+
+    // do after render
+    jest.useFakeTimers();
+    jest.spyOn(global, 'setTimeout');
+
+    // starts edit
+    userEvent.click(component);
+    expect(component).toHaveClass(`${blockClass}--editing`);
+
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+    jest.runOnlyPendingTimers();
+    expect(input.scrollLeft).toEqual(0); // scroll moved to end
+
+    jest.spyOn(global, 'setTimeout').mockRestore();
   });
 
   it('can start and save edit with buttons', () => {
@@ -135,12 +197,15 @@ describe(componentName, () => {
     const { fn } = jest;
     const handleChange = fn();
     const handleSave = fn();
+    const handleCancel = fn();
+
     const { container } = render(
       <InlineEdit
         {...requiredProps}
         value={startingValue}
         onChange={handleChange}
         onSave={handleSave}
+        onCancel={handleCancel}
       />
     );
     const component = container.querySelector(`.${blockClass}`);
@@ -160,14 +225,58 @@ describe(componentName, () => {
     const controls = screen.getAllByRole('button');
     expect(controls).toHaveLength(2); // cancel and save
 
-    // click cancel check result reverts
     userEvent.click(controls[1]);
-    expect(input).toHaveTextContent(startingValue);
     expect(component).not.toHaveClass(`${blockClass}--editing`);
     expect(input).toHaveTextContent(`${startingValue}ABC`);
 
-    expect(handleChange).toBeCalledTimes(3);
-    expect(handleSave).toBeCalled();
+    expect(handleChange).toBeCalledTimes(3); // 3 for ABC and once for cancel
+    expect(handleSave).toBeCalledTimes(1);
+  });
+
+  it('can start and cancel edit with buttons', () => {
+    const startingValue = value;
+    const { fn } = jest;
+    const handleChange = fn();
+    const handleSave = fn();
+    const handleCancel = fn();
+
+    const { container } = render(
+      <InlineEdit
+        {...requiredProps}
+        value={startingValue}
+        onChange={handleChange}
+        onSave={handleSave}
+        onCancel={handleCancel}
+      />
+    );
+    const component = container.querySelector(`.${blockClass}`);
+
+    const input = screen.getByRole('textbox');
+
+    // starts edit
+    const edit = container.querySelector(`.${blockClass}__edit`);
+    userEvent.click(edit);
+    expect(component).toHaveClass(`${blockClass}--editing`);
+
+    // types some text
+    userEvent.type(input, 'ABC');
+    expect(input).toHaveTextContent(`${startingValue}ABC`);
+
+    // has edit buttons
+    const controls = screen.getAllByRole('button');
+    expect(controls).toHaveLength(2); // cancel and save
+
+    // click save and check event
+
+    // click cancel check result reverts
+    userEvent.click(controls[0]);
+    expect(component).not.toHaveClass(`${blockClass}--editing`);
+    // JSDom does not implement innerText, so cannot check toHaveTextContent
+    expect(input.innerText).toEqual(startingValue);
+
+    expect(handleChange).toBeCalledTimes(4); // 3 for ABC and once for cancel
+    expect(handleSave).not.toBeCalled();
+    expect(handleCancel).toBeCalledTimes(1);
   });
 
   it('does not allow disabled edit', () => {

57b766ca8d75f76d7c4f68592d779d30e5732edf
David Menendez <dmenend@us.ibm.com>
2022-03-22 10:52:34 -0500

    chore: adds moodifier to add select

--- packages/cloud-cognitive/src/components/AddSelect/AddSelectList.js
+++ packages/cloud-cognitive/src/components/AddSelect/AddSelectList.js
@@ -13,15 +13,18 @@ import {
   StructuredListWrapper,
   StructuredListBody,
   StructuredListCell,
+  Dropdown,
 } from 'carbon-components-react';
 import { ChevronRight16 } from '@carbon/icons-react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import { pkg } from '../../settings';
 const componentName = 'AddSelectList';
 
 export let AddSelectList = ({
   filteredItems,
   inColumn,
+  modifiers,
   multi,
   multiSelection,
   path,
@@ -75,37 +78,57 @@ export let AddSelectList = ({
     }
   };
 
+  const isSelected = (id) => multiSelection.includes(id);
+
   return (
     <div className={`${blockClass}-wrapper`}>
       <StructuredListWrapper selection className={`${blockClass}`}>
         <StructuredListBody>
           {filteredItems.map((item) => (
-            <StructuredListRow key={item.id} className={`${blockClass}-row`}>
+            <StructuredListRow
+              key={item.id}
+              className={cx(`${blockClass}-row`, {
+                [`${blockClass}-row-selected`]: isSelected(item.id),
+              })}
+            >
               <StructuredListCell className={`${blockClass}-cell`}>
                 <div className={`${blockClass}-cell-wrapper`}>
                   {multi ? (
-                    <Checkbox
-                      className={`${blockClass}-checkbox`}
-                      onChange={(checked) =>
-                        handleMultiSelection(item.id, checked)
-                      }
-                      labelText={
-                        !inColumn ? (
-                          <>
-                            <span className={`${blockClass}-cell-title`}>
-                              {item.title}
-                            </span>
-                            <span className={`${blockClass}-cell-subtitle`}>
-                              {item.subtitle}
-                            </span>
-                          </>
-                        ) : (
-                          item.title
-                        )
-                      }
-                      id={item.id}
-                      checked={multiSelection.includes(item.id)}
-                    />
+                    <>
+                      <Checkbox
+                        className={`${blockClass}-checkbox`}
+                        onChange={(checked) =>
+                          handleMultiSelection(item.id, checked)
+                        }
+                        labelText={
+                          !inColumn ? (
+                            <>
+                              <span className={`${blockClass}-cell-title`}>
+                                {item.title}
+                              </span>
+                              <span className={`${blockClass}-cell-subtitle`}>
+                                {item.subtitle}
+                              </span>
+                            </>
+                          ) : (
+                            item.title
+                          )
+                        }
+                        id={item.id}
+                        checked={isSelected(item.id)}
+                      />
+                      {modifiers?.options?.length && (
+                        <Dropdown
+                          id={`${item.id}-modifier`}
+                          type="inline"
+                          items={modifiers?.options}
+                          light
+                          label={modifiers?.label}
+                          disabled={!isSelected(item.id)}
+                          className={`${blockClass}-dropdown`}
+                        />
+                      )}
+                    </>
                   ) : (
                     <RadioButton
                       className={`${blockClass}-radio`}
@@ -133,6 +156,7 @@ export let AddSelectList = ({
 AddSelectList.propTypes = {
   filteredItems: PropTypes.array,
   inColumn: PropTypes.bool,
+  modifiers: PropTypes.object,
   multi: PropTypes.bool,
   multiSelection: PropTypes.array,
   path: PropTypes.array,

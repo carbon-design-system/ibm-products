946e1002c2d7f1870cbabd6b7305e575f9e54df6
Ratheesh Rajan <Ratheesh.Rajan@ibm.com>
2022-03-24 10:57:23 +0530

    fix(HTTP errors): responsive behaviour

--- packages/cloud-cognitive/src/components/HTTPErrors/assets/HTTPErrorSvgOther.js
+++ packages/cloud-cognitive/src/components/HTTPErrors/assets/HTTPErrorSvgOther.js
@@ -16,8 +16,6 @@ export const HTTPErrorSvgOther = ({ className }) => {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
-      width={1584}
-      height={916}
       viewBox="0 0 1584 916"
       className={className}
     >

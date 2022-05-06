c7b83bf4494701320d9eb49cc85eebda9b20c32f
Matt Gallo <mdgallo@us.ibm.com>
2022-05-06 11:00:00 -0400

    chore: add workaround for uuid package to work with jest 28

--- config/jest-config-ibm-cloud-cognitive/index.js
+++ config/jest-config-ibm-cloud-cognitive/index.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2020, 2020
+ * Copyright IBM Corp. 2020, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -44,6 +44,7 @@ module.exports = {
     },
   },
   moduleFileExtensions: ['js', 'json', 'node'],
+  resolver: require.resolve('./setup/resolver.js'),
   reporters: ['default'],
   setupFiles: [require.resolve('./setup/setupFiles')],
   setupFilesAfterEnv: [require.resolve('./setup/setupFilesAfterEnv')],

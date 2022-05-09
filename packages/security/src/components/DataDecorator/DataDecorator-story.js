c42a7d82eb3caaf7191dd64ee2ea14e3897fd06b
Anne-Michelle Toussaint <am.toussaint@ibm.com>
2022-05-09 16:07:00 -0400

    fix: update to link to proper component

--- packages/security/src/components/DataDecorator/DataDecorator-story.js
+++ packages/security/src/components/DataDecorator/DataDecorator-story.js
@@ -17,7 +17,7 @@ import { Grid } from 'carbon-components-react';
 
 import { props, midLine } from './_mocks_';
 
-import { InlineNotification } from '../../';
+import { InlineNotification, NotificationActionButton } from '../../';
 
 const { type, value, score, href } = props;
 const { scoreThresholds } = Decorator.defaultProps;
@@ -65,8 +65,17 @@ storiesOf(patterns('DataDecorator#legacy'), module)
     <>
       <InlineNotification
         className="page-layouts__banner"
+        actions={
+          <NotificationActionButton
+            href="https://carbon-for-ibm-products.netlify.app/?path=/story/security-components-decorator--default&knob-Delimiter%20(delimiter)=,%20&knob-Items%20(items)=Item%201,Item%202,Item%203,Item%204,Item%205,Item%206,Item%207,Item%208,Item%209,Item%2010&knob-Lines%20(`lines`)=1&knob-Score%20(`score`)=3&knob-Title%20(`title`)=10.0.0.0&knob-Truncate%20(`truncate`)=true&knob-Truncate%20(truncate)=true&knob-Type%20(`type`)=IP&knob-Value%20(`value`)=10.0.0.0.563409u53250u&knob-Width%20(`width`)=100px&knob-type=IP&knob-value=127.0.0.1"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            View replacement
+          </NotificationActionButton>
+        }
         kind="info"
-        subtitle="This component is now legacy."
+        subtitle="Pattern no longer supported. The pattern will remain available, but plan to migrate to the pattern replacement."
         title=""
         hideCloseButton
       />

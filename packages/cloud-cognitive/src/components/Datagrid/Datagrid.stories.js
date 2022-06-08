c9ca44fdb698c12898e6e6b3015b9b996c605c06
Faraz Naseem <faraz.naseem@ibm.com>
2022-06-08 10:08:25 -0400

    chore: added the select all button

--- packages/cloud-cognitive/src/components/Datagrid/Datagrid.stories.js
+++ packages/cloud-cognitive/src/components/Datagrid/Datagrid.stories.js
@@ -796,7 +796,7 @@ export const SelectItemsInAllPages = () => {
     <>
       <Datagrid datagridState={{ ...datagridState }} />
       <h3>Doc in Notes...</h3>
-      <p>{`Are all selected across all pages? - ${areAllSelected}`}</p>
+      <p>{`Are all entries selected across all pages? - ${areAllSelected}`}</p>
     </>
   );
 };
@@ -966,17 +966,32 @@ const BatchActionsDatagridBatchActions = (datagridState) => {
 export const BatchActions = () => {
   const columns = React.useMemo(() => defaultHeader, []);
   const [data] = useState(makeData(10));
+  const [areAllSelected, setAreAllSelected] = useState(false);
   const datagridState = useDatagrid(
     {
       columns,
       data,
+      selectAllToggle: {
+        labels: {
+          allRows: 'Select all',
+        },
+        onSelectAllRows: setAreAllSelected,
+      },
+      DatagridPagination,
       DatagridActions: DatagridActionsBatchActions,
       DatagridBatchActions: BatchActionsDatagridBatchActions,
     },
-    useSelectRows
+    useSelectRows,
+    useSelectAllWithToggle
   );
 
-  return <Datagrid datagridState={{ ...datagridState }} />;
+  return (
+    <>
+      <Datagrid datagridState={{ ...datagridState }} />
+      <h3>Doc in Notes...</h3>
+      <p>{`Are all entries selected across all pages? - ${areAllSelected}`}</p>
+    </>
+  );
 };
 
 export const DisableSelectRow = () => {

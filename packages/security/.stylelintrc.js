a1b29720327d8c5748d9b740e37c66b4eecc0917
Lee Chase <lee.chase@uk.ibm.com>
2022-07-11 16:00:51 +0100

    chore: update carbon token linter to match v11 code (#2083)
    * chore: update carbon stylelint
    
    * chore: update carbon token linter
    
    * chore: update lock
    
    * chore: update snapshot
    
    Co-authored-by: Matthew Gallo <mdgallo@us.ibm.com>

--- packages/security/.stylelintrc.js
+++ packages/security/.stylelintrc.js
@@ -6,7 +6,7 @@
  */
 
 module.exports = {
-  extends: '../../.stylelintrc',
+  extends: ['stylelint-config-ibm-products'],
   ignoreFiles: ['**/css-gridish/**'],
   rules: {
     'declaration-property-value-disallowed-list': null,
@@ -33,6 +33,27 @@ module.exports = {
           'get-component-namespace',
           'deprecate',
         ],
+      },
+    ],
+    'carbon/layout-token-use': [
+      true,
+      { severity: 'error', acceptUndefinedVariables: true },
+    ],
+    'carbon/motion-token-use': [
+      true,
+      { severity: 'error', acceptUndefinedVariables: true },
+    ],
+    'carbon/theme-token-use': [
+      true,
+      { severity: 'error', acceptUndefinedVariables: true },
+    ],
+    'carbon/type-token-use': [
+      true,
+      {
+        severity: 'error',
+        acceptCarbonTypeScaleFunction: true,
+        acceptCarbonFontWeightFunction: true,
+        acceptUndefinedVariables: true,
       },
     ],
   },

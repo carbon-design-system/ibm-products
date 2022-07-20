60da68a8d278640f702a15311b5a5bdf636d7789
Lee Chase <lee.chase@uk.ibm.com>
2022-07-20 11:36:20 +0100

    chore: update v11 branch to token linter beta 13

--- packages/cdai/.stylelintrc.js
+++ packages/cdai/.stylelintrc.js
@@ -23,7 +23,14 @@ module.exports = {
         acceptCarbonMiniUnitsFunction: true,
       },
     ],
-    'carbon/motion-token-use': [true, { severity: 'warning' }],
+    'carbon/motion-duration-use': [
+      true,
+      { severity: 'error', acceptUndefinedVariables: true },
+    ],
+    'carbon/motion-easing-use': [
+      true,
+      { severity: 'error', acceptUndefinedVariables: true },
+    ],
     'carbon/theme-token-use': [
       true,
       { severity: 'warning', acceptUndefinedVariables: true },

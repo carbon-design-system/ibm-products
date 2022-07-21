cd09babac9f862339760acd64fc24340c06423ae
Jagath Jayakumar <jagath.gj@gmail.com>
2022-07-22 01:20:11 +0530

    feat(edit and update): edit and update tearsheet (#1975)
    * feat(edit and update): edit and update tearsheet
    
    * feat(edit tearsheet): review comment changes added
    
    * chore(edit tearsheet): review changes added
    
    * chore(edit tearsheet): simplified edit tearsheet
    
    * chore(edit tearsheet): test written as sample
    
    * chore(edit tearsheet): tests added and match media
    
    * chore(edit tearsheet): scss animation name
    
    Co-authored-by: Elysia <elysia.hwang@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    Co-authored-by: David Menendez <damenend@gmail.com>

--- packages/cloud-cognitive/src/__tests__/index-all-enabled.test.js
+++ packages/cloud-cognitive/src/__tests__/index-all-enabled.test.js
@@ -33,6 +33,16 @@ describe(name, () => {
       unobserve: jest.fn(),
       disconnect: jest.fn(),
     }));
+    window.matchMedia = jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    }));
   });
 
   afterEach(() => {

e46be1339eda85f64c1e28a36f744ebc706da09b
Alexander Melo <alexandermelox@gmail.com>
2022-08-17 11:36:15 -0400

    chore: adds copyright headers

--- packages/cloud-cognitive/src/components/WebTerminal/hooks/index.js
+++ packages/cloud-cognitive/src/components/WebTerminal/hooks/index.js
@@ -1,3 +1,10 @@
+//
+// Copyright IBM Corp. 2022, 2022
+//
+// This source code is licensed under the Apache-2.0 license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
 import React, { useState, useCallback, createContext } from 'react';
 import { useContext } from 'react';
 import PropTypes from 'prop-types';

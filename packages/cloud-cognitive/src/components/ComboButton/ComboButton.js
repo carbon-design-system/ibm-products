21b4965fc16a23f5f2a00c8392c4847b2f24961d
James Thompson <qbi11y@gmail.com>
2022-10-04 13:07:03 -0500

    fix(ComboButton): ran yarn format

--- packages/cloud-cognitive/src/components/ComboButton/ComboButton.js
+++ packages/cloud-cognitive/src/components/ComboButton/ComboButton.js
@@ -2,7 +2,7 @@
  * Copyright IBM Corp. 2020, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
- * LICENSE file in the root directory of this source tree. 
+ * LICENSE file in the root directory of this source tree.
  */
 
 import { ChevronDown16, ChevronUp16 } from '@carbon/icons-react';

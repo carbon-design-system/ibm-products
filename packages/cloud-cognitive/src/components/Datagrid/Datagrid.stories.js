d65f50c22018cd7970dbf85a8bc8ee4e3164726f
Matt Gallo <mdgallo@us.ibm.com>
2022-10-04 15:33:44 -0400

    chore(Datagrid): create customize columns storybook extension (v11) (#2338)
    * chore(Datagrid): add column customization storybook extension
    
    * chore(Datagrid): remove row size story, included in extensions

--- packages/cloud-cognitive/src/components/Datagrid/Datagrid.stories.js
+++ packages/cloud-cognitive/src/components/Datagrid/Datagrid.stories.js
@@ -22,19 +22,12 @@ import {
   useSelectRows,
   useSortableColumns,
   useDisableSelectRows,
-  useCustomizeColumns,
   useSelectAllWithToggle,
   useStickyColumn,
   useActionsColumn,
-  useColumnOrder,
 } from '.';
 
-import {
-  CustomizeColumnStory,
-  RowSizeDropdownStory,
-  SelectAllWitHToggle,
-  LeftPanelStory,
-} from './Datagrid.stories';
+import { SelectAllWitHToggle, LeftPanelStory } from './Datagrid.stories';
 import mdx from './Datagrid.mdx';
 
 import { pkg } from '../../settings';
@@ -421,113 +414,6 @@ export const SelectItemsInAllPages = () => {
   );
 };
 SelectItemsInAllPages.story = SelectAllWitHToggle;
-
-export const CustomizingColumns = () => {
-  const columns = React.useMemo(() => defaultHeader, []);
-  const [data] = useState(makeData(10));
-  const datagridState = useDatagrid(
-    {
-      columns,
-      className: `c4p--datagrid__hidden--columns`,
-      data,
-      initialState: {
-        hiddenColumns: ['age'],
-        columnOrder: [],
-      },
-      customizeColumnsProps: {
-        onSaveColumnPrefs: (newColDefs) => {
-          console.log(newColDefs);
-        },
-        labels: {
-          findColumnPlaceholderLabel: 'Find column',
-          resetToDefaultLabel: 'Reset to default',
-          customizeModalHeadingLabel: 'Customize display',
-          primaryButtonTextLabel: 'Save',
-          secondaryButtonTextLabel: 'Cancel',
-          instructionsLabel:
-            'Deselect columns to hide them. Click and drag the white box to reorder the columns. These specifications will be saved and persist if you leave and return to the data table.',
-          iconTooltipLabel: 'Customize columns',
-          assistiveTextInstructionsLabel:
-            'Press space bar to toggle drag drop mode, use arrow keys to move selected elements.',
-          assistiveTextDisabledInstructionsLabel:
-            'Reordering columns are disabled because they are filtered currently.',
-          selectAllLabel: 'Column name',
-        },
-      },
-      DatagridActions,
-      DatagridBatchActions,
-    },
-    useCustomizeColumns,
-    useColumnOrder
-  );
-
-  return (
-    <>
-      <Datagrid datagridState={{ ...datagridState }} />
-      <div>
-        Hidden column ids:
-        <pre>{JSON.stringify(datagridState.state.hiddenColumns, null, 2)}</pre>
-      </div>
-      <p>
-        More details in the <strong>Notes</strong> section
-      </p>
-    </>
-  );
-};
-CustomizingColumns.story = CustomizeColumnStory;
-
-export const RowSizeDropdown = () => {
-  const columns = React.useMemo(
-    () => [
-      ...defaultHeader.slice(0, 3),
-      {
-        Header: 'Different cell content',
-        id: 'rowSizeDemo-cell',
-        disableSortBy: true,
-        Cell: ({ rowSize }) => rowSize,
-      },
-    ],
-    []
-  );
-  const [data] = useState(makeData(10));
-  const datagridState = useDatagrid(
-    {
-      columns,
-      data,
-      rowSize: 'xs',
-      rowSizes: [
-        {
-          value: 'xl',
-          labelText: 'More than super',
-        },
-        {
-          value: 'lg',
-          labelText: 'Super tall row',
-        },
-        {
-          value: 'md',
-        },
-        {
-          value: 'xs',
-          labelText: 'Teeny tiny row',
-        },
-      ],
-      onRowSizeChange: (value) => {
-        console.log('row size changed to: ', value);
-      },
-      DatagridActions,
-      DatagridBatchActions,
-    },
-    useSelectRows
-  );
-
-  return (
-    <Wrapper>
-      <Datagrid datagridState={{ ...datagridState }} />
-    </Wrapper>
-  );
-};
-RowSizeDropdown.story = RowSizeDropdownStory;
 
 export const LeftPanel = () => {
   const columns = React.useMemo(() => defaultHeader, []);

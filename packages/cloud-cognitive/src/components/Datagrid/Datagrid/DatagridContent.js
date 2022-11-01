682bdd7b578a7c628b61e68da967c3cb584f8f23
Matt Gallo <mdgallo@us.ibm.com>
2022-11-01 15:03:14 -0400

    docs(Datagrid): add column alignment and inline edit docs

--- packages/cloud-cognitive/src/components/Datagrid/Datagrid/DatagridContent.js
+++ packages/cloud-cognitive/src/components/Datagrid/Datagrid/DatagridContent.js
@@ -108,7 +108,7 @@ export const DatagridContent = ({ datagridState }) => {
       return;
     }
     const gridElement = document.querySelector(`#${tableId}`);
-    const tableHeader = document.querySelector(
+    const tableHeader = gridElement?.querySelector(
       `.${carbon.prefix}--data-table-header`
     );
     gridElement.style.setProperty(

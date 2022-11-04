6f2ea232b7e2869b89fa97bb98069d288ae57c79
Liz Tremblay <liz.tremblay@ibm.com>
2022-11-04 09:14:33 -0400

    feat(Datagrid): filter panel progress

--- packages/cloud-cognitive/src/components/Datagrid/Datagrid.stories.js
+++ packages/cloud-cognitive/src/components/Datagrid/Datagrid.stories.js
@@ -642,13 +642,12 @@ SelectItemsInAllPages.story = SelectAllWitHToggle;
 export const LeftPanel = () => {
   const columns = React.useMemo(() => defaultHeader, []);
   const [data] = useState(makeData(10));
+  // const sections = [{ title: "Source Details", subsections: <Accordion><AccordionItem title="IP address"><Checkbox labelText='Jambo' id="checkbox-label-1" /> <Checkbox labelText={`Bingus`} id="checkbox-label-2" /></AccordionItem><AccordionItem title="IP address"><Checkbox labelText='Jumbos' id="checkbox-label-3" /> <Checkbox labelText="memow" id="checkbox-label-4" /></AccordionItem></Accordion>}];
   const datagridState = useDatagrid({
     leftPanel: {
       isOpen: true, // this toggling will happen from datagridActions.
       panelContent: (
-        <div className={`${blockClass}__panel-content`}>
-          Panel content will go here along with any button interactions
-        </div>
+        // <FilterPanelSkeleton header='Filter' filterSections={sections}/>
       ),
     },
     columns,

dc18db7c3ac87c4ede4db00fa0f9066ab4ae7e36
David Menendez <damenend@gmail.com>
2022-11-11 15:10:56 -0600

    fix: add select filter fix

--- packages/cloud-cognitive/src/components/AddSelect/add-select-utils.js
+++ packages/cloud-cognitive/src/components/AddSelect/add-select-utils.js
@@ -85,7 +85,7 @@ export const getFilteredItems = (
     if (searchTerm || globalFiltersApplied) {
       const results = itemIds
         .reduce((prev, cur) => {
-          if (normalizedItems[cur].title.toLowerCase().includes(searchTerm)) {
+          if (normalizedItems[cur].title.toLowerCase().includes(searchTerm.toLowerCase())) {
             prev.push(normalizedItems[cur]);
           }
           return prev;
@@ -123,7 +123,7 @@ export const getFilteredItems = (
   } else {
     if (searchTerm) {
       return items.entries.filter((item) =>
-        item.title.toLowerCase().includes(searchTerm)
+        item.title.toLowerCase().includes(searchTerm.toLowerCase())
       );
     }
     return items.entries;

a4379a3d9419a32829e531f8f09da167bee28ff7
Ratheesh Rajan <Ratheesh.Rajan@ibm.com>
2022-12-13 16:02:59 +0530

    fix(DataGrid): empty state with action and link

--- packages/cloud-cognitive/src/components/Datagrid/Datagrid.stories.js
+++ packages/cloud-cognitive/src/components/Datagrid/Datagrid.stories.js
@@ -147,6 +147,16 @@ export const EmptyState = () => {
   const emptyStateDescription = 'Description explaining why the table is empty';
   const emptyStateSize = 'lg';
   const illustrationTheme = 'light';
+  const emptyStateAction = {
+    text: 'Create new',
+    onClick: action('Clicked empty state action button'),
+    renderIcon: Add16,
+    iconDescription: 'Add icon',
+  };
+  const emptyStateLink = {
+    text: 'View documentation',
+    href: 'https://www.carbondesignsystem.com',
+  };
 
   const datagridState = useDatagrid({
     columns,
@@ -155,6 +165,8 @@ export const EmptyState = () => {
     emptyStateDescription,
     emptyStateSize,
     illustrationTheme,
+    emptyStateAction,
+    emptyStateLink,
     DatagridActions,
     DatagridBatchActions,
     DatagridPagination,

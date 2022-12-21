ced27eaec6652dcc4286d7df5fdc45fc290da99f
Elysia Hwang <Elysia.Hwang@ibm.com>
2022-12-21 17:23:44 -0500

    chore: fix customize columns styles

--- packages/cloud-cognitive/src/components/Datagrid/Datagrid/addons/CustomizeColumns/Columns.js
+++ packages/cloud-cognitive/src/components/Datagrid/Datagrid/addons/CustomizeColumns/Columns.js
@@ -93,17 +93,18 @@ const Columns = ({
               : assistiveTextDisabledInstructionsLabel}
           </span>
           <div
-            id={`${blockClass}__customize-columns--select-all`}
-            className={classNames({
-              [`${blockClass}__customize-columns--select-all`]:
-                getVisibleColumnsCount() === 0,
-              [`${blockClass}__customize-columns--select-all ${blockClass}__customize-columns--select-all-selected`]:
-                getVisibleColumnsCount() > 0,
-            })}
+            id={`${blockClass}__customize-columns-select-all`}
+            className={classNames(
+              `${blockClass}__customize-columns-select-all`,
+              {
+                [`${blockClass}__customize-columns-select-all--selected`]:
+                  getVisibleColumnsCount() > 0,
+              }
+            )}
             selected={getVisibleColumnsCount() > 0}
           >
             <Checkbox
-              wrapperClassName={`${blockClass}__customize-columns--checkbox-wrapper`}
+              wrapperClassName={`${blockClass}__customize-columns-checkbox-wrapper`}
               checked={getVisibleColumnsCount() === columns.length}
               indeterminate={
                 getVisibleColumnsCount() < columns.length &&
@@ -159,7 +160,7 @@ const Columns = ({
                 selected={isColumnVisible(colDef)}
               >
                 <Checkbox
-                  wrapperClassName={`${blockClass}__customize-columns--checkbox-wrapper`}
+                  wrapperClassName={`${blockClass}__customize-columns-checkbox-wrapper`}
                   checked={isColumnVisible(colDef)}
                   onChange={onSelectColumn.bind(null, colDef)}
                   id={`${blockClass}__customization-column-${colDef.id}`}

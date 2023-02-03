990e8c6ded95cdc5109ea8a9fc4d0e0cd52818ad
Ratheesh Rajan <Ratheesh.Rajan@ibm.com>
2023-02-03 21:09:18 +0530

    fix(DataGrid): filter summary add heights options v11 (#2643)
    * fix(DataGrid): filter summary height v11
    
    * chore(DataGrid): lint fix
    
    * chore(DataGrid): lint fix
    
    * chore(DataGrid): lint fix
    
    * chore(DataGrid): lint fix
    
    * chore(DataGrid): lint fix
    
    ---------
    
    Co-authored-by: Alexander Melo <alexandermelox@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/cloud-cognitive/src/components/Datagrid/Datagrid/DatagridToolbar.js
+++ packages/cloud-cognitive/src/components/Datagrid/Datagrid/DatagridToolbar.js
@@ -157,8 +157,13 @@ const DatagridBatchActionsToolbar = (datagridState, width, ref) => {
 
 const DatagridToolbar = (datagridState) => {
   const { width, ref } = useResizeDetector();
-  const { DatagridActions, DatagridBatchActions, batchActions, state } =
-    datagridState;
+  const {
+    DatagridActions,
+    DatagridBatchActions,
+    batchActions,
+    state,
+    rowSize,
+  } = datagridState;
   const { filterTags, EventEmitter } = useContext(FilterContext);
 
   const renderFilterSummary = () =>
@@ -169,8 +174,16 @@ const DatagridToolbar = (datagridState) => {
       />
     );
 
+  const getRowHeight = rowSize ? rowSize : 'lg';
+
   return batchActions && DatagridActions ? (
-    <div ref={ref} className={`${blockClass}__table-toolbar`}>
+    <div
+      ref={ref}
+      className={cx(
+        `${blockClass}__table-toolbar`,
+        `${blockClass}__table-toolbar--${getRowHeight}`
+      )}
+    >
       <TableToolbar>
         {DatagridActions && DatagridActions(datagridState)}
         {DatagridBatchActionsToolbar &&

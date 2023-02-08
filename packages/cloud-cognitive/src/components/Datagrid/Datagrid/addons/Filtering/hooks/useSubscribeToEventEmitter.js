cfde86090bfcd437facfee778d75cf19f1bf6084
Alexander Melo <alexandermelox@gmail.com>
2023-02-08 16:28:28 -0500

    fix: updates copyright header

--- packages/cloud-cognitive/src/components/Datagrid/Datagrid/addons/Filtering/hooks/useSubscribeToEventEmitter.js
+++ packages/cloud-cognitive/src/components/Datagrid/Datagrid/addons/Filtering/hooks/useSubscribeToEventEmitter.js
@@ -1,9 +1,8 @@
-/*
- * Licensed Materials - Property of IBM
- * 5724-Q36
- * (c) Copyright IBM Corp. 2022
- * US Government Users Restricted Rights - Use, duplication or disclosure
- * restricted by GSA ADP Schedule Contract with IBM Corp.
+/**
+ * Copyright IBM Corp. 2022, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 import { useEffect, useContext } from 'react';
 import { FilterContext } from '../FilterProvider';

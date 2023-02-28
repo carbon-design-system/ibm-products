fb1852a1f96e7b521a11c6f290d76ec206079daf
Elysia Hwang <Elysia.Hwang@ibm.com>
2023-02-28 09:23:16 -0500

    chore: update copyright headers

--- packages/cloud-cognitive/src/components/Datagrid/useSortableColumns.js
+++ packages/cloud-cognitive/src/components/Datagrid/useSortableColumns.js
@@ -1,10 +1,10 @@
-/*
- * Licensed Materials - Property of IBM
- * 5724-Q36
- * (c) Copyright IBM Corp. 2020
- * US Government Users Restricted Rights - Use, duplication or disclosure
- * restricted by GSA ADP Schedule Contract with IBM Corp.
+/**
+ * Copyright IBM Corp. 2020, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
  */
+
 import React from 'react';
 import cx from 'classnames';
 import { pkg, carbon } from '../../settings';

50efd30a73ac321c2ba24879803e719322bc5b0a
Ratheesh Rajan <ratheesh.rajan@ibm.com>
2023-04-19 13:03:16 +0530

    fix(Datagrid): removed explicit role from tbody

--- packages/cloud-cognitive/src/components/Datagrid/Datagrid/DatagridEmptyBody.js
+++ packages/cloud-cognitive/src/components/Datagrid/Datagrid/DatagridEmptyBody.js
@@ -29,7 +29,7 @@ const DatagridEmptyBody = (datagridState) => {
 
   return (
     <TableBody
-      {...getTableBodyProps()}
+      {...getTableBodyProps({role:false})}
       className={`${blockClass}__empty-state-body`}
     >
       <TableRow>

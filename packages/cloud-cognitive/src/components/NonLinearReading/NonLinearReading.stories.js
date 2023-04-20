e7f2bb417631ab8a462d5f0db8119e66bb0ec123
Paul Balchin <Paul.Balchin@ibm.com>
2023-04-20 14:19:16 -0400

    fix(nonlinear): update styling

--- packages/cloud-cognitive/src/components/NonLinearReading/NonLinearReading.stories.js
+++ packages/cloud-cognitive/src/components/NonLinearReading/NonLinearReading.stories.js
@@ -8,6 +8,7 @@
 import React from 'react';
 
 import {
+  getSelectedCarbonTheme,
   getStoryTitle,
   prepareStory,
 } from '../../global/js/utils/story-helper';
@@ -26,23 +27,64 @@ export default {
       page: mdx,
     },
   },
+  argTypes: {
+    definition: {
+      table: {
+        disable: true,
+      },
+    },
+    theme: {
+      control: { type: null },
+    },
+  },
 };
 
 const defaultProps = {
+  definition: (
+    <>
+      This is a <strong>technical</strong> component that uses a set of{' '}
+      <em>rules</em> to process alerts from your{' '}
+      <a href="https://www.ibm.com/" target="_blank" rel="noreferrer">
+        sources
+      </a>
+      , and streamline threat analysis.
+    </>
+  ),
   theme: 'light',
 };
 
 // As a standalone component, the "story" is meaningless:
-// just a pill for a term, expanding to show its definition.
+// just a pill for a keyword, expanding to show its definition.
 // Should always be shown in context with surrounding text.
 const Template = (args) => {
+  const theme = getSelectedCarbonTheme();
+
   return (
     <>
       XDR Connect’s correlation
-      <NonLinearReading {...args} />
+      <NonLinearReading {...args} theme={theme} />
       creates a case by processing data from alerts across multiple security
       tools.
     </>
+  );
+};
+const TemplateGradientBG = (args) => {
+  return (
+    <div>
+      Some other Novice to pro components use a gradient background as shown
+      below.
+      <br />
+      If using NonLinearReading inside one of these components, always use{' '}
+      <code>theme=&quot;dark&quot;</code>.
+      <br />
+      <br />
+      <div className="gradient-bg">
+        XDR Connect’s correlation
+        <NonLinearReading {...args} theme="dark" />
+        creates a case by processing data from alerts across multiple security
+        tools.
+      </div>
+    </div>
   );
 };
 
@@ -50,7 +92,12 @@ export const Default = prepareStory(Template, {
   args: {
     ...defaultProps,
     children: 'engine,',
-    definition:
-      'This is a technical component that uses a set of rules to process alerts from your sources, and streamline threat analysis.',
+  },
+});
+
+export const GradientBG = prepareStory(TemplateGradientBG, {
+  args: {
+    ...defaultProps,
+    children: 'engine,',
   },
 });

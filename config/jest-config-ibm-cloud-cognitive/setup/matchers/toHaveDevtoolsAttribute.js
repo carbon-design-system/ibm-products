32dd019bbffd2b05ae0c1a3d11185b2ab7badc55
Elysia Hwang <Elysia.Hwang@ibm.com>
2023-04-20 15:45:15 -0400

    chore: rename to ibm-products

--- config/jest-config-ibm-cloud-cognitive/setup/matchers/toHaveDevtoolsAttribute.js
+++ config/jest-config-ibm-cloud-cognitive/setup/matchers/toHaveDevtoolsAttribute.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-// Avoid side effects from `Canary` in `cloud-cognitive/src/settings`.
+// Avoid side effects from `Canary` in `ibm-products/src/settings`.
 import settings from '../../../../packages/ibm-products/src/global/js/package-settings';
 
 const { devtoolsAttribute, getDevtoolsId } = settings;

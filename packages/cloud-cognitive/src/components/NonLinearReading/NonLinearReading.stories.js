af95f01118778e06801ed2c91a9bc65c212590d0
Paul Balchin <Paul.Balchin@ibm.com>
2023-04-21 13:04:11 -0400

    fix: style update

--- packages/cloud-cognitive/src/components/NonLinearReading/NonLinearReading.stories.js
+++ packages/cloud-cognitive/src/components/NonLinearReading/NonLinearReading.stories.js
@@ -18,11 +18,14 @@ import mdx from './NonLinearReading.mdx';
 
 import styles from './_storybook-styles.scss';
 
+const storyClass = 'non-linear-reading-stories';
+
 export default {
   title: getStoryTitle(NonLinearReading.displayName),
   component: NonLinearReading,
   parameters: {
     styles,
+    layout: 'padded',
     docs: {
       page: mdx,
     },
@@ -56,46 +59,110 @@ const defaultProps = {
 // As a standalone component, the "story" is meaningless:
 // just a pill for a keyword, expanding to show its definition.
 // Should always be shown in context with surrounding text.
-const Template = (args) => {
+
+const TemplateSingleUse = (args) => {
   const theme = getSelectedCarbonTheme();
 
   return (
-    <>
+    <div className={`${storyClass}__viewport`}>
       XDR Connect’s correlation
       <NonLinearReading {...args} theme={theme} />
       creates a case by processing data from alerts across multiple security
       tools.
-    </>
+    </div>
   );
 };
-const TemplateGradientBG = (args) => {
+
+const TemplateMultipleUse = (args) => {
+  const theme = getSelectedCarbonTheme();
+
   return (
-    <div>
-      Some other Novice to pro components use a gradient background as shown
-      below.
-      <br />
-      If using NonLinearReading inside one of these components, always use{' '}
-      <code>theme=&quot;dark&quot;</code>.
-      <br />
-      <br />
+    <div className={`${storyClass}__viewport`}>
+      Findings are created by the alerts{' '}
+      <NonLinearReading
+        {...args}
+        definition={
+          <>
+            We examine the alerts from each source, and{' '}
+            <NonLinearReading
+              {...args}
+              definition="Correlation allows us to identify connections between common
+                  observables, tactics, and techniques, and to remove any
+                  duplicate data, thereby streamlining the investigation for
+                  you."
+              theme={theme}
+            >
+              correlate
+            </NonLinearReading>{' '}
+            them together with{' '}
+            <a href="https://www.ibm.com/" target="_blank" rel="noreferrer">
+              rules
+            </a>
+            . While a case can contain multiple alerts, a single finding can
+            only be created from a single alert. So, when you select a finding,
+            you can drill right down to the raw data behind it: the payload. We
+            also use our
+            <NonLinearReading
+              {...args}
+              definition="
+                  Our threat intelligence service contains an enrichment
+                  capability. During the enrichment process, we add context that
+                  is used to establish the severity of artifacts associated with
+                  a finding. This is what determines the severity of the finding
+                  itself. Bear in mind that each case can have multiple
+                  findings, and every finding will have its own severity.
+                "
+              theme={theme}
+            >
+              threat intelligence service
+            </NonLinearReading>
+            to establish the severity of the artifacts.
+          </>
+        }
+        theme={theme}
+      >
+        ingested
+      </NonLinearReading>{' '}
+      from your own security systems. The findings here are confirmed findings
+      that have been created from alerts ingested from your own sources, before
+      being enriched to create cases.
+    </div>
+  );
+};
+
+const TemplateWithGradientBG = (args) => {
+  return (
+    <div className={`${storyClass}__viewport`}>
       <div className="gradient-bg">
         XDR Connect’s correlation
         <NonLinearReading {...args} theme="dark" />
         creates a case by processing data from alerts across multiple security
         tools.
       </div>
+      <br />
+      Some other Novice to pro components use a gradient background as shown
+      above.
+      <br />
+      If using NonLinearReading inside one of these components, always use{' '}
+      <code>theme=&quot;dark&quot;</code>.
     </div>
   );
 };
 
-export const Default = prepareStory(Template, {
+export const SingleUse = prepareStory(TemplateSingleUse, {
   args: {
     ...defaultProps,
     children: 'engine,',
   },
 });
 
-export const GradientBG = prepareStory(TemplateGradientBG, {
+export const MultipleUse = prepareStory(TemplateMultipleUse, {
+  args: {
+    ...defaultProps,
+  },
+});
+
+export const WithGradientBG = prepareStory(TemplateWithGradientBG, {
   args: {
     ...defaultProps,
     children: 'engine,',

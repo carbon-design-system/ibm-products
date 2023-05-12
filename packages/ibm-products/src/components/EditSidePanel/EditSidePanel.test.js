fd58a46c1009af5724a1c61c10d5c64a44f50daf
Elysia Hwang <Elysia.Hwang@ibm.com>
2023-05-12 10:40:07 -0400

    chore: fix EditSidePanel tests

--- packages/ibm-products/src/components/EditSidePanel/EditSidePanel.test.js
+++ packages/ibm-products/src/components/EditSidePanel/EditSidePanel.test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2021, 2021
+ * Copyright IBM Corp. 2021, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -64,7 +64,6 @@ describe(componentName, () => {
   });
 
   afterEach(() => {
-    jest.restoreAllMocks();
     window.ResizeObserver = ResizeObserver;
   });
 

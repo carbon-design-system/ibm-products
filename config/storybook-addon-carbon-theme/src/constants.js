9ed1571d050191301b37807bdb5fae11e1fa8c51
Lee Chase <lee.chase@uk.ibm.com>
2023-06-01 11:46:12 +0100

    fix: constant exports

--- config/storybook-addon-carbon-theme/src/constants.js
+++ config/storybook-addon-carbon-theme/src/constants.js
@@ -5,6 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 export const ADDON_ID = '@carbon/theme';
+export const PANEL_ID = `${ADDON_ID}/panel`;
+export const TOOL_ID = `${ADDON_ID}/tool`;
 export const PARAM_KEY = 'carbonTheme';
 export const CARBON_THEMES = {
   white: 'white',

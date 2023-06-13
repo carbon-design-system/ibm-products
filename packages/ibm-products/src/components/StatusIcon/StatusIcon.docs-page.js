7bd11fafea64fe48e5efafcdb8410c1462de055f
Lee Chase <lee.chase@uk.ibm.com>
2023-06-13 12:01:40 +0100

    chore: tagset docs update

---
+++ packages/ibm-products/src/components/StatusIcon/StatusIcon.docs-page.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StoryDocsPage } from '../../global/js/utils/StoryDocsPage';
+import { StatusIcon } from '.';
+import * as stories from './StatusIcon.stories';
+
+const docsPage = () => (
+  <StoryDocsPage
+    componentName={StatusIcon.displayName}
+    guidelinesHref="https://pages.github.ibm.com/cdai-design/pal/patterns/status-icons/usage"
+    hasCodedExample={true}
+    blocks={[
+      {
+        story: stories.Default,
+      },
+      {
+        title: 'Accessibility',
+        description: `The \`StatusIcon\` component follows the Carbon guidelines for status icons with
+some added specifications around illustration usage. For additional usage
+guidelines and documentation please refer to the links above.
+
+_Status icons_ are an important method of communicating severity level
+information to users. The shapes and colors, communicate severity that enable
+users to quickly assess and identify status and respond accordingly.`,
+      },
+      {
+        title: 'Sizing',
+        description: `
+| Size    | Pixels  |
+| ------- | ------- |
+| small   | 16 x 16 |
+| medium  | 20 x 20 |
+| large   | 24 x 24 |
+| x-large | 32 x 32 |
+          `,
+      },
+    ]}
+  />
+);
+
+export default docsPage;

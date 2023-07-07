4736d5be4e3254d1cb1153d9f3eae4945d1ed7d7
kristastarr <krista.starr@ibm.com>
2023-07-07 13:55:47 -0500

    feat: media queries and reduced motion query for welcome animation

--- packages/core/src/welcome/Welcome.js
+++ packages/core/src/welcome/Welcome.js
@@ -5,69 +5,82 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
-import Lottie from "lottie-react";
-import bg from './welcome-banner-v2.svg';
-import lottie from './welcome-lottie.json';
+import React, { useState, useRef, useEffect } from 'react';
 import PackageInfo from '../../../ibm-products/package.json';
 import { Link, Tag } from '@carbon/react';
-import { ArrowRight } from '@carbon/icons-react';
-
+import { ArrowRight, Pause, Play } from '@carbon/icons-react';
+import Lottie from "lottie-react";
+import WelcomeLottie from './welcome-lottie.json';
 
 const Welcome = () => {
 
+  const lottieRef = useRef();
+
+  let prefersReducedMotion = window.matchMedia(`(prefers-reduced-motion: reduce)`).matches
+  let isAnimating = !prefersReducedMotion
+  
+  const toggleAnimation = () => {
+     isAnimating = !isAnimating;
+    if (isAnimating) {
+      lottieRef.current.play()
+     } 
+     else {
+      lottieRef.current.pause()
+    }
+  }
 
   const WelcomeAnimation = () => {
-    return <Lottie className="lottie" animationData={lottie} />;
-  };
-
-
+    return (
+      <Lottie 
+        className="welcome__lottie" 
+        animationData={ WelcomeLottie }
+        autoplay={ !prefersReducedMotion }
+        loop={ true }
+        lottieRef={ lottieRef } 
+      />
+    )
+  } 
 
   return (
-    <div className="welcome-container--outer">
-      <div
-        className="welcome__container--inner "
-        style={{
-          backgroundImage: `url(${bg})`,
-        }}
-      >
-      <div className="welcome__col--left">
-        <hgroup className="welcome__header">
-          <h2 className="welcome__heading">
-            Carbon for
-            <br />
-            IBM Products
-          </h2>
-          <h4 className="welcome__subtitle">{`v${PackageInfo.version}`}</h4>
-          <Tag size="md" type="purple" className="welcome__tag--xl">
-            Carbon 11 (v2)
-          </Tag>
-        </hgroup>
-        <div className="welcome__links">
-          <Link
-            href="https://github.com/carbon-design-system/ibm-products/blob/main/docs/guides/v2.md"
-            className="welcome__link"
-            renderIcon={ArrowRight}
-            size="lg"
-          >
-            Migration guide
-          </Link>
-          <Link
-            href="https://v1-ibm-products.carbondesignsystem.com/"
-            className="welcome__link"
-            renderIcon={ArrowRight}
-            size="lg"
-          >
-            Carbon 10 (v1) Storybook
-          </Link>
+    <div className="welcome__container--outer">
+      <div className="welcome__container--inner">
+        <div className="welcome__col--left">
+            <hgroup className="welcome__header">
+              <h2 className="welcome__heading">
+                Carbon for
+                <br />
+                IBM Products
+              </h2>
+              <h4 className="welcome__subtitle">{`v${PackageInfo.version}`}</h4>
+              <Tag size="md" type="purple" className="welcome__tag--xl">
+                Carbon 11 (v2)
+              </Tag>
+            </hgroup>
+            <div className="welcome__links">
+              <Link
+                href="https://github.com/carbon-design-system/ibm-products/blob/main/docs/guides/v2.md"
+                className="welcome__link"
+                renderIcon={ArrowRight}
+                size="lg"
+              >
+                Migration guide
+              </Link>
+              <Link
+                href="https://v1-ibm-products.carbondesignsystem.com/"
+                className="welcome__link"
+                renderIcon={ArrowRight}
+                size="lg"
+              >
+                Carbon 10 (v1) Storybook
+              </Link>
+            </div>
+        </div>
+        <div className="welcome__col--right">
+          <WelcomeAnimation />
+          <button className="welcome__pause-button" onClick={toggleAnimation}>{ isAnimating ? < Pause /> : < Play /> }</button>
         </div>
       </div>
-
-<div className="welcome__col--right">
-        <WelcomeAnimation />
-      </div>
     </div>
-  </div>
   );
 };
 

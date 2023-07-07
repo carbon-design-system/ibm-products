05bf5f767c4cfe605bb44a0994fc016ceebea679
Elysia Hwang <Elysia.Hwang@ibm.com>
2023-07-07 13:08:23 -0400

    chore: update mocks and snapshot

--- config/jest-config-ibm-cloud-cognitive/setup/setupFiles.js
+++ config/jest-config-ibm-cloud-cognitive/setup/setupFiles.js
@@ -49,3 +49,19 @@ if (global.HTMLElement) {
 // computed style for that regardless of a pseudo-element being supplied
 const oldGetComputedStyle = global.getComputedStyle;
 global.getComputedStyle = jest.fn((elt) => oldGetComputedStyle(elt));
+
+// Mock methods not in jsdom
+// https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});

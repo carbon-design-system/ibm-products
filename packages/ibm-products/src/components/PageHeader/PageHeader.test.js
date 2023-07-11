c18c206525b22446c4ceb895e6eefabb0c7c2483
David Menendez <damenend@gmail.com>
2023-07-11 12:09:23 -0500

    fix: update to Carbon 11 compatible versions to latest (#3198)
    * chore: package updates
    
    * fix: snapshot

--- packages/ibm-products/src/components/PageHeader/PageHeader.test.js
+++ packages/ibm-products/src/components/PageHeader/PageHeader.test.js
@@ -227,6 +227,16 @@ describe('PageHeader', () => {
       unobserve: jest.fn(),
       disconnect: jest.fn(),
     }));
+    window.matchMedia = jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    }));
   });
 
   afterEach(() => {

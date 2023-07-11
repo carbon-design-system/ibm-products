72db937bccff7faa0c05748fea9d93140d23dd50
David Menendez <damenend@gmail.com>
2023-07-11 11:34:49 -0500

    chore: package updates

--- packages/ibm-products/src/components/Tearsheet/Tearsheet.test.js
+++ packages/ibm-products/src/components/Tearsheet/Tearsheet.test.js
@@ -318,6 +318,16 @@ describe(componentName, () => {
       disconnect: jest.fn(),
     }));
     pkg.feature['default-portal-target-body'] = false;
+    window.matchMedia = jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    }));
   });
 
   afterAll(() => {

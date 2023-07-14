2894efac88a3b1b74a9dafe6f4a855723b3648b6
Elysia Hwang <Elysia.Hwang@ibm.com>
2023-07-14 16:05:01 -0400

    fix: failed accessibility tests

--- packages/ibm-products/src/components/CreateSidePanel/CreateSidePanel.js
+++ packages/ibm-products/src/components/CreateSidePanel/CreateSidePanel.js
@@ -18,6 +18,7 @@ import { getDevtoolsProps } from '../../global/js/utils/devtools';
 // Carbon and package components we use.
 import { Form } from 'carbon-components-react';
 import { SidePanel } from '../SidePanel';
+import uuidv4 from '../../global/js/utils/uuidv4';
 
 // The block part of our conventional BEM class names (blockClass__E--M).
 const blockClass = `${pkg.prefix}--create-side-panel`;
@@ -68,6 +69,8 @@ export let CreateSidePanel = React.forwardRef(
       },
     ];
 
+    const formTitleId = uuidv4();
+
     return (
       selectorPageContent && (
         <SidePanel
@@ -91,6 +94,7 @@ export let CreateSidePanel = React.forwardRef(
         >
           <h3
             className={`${blockClass}__form-title-text ${blockClass}__content-text`}
+            id={formTitleId}
           >
             {formTitle}
           </h3>
@@ -99,7 +103,9 @@ export let CreateSidePanel = React.forwardRef(
           >
             {formDescription}
           </p>
-          <Form className={`${blockClass}__form`}>{children}</Form>
+          <Form className={`${blockClass}__form`} aria-labelledby={formTitleId}>
+            {children}
+          </Form>
         </SidePanel>
       )
     );

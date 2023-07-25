4d93f4922f9e33c27a37add2586bcbb456de2426
Ratheesh Rajan <ratheesh.rajan@ibm.com>
2023-07-25 19:11:07 +0530

    fix(Datagrid): remove sort styles hook not use v1

--- packages/ibm-products/src/components/Datagrid/Datagrid/DatagridHeaderRow.js
+++ packages/ibm-products/src/components/Datagrid/Datagrid/DatagridHeaderRow.js
@@ -37,7 +37,7 @@ const HeaderRow = (datagridState, headRef, headerGroup) => (
               {
                 [`${blockClass}__resizableColumn`]: header.getResizerProps,
                 [`${blockClass}__isResizing`]: header.isResizing,
-                [`${blockClass}__sortableColumn`]: header.canSort,
+                [`${blockClass}__sortableColumn`]: datagridState.isTableSortable,
                 [`${blockClass}__isSorted`]: header.isSorted,
               },
               header.getHeaderProps().className

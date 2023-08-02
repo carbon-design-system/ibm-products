f7c5f02098fcc699a3dbc3116c21ee0a4e0ba4dc
Matt Gallo <mdgallo@us.ibm.com>
2023-08-02 13:37:46 -0400

    chore: update when to apply header to key

--- packages/ibm-products/src/components/Datagrid/useDefaultStringRenderer.js
+++ packages/ibm-products/src/components/Datagrid/useDefaultStringRenderer.js
@@ -28,7 +28,7 @@ const useDefaultStringRenderer = (hooks) => {
     <div
       className={`${blockClass}__defaultStringRenderer`}
       title={typeof header === 'string' ? header : ''}
-      key={header}
+      key={typeof header === 'string' ? header : ''}
     >
       {header}
     </div>

32584f94f46ab998f6d47c5b98e716018a454c53
Jeff Longshore <madkidflash@gmail.com>
2023-08-18 10:01:11 -0500

    feat(Coachmark): Novice to Pro component (#3143)
    * Removing Coachmark and re-scaffolding
    
    * WIP
    
    * Stories added - first draft
    
    * Fixing linting errors
    
    * working through a ref issue with coachmark in a stacked scenario
    
    * Removing steppedAnimatedMedia in order to get it from main_v1
    
    * Working on getting tests to pass
    
    * WIP
    
    * Added some logic to fix visual bug in CoachmarkStack
    * Stacked elements were showing and animating down on mount.
    * Updated comments for the children prop of the CoachmarkStack
    * Updated the issue with the body query selector fallback for the portalSelector
    * Added an is-mounted type of classname to help with the visual glitch
    
    * ran format script
    
    * Added reduced motion rules in styles.
    If user prefers reduced motion:
    * Carousel now scrolling set to auto (snaps)
    * Coachmark tagline doesn't fade in.
    
    * Initial code review items
    * Removed commented out dead code
    * modified 'closebuttonProps' to be 'closeButtonProps'
    * Modified some comments to not be all caps
    * Removed some CSpell items
    * Modified beacon css for an SVG to be non url encoded
    * In Coachmark, modified 'PortalSelector' prop to match tearsheets 'portalTarget'
    * used $carbon-prefix in place of '.bx...' classes.
    
    * cspell issue with "pathfill"
    
    * Updated documentation with copy from content review
    Removed odd prop controls from stories
    
    * Changes per review comments.
    
    * Trivial change
    
    * chore: update yarn.lock
    
    ---------
    
    Co-authored-by: Jeff Longshore <jeff.longshore@ibm.com>
    Co-authored-by: Elysia <elysia.hwang@gmail.com>

---
+++ packages/ibm-products/src/components/CoachmarkOverlayElement/CoachmarkOverlayElement.test.js
@@ -0,0 +1,123 @@
+/**
+ * Copyright IBM Corp. 2023, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import { render, screen } from '@testing-library/react'; // https://testing-library.com/docs/react-testing-library/intro
+import userEvent from '@testing-library/user-event';
+import { pkg } from '../../settings';
+import uuidv4 from '../../global/js/utils/uuidv4';
+import { Coachmark, CoachmarkBeacon, CoachmarkOverlayElements } from '..';
+import { CoachmarkOverlayElement } from '.';
+import { BEACON_KIND } from '../Coachmark/utils/enums';
+
+const blockClass = `${pkg.prefix}--coachmark-overlay-element`;
+const componentName = CoachmarkOverlayElement.displayName;
+
+// values to use
+const dataTestId = uuidv4();
+const className = `class-${uuidv4()}`;
+
+const renderCoachmarkWithOverlayElement = ({ ...rest } = {}) =>
+  render(
+    <Coachmark
+      theme={'dark'}
+      align={'bottom'}
+      positionTune={{ x: 0, y: 0 }}
+      target={
+        <CoachmarkBeacon label="Show information" kind={BEACON_KIND.DEFAULT} />
+      }
+    >
+      <CoachmarkOverlayElements closeButtonLabel="Done">
+        <CoachmarkOverlayElement {...rest} />
+      </CoachmarkOverlayElements>
+    </Coachmark>
+  );
+
+describe(componentName, () => {
+  it('renders a component CoachmarkOverlayElement', () => {
+    renderCoachmarkWithOverlayElement({
+      title: 'Test title',
+      description: 'This is a test description.',
+      'data-testid': dataTestId,
+    });
+    const beaconOrButton = screen.getByRole('button', {
+      name: 'Show information',
+    });
+    userEvent.click(beaconOrButton);
+    expect(screen.getByTestId(dataTestId)).toHaveClass(blockClass);
+  });
+
+  it('has no accessibility violations', async () => {
+    const { container } = renderCoachmarkWithOverlayElement({
+      title: 'Test title',
+      description: 'This is a test description.',
+    });
+    const beaconOrButton = screen.getByRole('button', {
+      name: 'Show information',
+    });
+    userEvent.click(beaconOrButton);
+    await expect(container).toBeAccessible(componentName);
+    await expect(container).toHaveNoAxeViolations();
+  });
+
+  it('applies className to the containing node', () => {
+    renderCoachmarkWithOverlayElement({
+      title: 'Test title',
+      description: 'This is a test description.',
+      'data-testid': dataTestId,
+      className,
+    });
+    const beaconOrButton = screen.getByRole('button', {
+      name: 'Show information',
+    });
+    userEvent.click(beaconOrButton);
+    expect(screen.getByTestId(dataTestId)).toHaveClass(className);
+  });
+
+  it('adds additional props to the containing node', () => {
+    const tmpTestID = `coachmarkOverlayElement-${uuidv4()}`;
+    renderCoachmarkWithOverlayElement({
+      title: 'Test title',
+      description: 'This is a test description.',
+      'data-testid': tmpTestID,
+    });
+    const beaconOrButton = screen.getByRole('button', {
+      name: 'Show information',
+    });
+    userEvent.click(beaconOrButton);
+    screen.getByTestId(tmpTestID);
+  });
+
+  it('forwards a ref to an appropriate node', () => {
+    const ref = React.createRef();
+    renderCoachmarkWithOverlayElement({
+      title: 'Test title',
+      description: 'This is a test description.',
+      ref,
+    });
+    const beaconOrButton = screen.getByRole('button', {
+      name: 'Show information',
+    });
+    userEvent.click(beaconOrButton);
+    expect(ref.current).toHaveClass(blockClass);
+  });
+
+  it('adds the Devtools attribute to the containing node', () => {
+    renderCoachmarkWithOverlayElement({
+      title: 'Test title',
+      description: 'This is a test description.',
+      'data-testid': dataTestId,
+    });
+    const beaconOrButton = screen.getByRole('button', {
+      name: 'Show information',
+    });
+    userEvent.click(beaconOrButton);
+    expect(screen.getByTestId(dataTestId)).toHaveDevtoolsAttribute(
+      componentName
+    );
+  });
+});

a82c4578f1255bf77d14d025421d92d74e5320ec
David Menendez <damenend@gmail.com>
2023-09-21 13:59:25 -0500

    fix: datagrid pagination review fixes

--- packages/ibm-products/src/components/Datagrid/utils/DatagridPagination.js
+++ packages/ibm-products/src/components/Datagrid/utils/DatagridPagination.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 /**
- * Copyright IBM Corp. 2022, 2022
+ * Copyright IBM Corp. 2022, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -18,6 +18,7 @@ export const DatagridPagination = ({ state, setPageSize, gotoPage, rows }) => {
 
   return (
     <Pagination
+      {...state}
       page={state.pageIndex + 1} // react-table is zero-based
       pageSize={state.pageSize}
       pageSizes={state.pageSizes || [10, 20, 30, 40, 50]}

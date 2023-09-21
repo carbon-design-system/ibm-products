e02b098a4bb0b76172eca0c101f42d44e9afab66
Lee Chase <lee.chase@uk.ibm.com>
2023-09-21 20:20:43 +0100

    chore: automate reproduction of examples (#3444)
    * chore: automate reproduction of examples
    
    * chore: update following merge
    
    * chore: check gallery config for each PR
    
    * fix: update gallery config with dep updates
    
    * fix: spacing in pacakge.json
    
    * fix: upgrade dependency examples
    
    * chore: add gallery example gen documentation
    
    * fix: add example gallery min to generate
    
    * chore: fix generate example
    
    * chore: fix generate example
    
    * chore: add gallery configs and README
    
    * chore: ignore template files
    
    * chore: match examples deps with other packages
    
    * Update .github/workflows/pr.yml
    
    Co-authored-by: Elysia <elysia.hwang@gmail.com>
    
    ---------
    
    Co-authored-by: Elysia <elysia.hwang@gmail.com>

--- examples/carbon-for-ibm-products/Datagrid/src/main.jsx
+++ examples/carbon-for-ibm-products/Datagrid/src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.scss';
 
-import './config';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- examples/carbon-for-ibm-products/TagSet/src/Example/Example.jsx
+++ examples/carbon-for-ibm-products/TagSet/src/Example/Example.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
-import "./_example.scss";
+import './_example.scss';
 
 // config.js enables components that have not yet been reviewed/released
 // but which we want to use in their 'canary' form. Note that that has to
 // be done in an import so that it happens before all component imports.
 
-import { TagSet } from "@carbon/ibm-products";
-import { types as tagTypes } from "@carbon/react/es/components/Tag/Tag";
-import { DisplayBox } from "./components/DisplayBox";
+import { TagSet } from '@carbon/ibm-products';
+import { types as tagTypes } from '@carbon/react/es/components/Tag/Tag';
+import { DisplayBox } from './components/DisplayBox';
 
 const tags = [];
 for (let i = 0; i < 200; i++) {
@@ -23,10 +23,10 @@ export const Example = () => (
     <DisplayBox componentName={TagSet.displayName}>
       <TagSet
         tags={tags}
-        allTagsModalSearchLabel='Filter tags'
-        allTagsModalTile='All tags'
-        allTagsModalSearchPlaceholderText='Filter tags'
-        showAllTagsLabel='Show all tags'
+        allTagsModalSearchLabel="Filter tags"
+        allTagsModalTile="All tags"
+        allTagsModalSearchPlaceholderText="Filter tags"
+        showAllTagsLabel="Show all tags"
       />
     </DisplayBox>
   </>

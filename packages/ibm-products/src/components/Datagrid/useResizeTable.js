71e036775d3d7c59133f91bae45842969878dce4
Matt Gallo <mdgallo@us.ibm.com>
2023-11-07 14:03:11 -0500

    fix(Datagrid): infinite scroll review changes

--- packages/ibm-products/src/components/Datagrid/useResizeTable.js
+++ packages/ibm-products/src/components/Datagrid/useResizeTable.js
@@ -1,10 +1,10 @@
-/*
- * Licensed Materials - Property of IBM
- * 5724-Q36
- * (c) Copyright IBM Corp. 2020
- * US Government Users Restricted Rights - Use, duplication or disclosure
- * restricted by GSA ADP Schedule Contract with IBM Corp.
+/**
+ * Copyright IBM Corp. 2020, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
  */
+
 import { useLayoutEffect } from 'react';
 
 const useResizeTable = (hooks) => {
@@ -27,7 +27,7 @@ const useResizeTable = (hooks) => {
         };
       }
       return console.error(
-        ' Could not resize table, no support for ResizeObserver'
+        'Could not resize table, no support for ResizeObserver'
       );
     }, [handleResize, parent]);
   };

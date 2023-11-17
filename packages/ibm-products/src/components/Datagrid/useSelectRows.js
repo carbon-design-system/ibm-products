d903a0c4804c9846f76d6ce3793c5eb6b4d156b2
Matt Gallo <mdgallo@us.ibm.com>
2023-11-17 15:48:28 -0500

    feat(Datagrid): return select row data object (#3828)
    Co-authored-by: Elysia <elysia.hwang@gmail.com>

--- packages/ibm-products/src/components/Datagrid/useSelectRows.js
+++ packages/ibm-products/src/components/Datagrid/useSelectRows.js
@@ -12,6 +12,7 @@ import { TableSelectRow } from 'carbon-components-react';
 import { SelectAll } from './Datagrid/DatagridSelectAll';
 import { selectionColumnId } from './common-column-ids';
 import { pkg, carbon } from '../../settings';
+import { handleToggleRowSelected } from './Datagrid/addons/stateReducer';
 
 const blockClass = `${pkg.prefix}--datagrid`;
 const checkboxClass = `${blockClass}__checkbox-cell`;
@@ -70,6 +71,7 @@ const SelectRow = (datagridState) => {
     onRowSelect,
     columns,
     withStickyColumn,
+    dispatch,
   } = datagridState;
 
   const [windowSize, setWindowSize] = useState(window.innerWidth);
@@ -86,16 +88,17 @@ const SelectRow = (datagridState) => {
   const cellProps = cell.getCellProps();
   const isFirstColumnStickyLeft =
     columns[0]?.sticky === 'left' && withStickyColumn;
-  const onSelectHandler = (e) => {
-    e.stopPropagation(); // avoid triggering onRowClick
+  const onSelectHandler = (event) => {
+    event.stopPropagation(); // avoid triggering onRowClick
     if (radio) {
       toggleAllRowsSelected(false);
       if (onRadioSelect) {
         onRadioSelect(row);
       }
     }
-    onChange(e);
-    onRowSelect?.(row, e);
+    onChange(event);
+    onRowSelect?.(row, event);
+    handleToggleRowSelected(dispatch, row, event.target.checked);
   };
   const rowId = `${tableId}-${row.index}`;
   return (

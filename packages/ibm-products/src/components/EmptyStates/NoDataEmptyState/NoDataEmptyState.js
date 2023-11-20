2448cd1e29e03efe88beda581186c460183671e1
David Menendez <damenend@gmail.com>
2023-11-20 14:05:08 -0600

    fix: copy paste whoopsie

--- packages/ibm-products/src/components/EmptyStates/NoDataEmptyState/NoDataEmptyState.js
+++ packages/ibm-products/src/components/EmptyStates/NoDataEmptyState/NoDataEmptyState.js
@@ -100,7 +100,7 @@ NoDataEmptyState.propTypes = {
    * Empty state illustration theme variations.
    * To ensure you use the correct themed illustrations, you can conditionally specify light or dark
    * based on your app's current theme value. Example:
-   * `illustrationtitle={title} theme={appTheme === ('carbon--g100' || 'carbon--g90') ? 'dark' : 'light'}`
+   * `illustrationTheme theme={appTheme === ('carbon--g100' || 'carbon--g90') ? 'dark' : 'light'}`
    */
   illustrationTheme: PropTypes.oneOf(['light', 'dark']),
 

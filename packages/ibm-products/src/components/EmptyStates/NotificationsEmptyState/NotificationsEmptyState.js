aa7a3925b32a3c55d048ae9b2b709ef75e510bd7
David Menendez <damenend@gmail.com>
2023-11-20 14:29:33 -0600

    fix: copy paste whoopsie 2

--- packages/ibm-products/src/components/EmptyStates/NotificationsEmptyState/NotificationsEmptyState.js
+++ packages/ibm-products/src/components/EmptyStates/NotificationsEmptyState/NotificationsEmptyState.js
@@ -107,7 +107,7 @@ NotificationsEmptyState.propTypes = {
    * Empty state illustration theme variations.
    * To ensure you use the correct themed illustrations, you can conditionally specify light or dark
    * based on your app's current theme value. Example:
-   * `illustrationTheme theme={appTheme === ('carbon--g100' || 'carbon--g90') ? 'dark' : 'light'}`
+   * `illustrationTheme={appTheme === ('carbon--g100' || 'carbon--g90') ? 'dark' : 'light'}`
    */
   illustrationTheme: PropTypes.oneOf(['light', 'dark']),
 

29e5de9f655a6bec1150e8048655a5a87cd20068
Matt Gallo <mdgallo@us.ibm.com>
2023-12-14 14:54:49 -0500

    fix(Datagrid): re-apply filters during fetching state (#3955)
    * chore: investigating refresh filter bug
    
    * chore: more investigation
    
    * fix(Datagrid): reapply filters during fetching state
    
    * chore: remove console log
    
    * chore: cleanup story changes

--- packages/ibm-products/src/components/Datagrid/Datagrid/addons/Filtering/hooks/useFilters.js
+++ packages/ibm-products/src/components/Datagrid/Datagrid/addons/Filtering/hooks/useFilters.js
@@ -71,11 +71,13 @@ const useFilters = ({
   onCancel = () => {},
   panelOpen,
   autoHideFilters,
+  isFetching,
 }) => {
   /** State */
   const [filtersState, setFiltersState] = useState(
     getInitialStateFromFilters(filters, variation, reactTableFiltersState)
   );
+  const [fetchingReset, setFetchingReset] = useState(false);
 
   const [filtersObjectArray, setFiltersObjectArray] = useState(
     reactTableFiltersState
@@ -433,6 +435,20 @@ const useFilters = ({
     setAllFilters,
     revertToPreviousFilters,
   ]);
+
+  // Re-applies filters if the Datagrid goes into a fetching state while panel is open
+  // and has had filters changed without applying
+  useEffect(() => {
+    if (isFetching && !fetchingReset) {
+      setFiltersState(JSON.parse(prevFiltersRef.current));
+      setFiltersObjectArray(JSON.parse(prevFiltersRef.current));
+      setAllFilters(JSON.parse(prevFiltersObjectArrayRef.current));
+      setFetchingReset(true);
+    }
+    if (!isFetching) {
+      setFetchingReset(false);
+    }
+  }, [isFetching, reactTableFiltersState, setAllFilters, fetchingReset]);
 
   const cancel = () => {
     // Reverting to previous filters only applies when using batch actions

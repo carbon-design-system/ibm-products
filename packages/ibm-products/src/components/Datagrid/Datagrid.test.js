54e4aec7f119056bb6bf8c194c5e878798218adf
Matt Gallo <mdgallo@us.ibm.com>
2024-01-17 10:30:55 -0500

    chore: extract required filter props in utility for testing

--- packages/ibm-products/src/components/Datagrid/Datagrid.test.js
+++ packages/ibm-products/src/components/Datagrid/Datagrid.test.js
@@ -58,14 +58,12 @@ import namor from 'namor';
 
 import userEvent from '@testing-library/user-event';
 import { getInlineEditColumns } from './utils/getInlineEditColumns';
-import {
-  FilteringUsage,
-  filterProps,
-} from './Extensions/Filtering/Panel.stories';
+import { FilteringUsage } from './Extensions/Filtering/Panel.stories';
 import {
   FilteringUsage as FlyoutUsage,
   filterProps as flyoutProps,
 } from './Extensions/Filtering/Flyout.stories';
+import { filterProps as testFilterProps } from './utils/filterPropsForTesting';
 const { click, hover, unhover } = userEvent.setup({
   // delay: null, // prev version
   advanceTimers: jest.advanceTimersByTime,
@@ -2296,20 +2294,11 @@ describe(componentName, () => {
   };
 
   it('should test basic interactions of filter panel', async () => {
-    const updatedFilterProps = { ...filterProps };
-    // Removing certain properties to test default function parameters in FilterPanel
-    delete updatedFilterProps.panelTitle;
-    delete updatedFilterProps.closeIconDescription;
-    delete updatedFilterProps.updateMethod;
-    delete updatedFilterProps.primaryActionLabel;
-    delete updatedFilterProps.secondaryActionLabel;
-    delete updatedFilterProps.onPanelOpen;
-    delete updatedFilterProps.onPanelClose;
     render(
       <FilteringUsage
         defaultGridProps={{
           ...sharedFilterGridProps,
-          filterProps: updatedFilterProps,
+          filterProps: testFilterProps,
         }}
       />
     );

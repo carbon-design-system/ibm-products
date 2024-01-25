27631f28907435c38fe460c6cbbd2b663c83a7c1
Matt Gallo <mdgallo@us.ibm.com>
2024-01-24 19:14:38 -0500

    test(Datagrid): increase filtering test coverage (v1) (#4143)
    * test(Datagrid): increase filtering test coverage
    
    * fix(FilterSummary): change boolean proptype to bool
    
    ---------
    
    Co-authored-by: elysia <elysia.hwang@gmail.com>

--- packages/ibm-products/src/components/Datagrid/Datagrid/addons/Filtering/FilterPanel.js
+++ packages/ibm-products/src/components/Datagrid/Datagrid/addons/Filtering/FilterPanel.js
@@ -38,10 +38,19 @@ const componentClass = `${blockClass}-filter-panel`;
 
 const MotionActionSet = motion(ActionSet);
 
+const defaults = {
+  title: 'Filter',
+  closeIconDescription: 'Close filter panel',
+  primaryActionLabel: 'Apply',
+  secondaryActionLabel: 'Cancel',
+  searchLabelText: 'Filter search',
+  searchPlaceholder: 'Find filters',
+};
+
 const FilterPanel = ({
-  title = 'Filter',
-  closeIconDescription = 'Close filter panel',
-  updateMethod = BATCH,
+  title = defaults.title,
+  closeIconDescription = defaults.closeIconDescription,
+  updateMethod,
   filterSections,
   setAllFilters,
   onApply = () => {},
@@ -50,10 +59,10 @@ const FilterPanel = ({
   onPanelClose = () => {},
   showFilterSearch = false,
   filterPanelMinHeight = 600,
-  primaryActionLabel = 'Apply',
-  secondaryActionLabel = 'Cancel',
-  searchLabelText = 'Filter search',
-  searchPlaceholder = 'Find filters',
+  primaryActionLabel = defaults.primaryActionLabel,
+  secondaryActionLabel = defaults.secondaryActionLabel,
+  searchLabelText = defaults.searchLabelText,
+  searchPlaceholder = defaults.searchPlaceholder,
   reactTableFiltersState = [],
   isFetching = false,
 }) => {
@@ -195,6 +204,7 @@ const FilterPanel = ({
       actionSetRef.current?.getBoundingClientRect().height;
 
     const height = `calc(100vh - ${filterHeadingHeight}px - ${
+      /* istanbul ignore next */
       showFilterSearch ? filterSearchHeight : 0
     }px - ${updateMethod === BATCH ? actionSetHeight : 0}px)`;
 
@@ -232,6 +242,7 @@ const FilterPanel = ({
             onClick={closePanel}
           />
           {showFilterSearch && (
+            /* istanbul ignore next */
             <div ref={filterSearchRef} className={`${componentClass}__search`}>
               <Search
                 labelText={searchLabelText}

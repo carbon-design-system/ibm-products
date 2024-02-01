a2fefe0fa58ef7d20ca1646a83b7cb554b1aad0d
Afsal K <makafsal@gmail.com>
2024-02-01 18:27:01 +0530

    test(searchBar): implement tests for scopes view

--- packages/ibm-products/src/components/SearchBar/SearchBar.test.js
+++ packages/ibm-products/src/components/SearchBar/SearchBar.test.js
@@ -9,23 +9,41 @@ import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react'; // https://testing-library.com/docs/react-testing-library/intro
 
 import { pkg, carbon } from '../../settings';
+import uuidv4 from '../../global/js/utils/uuidv4';
 import { SearchBar } from '.';
 
 const blockClass = `${pkg.prefix}--search-bar`;
 const componentName = SearchBar.displayName;
-const ref = React.createRef();
+const dataTestId = uuidv4();
+const scopesTypeLabel = 'Scopes';
+
 const defaultProps = {
   clearButtonLabelText: 'Clear',
   placeHolderText: 'Search...',
   submitLabel: 'Search',
 };
 
+const scopes = [
+  {
+    id: 'scope-2',
+    text: 'Scope 2',
+  },
+  {
+    id: 'scope-1',
+    text: 'Scope 1',
+  },
+  {
+    id: 'scope-3',
+    text: 'Scope 3',
+  },
+];
+
 const renderComponent = (props = defaultProps) => {
-  render(<SearchBar {...props} ref={ref} />);
+  return render(<SearchBar {...props} />);
 };
 
 describe(componentName, () => {
-  it('renders SearchBar with default props', async () => {
+  it('renders with default props', async () => {
     renderComponent();
 
     const searchBox = screen.getByRole('searchbox');
@@ -58,7 +76,7 @@ describe(componentName, () => {
     expect(clearButton).toHaveClass(`${carbon.prefix}--search-close--hidden`);
   });
 
-  it('user types search text, submit button is enabled, and clicks on submit button, then clear the input field', async () => {
+  it('type search text, that enables submit button, and click submit button, then clear text field', async () => {
     const mockSubmitFn = jest.fn();
 
     renderComponent({ ...defaultProps, onSubmit: mockSubmitFn });
@@ -81,44 +99,90 @@ describe(componentName, () => {
     expect(mockSubmitFn).toHaveBeenCalled();
 
     fireEvent.click(clearButton);
-    
+
     expect(searchBox.value).toBe('');
     expect(submitButton).toBeDisabled();
     expect(clearButton).toHaveClass(`${carbon.prefix}--search-close--hidden`);
   });
 
-  // it('has no accessibility violations', async () => {
-  //   const { container } = render(<SearchBar> </SearchBar>);
-  //   expect(container).toBeAccessible(componentName);
-  //   expect(container).toHaveNoAxeViolations();
-  // });
+  it('renders with scopes multi-select dropdown', async () => {
+    renderComponent({
+      ...defaultProps,
+      scopes,
+      scopesTypeLabel,
+    });
 
-  // it(`renders children`, async () => {
-  //   render(<SearchBar>{children}</SearchBar>);
-  //   screen.getByText(children);
-  // });
+    const scopesListBox = screen.getByRole('combobox');
+    const scopesListBoxLabel = screen.getByText(scopesTypeLabel);
 
-  // it('applies className to the containing node', async () => {
-  //   render(<SearchBar className={className}> </SearchBar>);
-  //   expect(screen.getByRole('main')).toHaveClass(className);
-  // });
+    expect(scopesListBox).toBeInTheDocument();
+    expect(scopesListBoxLabel).toBeInTheDocument();
+  });
 
-  // it('adds additional props to the containing node', async () => {
-  //   render(<SearchBar data-testid={dataTestId}> </SearchBar>);
-  //   screen.getByTestId(dataTestId);
-  // });
+  it('click on scopes dropdown to open first, then close', async () => {
+    renderComponent({
+      ...defaultProps,
+      scopes,
+      scopesTypeLabel,
+    });
 
-  // it('forwards a ref to an appropriate node', async () => {
-  //   const ref = React.createRef();
-  //   render(<SearchBar ref={ref}> </SearchBar>);
-  //   expect(ref.current).toHaveClass(blockClass);
-  // });
+    const scopesListBox = screen.getByRole('combobox');
+    fireEvent.click(scopesListBox);
 
-  // it('adds the Devtools attribute to the containing node', async () => {
-  //   render(<SearchBar data-testid={dataTestId}> </SearchBar>);
+    expect(scopesListBox).toHaveAttribute('aria-expanded', 'true');
+    expect(scopesListBox).toHaveAttribute('aria-haspopup', 'listbox');
 
-  //   expect(screen.getByTestId(dataTestId)).toHaveDevtoolsAttribute(
-  //     componentName
-  //   );
-  // });
+    // List options in a <ul>
+    const listEl = screen.getByRole('listbox');
+    // Check <li>s with same length as scopes
+    expect(listEl.children).toHaveLength(scopes.length);
+
+    fireEvent.click(scopesListBox);
+
+    expect(scopesListBox).toHaveAttribute('aria-expanded', 'false');
+    expect(listEl.children).toHaveLength(0);
+  });
+
+  it('has no accessibility violations', async () => {
+    const { container } = renderComponent();
+
+    expect(container).toBeAccessible(componentName);
+    expect(container).toHaveNoAxeViolations();
+  });
+
+  it('applies className to the containing node', async () => {
+    const className = 'search-bar-main';
+    renderComponent({ ...defaultProps, className });
+
+    const form = document.getElementsByClassName(className)?.[0];
+    expect(form).toHaveClass(className);
+  });
+
+  it('adds additional props to the containing node', async () => {
+    renderComponent({
+      ...defaultProps,
+      ['data-testid']: dataTestId,
+    });
+
+    screen.getByTestId(dataTestId);
+  });
+
+  it('forwards a ref to an appropriate node', async () => {
+    const ref = React.createRef();
+
+    renderComponent({ ...defaultProps, ref });
+
+    expect(ref.current).toHaveClass(blockClass);
+  });
+
+  it('adds the Devtools attribute to the containing node', async () => {
+    renderComponent({
+      ...defaultProps,
+      ['data-testid']: dataTestId,
+    });
+
+    expect(screen.getByTestId(dataTestId)).toHaveDevtoolsAttribute(
+      componentName
+    );
+  });
 });

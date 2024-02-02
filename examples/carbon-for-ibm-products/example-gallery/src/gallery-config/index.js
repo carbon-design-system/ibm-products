ada95f6f34e73f7201b911fe61287772773b6fb9
Afsal K <makafsal@gmail.com>
2024-02-02 18:14:13 +0530

    docs(searchbar): update .gitignore file

--- examples/carbon-for-ibm-products/example-gallery/src/gallery-config/index.js
+++ examples/carbon-for-ibm-products/example-gallery/src/gallery-config/index.js
@@ -38,6 +38,7 @@ import imagePageHeader from './PageHeader--thumbnail.png';
 import imageProductiveCard from './ProductiveCard--thumbnail.png';
 import imageRemoveModal from './RemoveModal--thumbnail.png';
 import imageSaving from './Saving--thumbnail.png';
+import imageSearchBar from './SearchBar--thumbnail.png';
 import imageSidePanel from './SidePanel--thumbnail.png';
 import imageStatusIcon from './StatusIcon--thumbnail.png';
 import imageTagSet from './TagSet--thumbnail.png';
@@ -190,6 +191,11 @@ const config = [
     thumbnail: `url(${imageRemoveModal}`,
   },
   { label: 'Saving', directory: 'Saving', thumbnail: `url(${imageSaving}` },
+  {
+    label: 'Search-Bar',
+    directory: 'SearchBar',
+    thumbnail: `url(${imageSearchBar}`,
+  },
   {
     label: 'SidePanel',
     directory: 'SidePanel',

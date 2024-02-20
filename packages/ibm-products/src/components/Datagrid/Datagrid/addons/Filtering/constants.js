1a1c488447748f7b8f8972cbc123e52957c274ee
Matt Gallo <mdgallo@us.ibm.com>
2024-02-20 16:29:45 -0500

    feat(Datagrid): add support for multi select filter type (#4361)
    * chore: custom filter exploration continued
    
    * fix(DataGrid): add support for selectrow and nestedrow
    
    * chore: custom filter exploration
    
    * feat(Datagrid): add multi select option
    
    * chore: remove onClearFilters from prop types
    
    * chore: revert copyright year
    
    * chore: add multi select example to flyout
    
    * chore: revert nested/selection changes from branch
    
    * chore: update gallery config
    
    ---------
    
    Co-authored-by: Ratheesh Rajan <Ratheesh.Rajan@ibm.com>

--- packages/ibm-products/src/components/Datagrid/Datagrid/addons/Filtering/constants.js
+++ packages/ibm-products/src/components/Datagrid/Datagrid/addons/Filtering/constants.js
@@ -19,6 +19,7 @@ export const NUMBER = 'number';
 export const CHECKBOX = 'checkbox';
 export const RADIO = 'radio';
 export const DROPDOWN = 'dropdown';
+export const MULTISELECT = 'multiSelect';
 
 /** Constants for event emitters */
 export const CLEAR_FILTERS = 'clearFilters';

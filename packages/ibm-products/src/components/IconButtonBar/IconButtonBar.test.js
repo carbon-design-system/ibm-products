b4466229878c6ced187c9c7f7f5c78e3f5ccb9a6
Paul Balchin <Paul.Balchin@ibm.com>
2024-02-21 15:03:05 -0500

    fix: updated tests

--- packages/ibm-products/src/components/IconButtonBar/IconButtonBar.test.js
+++ packages/ibm-products/src/components/IconButtonBar/IconButtonBar.test.js
@@ -11,51 +11,56 @@ import { render, screen } from '@testing-library/react'; // https://testing-libr
 import { pkg } from '../../settings';
 import uuidv4 from '../../global/js/utils/uuidv4';
 
-import { IconButtonBar } from '.';
+import { Add } from '@carbon/react/icons';
+import { IconButtonBar, IconButtonBarItem } from '.';
 
 const blockClass = `${pkg.prefix}--icon-button-bar`;
 const componentName = IconButtonBar.displayName;
 
 // values to use
-const children = `hello, world (${uuidv4()})`;
+const children = (
+  <IconButtonBarItem label="Add">
+    <Add size={16} />
+  </IconButtonBarItem>
+);
 const className = `class-${uuidv4()}`;
 const dataTestId = uuidv4();
 
 describe(componentName, () => {
   it('renders a component IconButtonBar', async () => {
-    render(<IconButtonBar> </IconButtonBar>);
-    expect(screen.getByRole('main')).toHaveClass(blockClass);
+    render(<IconButtonBar>{children}</IconButtonBar>);
+    expect(screen.getByRole('toolbar')).toHaveClass(blockClass);
   });
 
   it('has no accessibility violations', async () => {
-    const { container } = render(<IconButtonBar> </IconButtonBar>);
+    const { container } = render(<IconButtonBar>{children}</IconButtonBar>);
     expect(container).toBeAccessible(componentName);
     expect(container).toHaveNoAxeViolations();
   });
 
   it(`renders children`, async () => {
     render(<IconButtonBar>{children}</IconButtonBar>);
-    screen.getByText(children);
+    screen.getByRole('button');
   });
 
   it('applies className to the containing node', async () => {
-    render(<IconButtonBar className={className}> </IconButtonBar>);
-    expect(screen.getByRole('main')).toHaveClass(className);
+    render(<IconButtonBar className={className}>{children}</IconButtonBar>);
+    expect(screen.getByRole('toolbar')).toHaveClass(className);
   });
 
   it('adds additional props to the containing node', async () => {
-    render(<IconButtonBar data-testid={dataTestId}> </IconButtonBar>);
+    render(<IconButtonBar data-testid={dataTestId}>{children}</IconButtonBar>);
     screen.getByTestId(dataTestId);
   });
 
   it('forwards a ref to an appropriate node', async () => {
     const ref = React.createRef();
-    render(<IconButtonBar ref={ref}> </IconButtonBar>);
+    render(<IconButtonBar ref={ref}>{children}</IconButtonBar>);
     expect(ref.current).toHaveClass(blockClass);
   });
 
   it('adds the Devtools attribute to the containing node', async () => {
-    render(<IconButtonBar data-testid={dataTestId}> </IconButtonBar>);
+    render(<IconButtonBar data-testid={dataTestId}>{children}</IconButtonBar>);
 
     expect(screen.getByTestId(dataTestId)).toHaveDevtoolsAttribute(
       componentName

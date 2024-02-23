3f9f400d01cb288fa3279b8ecdf0b1de45efa697
Lily Peng <lily.peng@ibm.com>
2024-02-23 14:46:30 -0500

    [feat] add unit tests

--- packages/ibm-products/src/components/Nav/Nav.js
+++ packages/ibm-products/src/components/Nav/Nav.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
--- packages/ibm-products/src/components/Nav/Nav.test.js
+++ packages/ibm-products/src/components/Nav/Nav.test.js
@@ -6,30 +6,35 @@
  */
 
 import React from 'react';
-import { render, screen } from '@testing-library/react'; // https://testing-library.com/docs/react-testing-library/intro
+import { render, screen, fireEvent } from '@testing-library/react'; // https://testing-library.com/docs/react-testing-library/intro
 
 import { pkg } from '../../settings';
 import uuidv4 from '../../global/js/utils/uuidv4';
 
-import { Nav } from '.';
-import { NavList } from '.';
-import { NavItem } from '.';
+import { Nav, NavItem, NavList } from '.';
 
 const blockClass = `${pkg.prefix}--nav`;
 const componentName = Nav.displayName;
+const navListBlockClass = `${pkg.prefix}--nav-list`;
 
-// values to use
-// const children = `hello, world (${uuidv4()})`;
 const className = `class-${uuidv4()}`;
 const dataTestId = uuidv4();
 
-const labelText = `label (${uuidv4()})`;
+const navLabel = `label (${uuidv4()})`;
 
 const renderComponent = ({ ...rest } = {}) =>
-  render(<Nav {...rest} label={labelText} />);
+  render(
+    <Nav className={className} heading={navLabel} label={navLabel} {...rest}>
+      <NavList title="Nav list 1">
+        <NavItem href="#1-1">Nav item 1-1</NavItem>
+      </NavList>
+      <NavList title="Nav list 2">
+        <NavItem href="#2-1">Nav item 2-1</NavItem>
+      </NavList>
+    </Nav>
+  );
 
-// Skip for now
-xdescribe(componentName, () => {
+describe(componentName, () => {
   it('renders a component Nav', async () => {
     const { container } = renderComponent();
     expect(container).toBeInTheDocument();
@@ -42,10 +47,11 @@ xdescribe(componentName, () => {
     expect(container).toHaveNoAxeViolations();
   });
 
-  // it(`renders children`, async () => {
-  //   render(<Nav label="label">{children}</Nav>);
-  //   screen.getByText(children);
-  // });
+  it('renders children', async () => {
+    renderComponent();
+    const foundText = screen.getAllByText(`Nav item 1-1`);
+    expect(foundText.length).toBeGreaterThanOrEqual(1);
+  });
 
   it('applies className to the containing node', async () => {
     const { container } = renderComponent({ className });
@@ -55,7 +61,7 @@ xdescribe(componentName, () => {
 
   it('adds additional props to the containing node', async () => {
     renderComponent({ 'data-testid': dataTestId });
-    screen.getByTestId(dataTestId);
+    expect(screen.getByTestId(dataTestId)).toBeInTheDocument();
   });
 
   it('forwards a ref to an appropriate node', async () => {
@@ -71,18 +77,30 @@ xdescribe(componentName, () => {
     );
   });
 
-  it(`renders heading`, async () => {
+  it('renders heading', async () => {
     const headingText = `heading (${uuidv4()})`;
     renderComponent({ heading: headingText });
-    screen.getByText(headingText);
+    expect(screen.getByText(headingText)).toBeInTheDocument();
   });
 
   it('adds aria-label to the component', async () => {
     renderComponent();
-    expect(screen.getByRole('navigation')).toHaveAccessibleName(labelText);
-    screen.getByLabelText(labelText);
+    expect(screen.getByRole('navigation')).toHaveAccessibleName(navLabel);
+    expect(screen.getByText(navLabel)).toBeInTheDocument();
   });
 
-  describe(NavList.displayName, () => {});
-  describe(NavItem.displayName, () => {});
+  it('expands NavList when clicked', async () => {
+    renderComponent();
+
+    expect(document.querySelector(`.${navListBlockClass}`)).not.toHaveClass(
+      `${pkg.prefix}--nav-item--expanded`
+    );
+
+    const menu = document.querySelector(`.${navListBlockClass}`);
+    fireEvent.click(menu);
+
+    expect(document.querySelector(`.${navListBlockClass}`)).toHaveClass(
+      `${pkg.prefix}--nav-item--expanded`
+    );
+  });
 });

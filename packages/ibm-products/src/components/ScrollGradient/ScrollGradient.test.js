073fdf1a6961ff257bd4f66206f79f307a4e8274
Jeff Longshore <Jeff.Longshore@ibm.com>
2024-02-28 18:24:32 -0600

    Tests running

--- packages/ibm-products/src/components/ScrollGradient/ScrollGradient.test.js
+++ packages/ibm-products/src/components/ScrollGradient/ScrollGradient.test.js
@@ -9,7 +9,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react'; // https://testing-library.com/docs/react-testing-library/intro
 
 import { pkg } from '../../settings';
-// import uuidv4 from '../../global/js/utils/uuidv4';
+import uuidv4 from '../../global/js/utils/uuidv4';
 
 import { ScrollGradient } from '.';
 
@@ -17,48 +17,74 @@ const blockClass = `${pkg.prefix}--scroll-gradient`;
 const componentName = ScrollGradient.displayName;
 
 // values to use
-// const children = `hello, world (${uuidv4()})`;
-// const className = `class-${uuidv4()}`;
-// const dataTestId = uuidv4();
+const className = `class-${uuidv4()}`;
+const dataTestId = uuidv4();
+const childDataTestId = `child-element-${uuidv4()}`;
+
+const contentBlockClass = `${blockClass}__content`;
+const storyCopy = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet rerum aliquid perferendis, nulla nam ad excepturi, iure earum dolorum ipsa quae cum voluptatibus nemo quis debitis, aperiam repudiandae dolore deleniti.`;
+
+const childrenContent = (
+  <div style={{ padding: 16 }} data-testid={childDataTestId}>
+    <p>{storyCopy}</p>
+    <p>{storyCopy}</p>
+    <p>{storyCopy}</p>
+    <p>{storyCopy}</p>
+    <p>{storyCopy}</p>
+    <p>{storyCopy}</p>
+  </div>
+);
+
+const renderComponent = ({ ...rest } = {}, children = childrenContent) =>
+  render(<ScrollGradient {...rest}>{children}</ScrollGradient>);
 
 describe(componentName, () => {
-  it.skip('renders a component ScrollGradient', async () => {
-    render(<ScrollGradient> </ScrollGradient>);
-    expect(screen.getByRole('main')).toHaveClass(blockClass);
+  it('renders a component ScrollGradient', async () => {
+    renderComponent({ 'data-testid': dataTestId });
+    expect(screen.getByTestId(dataTestId)).toHaveClass(blockClass);
   });
 
-  // it('has no accessibility violations', async () => {
-  //   const { container } = render(<ScrollGradient> </ScrollGradient>);
-  //   expect(container).toBeAccessible(componentName);
-  //   expect(container).toHaveNoAxeViolations();
-  // });
+  it('has no accessibility violations', async () => {
+    const { container } = renderComponent({ 'data-testid': dataTestId });
+    expect(container).toBeAccessible(componentName);
+    expect(container).toHaveNoAxeViolations();
+  });
 
-  // it(`renders children`, async () => {
-  //   render(<ScrollGradient>{children}</ScrollGradient>);
-  //   screen.getByText(children);
-  // });
+  it(`renders children`, async () => {
+    renderComponent({ 'data-testid': dataTestId });
+    screen.getByTestId(childDataTestId);
+  });
 
-  // it('applies className to the containing node', async () => {
-  //   render(<ScrollGradient className={className}> </ScrollGradient>);
-  //   expect(screen.getByRole('main')).toHaveClass(className);
-  // });
+  it('applies className to the containing node', async () => {
+    renderComponent({
+      className,
+      'data-testid': dataTestId,
+    });
+    expect(screen.getByTestId(dataTestId)).toHaveClass(className);
+  });
 
-  // it('adds additional props to the containing node', async () => {
-  //   render(<ScrollGradient data-testid={dataTestId}> </ScrollGradient>);
-  //   screen.getByTestId(dataTestId);
-  // });
+  it('adds additional props to the containing node', async () => {
+    renderComponent({ 'data-testid': dataTestId });
+    screen.getByTestId(dataTestId);
+  });
 
-  // it('forwards a ref to an appropriate node', async () => {
-  //   const ref = React.createRef();
-  //   render(<ScrollGradient ref={ref}> </ScrollGradient>);
-  //   expect(ref.current).toHaveClass(blockClass);
-  // });
+  it('forwards a ref to an appropriate node (root element)', async () => {
+    const ref = React.createRef();
+    renderComponent({ ref });
+    expect(ref.current).toHaveClass(blockClass);
+  });
 
-  // it('adds the Devtools attribute to the containing node', async () => {
-  //   render(<ScrollGradient data-testid={dataTestId}> </ScrollGradient>);
+  it('forwards a ref to an appropriate node (scroll element)', async () => {
+    const scrollingElement = React.createRef();
+    const scrollContainerElement = (el) => (scrollingElement.current = el);
+    renderComponent({ getScrollElementRef: scrollContainerElement });
+    expect(scrollingElement.current).toHaveClass(contentBlockClass);
+  });
 
-  //   expect(screen.getByTestId(dataTestId)).toHaveDevtoolsAttribute(
-  //     componentName
-  //   );
-  // });
+  it('adds the Devtools attribute to the containing node', async () => {
+    renderComponent({ 'data-testid': dataTestId });
+    expect(screen.getByTestId(dataTestId)).toHaveDevtoolsAttribute(
+      componentName
+    );
+  });
 });

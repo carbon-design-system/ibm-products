45f0b6d29d5e6276d013f94d86911315f48e2857
devadula-nandan <47176249+devadula-nandan@users.noreply.github.com>
2024-03-21 21:05:25 +0530

    reverting changes on actions prop to show the error

--- packages/ibm-products/src/components/ActionBar/ActionBar.js
+++ packages/ibm-products/src/components/ActionBar/ActionBar.js
@@ -25,11 +25,6 @@ import { ActionBarOverflowItems } from './ActionBarOverflowItems';
 const blockClass = `${pkg.prefix}--action-bar`;
 const componentName = 'ActionBar';
 
-// Default values for props
-const defaults = {
-  actions: Object.freeze([]),
-};
-
 // NOTE: the component SCSS is not imported here: it is rolled up separately.
 
 /**
@@ -40,7 +35,7 @@ export let ActionBar = React.forwardRef(
     {
       // The component props, in alphabetical order (for consistency).
 
-      actions = defaults.actions,
+      actions,
       className,
       maxVisible,
       menuOptionsClass,

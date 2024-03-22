3c9a35d2d9e788084042a8c87062b957e1e57f9c
Jeff Longshore <Jeff.Longshore@ibm.com>
2024-03-22 08:58:28 -0500

    fix: exporting description list child components

--- packages/ibm-products/src/global/js/package-settings.js
+++ packages/ibm-products/src/global/js/package-settings.js
@@ -94,6 +94,9 @@ const defaults = {
     DecoratorSingleButton: false,
     DecoratorDualButton: false,
     DescriptionList: false,
+    DescriptionListBody: false,
+    DescriptionListCell: false,
+    DescriptionListRow: false,
     SearchBar: false,
     UserAvatar: false,
 

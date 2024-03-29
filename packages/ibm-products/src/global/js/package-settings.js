ade8310e1d834a7e00fa82f55d02367e28ae71bb
Paul Balchin <52505287+paul-balchin-ibm@users.noreply.github.com>
2024-03-29 12:12:50 -0400

    feat(FilterPanel): added accordion and group (#4620)
    * feat: filter panel, added checkbox with overflow
    
    * fix: comment
    
    * fix: storybook styling
    
    * fix: menu button flicker
    
    * feat: filter panel, added accordion and group
    
    * fix: comment failing test
    
    * fix: clean up comment
    
    * fix: color
    
    * fix: format for undefined count
    
    * fix: review updates
    
    * chore: fix capitalization
    
    * chore: move filter panel checkbox with overflow
    
    Not sure why yet, but canary flags were not working
    
    * chore: re-add filter panel checkbox with overflow
    
    ---------
    
    Co-authored-by: Elysia <Elysia.Hwang@ibm.com>

--- packages/ibm-products/src/global/js/package-settings.js
+++ packages/ibm-products/src/global/js/package-settings.js
@@ -77,8 +77,11 @@ const defaults = {
     StatusIndicatorStep: false,
     ActionBar: false,
     FilterPanel: false,
+    FilterPanelAccordion: false,
+    FilterPanelAccordionItem: false,
     FilterPanelCheckbox: false,
     FilterPanelCheckboxWithOverflow: false,
+    FilterPanelGroup: false,
     FilterPanelLabel: false,
 
     /* new component flags here - comment used by generate CLI */

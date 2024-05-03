0e95764ee5079ba5177002008fbbe0888972e324
Ignacio Becerra <i1becerr@ucsd.edu>
2024-05-03 16:07:40 -0700

    refactor(Tearsheet): add TypeScript types

--- packages/ibm-products/src/components/EditTearsheet/EditTearsheet.tsx
+++ packages/ibm-products/src/components/EditTearsheet/EditTearsheet.tsx
@@ -5,13 +5,14 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, { forwardRef, useState, useRef, createContext } from 'react';
+import React, { forwardRef, ReactNode, useState, useRef, createContext, PropsWithChildren } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { Form, SideNav, SideNavItems, SideNavMenuItem } from '@carbon/react';
 import { pkg } from '../../settings';
 import { getDevtoolsProps } from '../../global/js/utils/devtools';
 import { TearsheetShell } from '../Tearsheet/TearsheetShell';
+import { prepareProps } from '../../global/js/utils/props-helper';
 
 const componentName = 'EditTearsheet';
 const blockClass = `${pkg.prefix}--tearsheet-edit`;
@@ -27,10 +28,95 @@ export const FormNumberContext = createContext(0);
 
 // Default values for props
 const defaults = {
-  verticalPosition: 'normal',
-  influencerWidth: 'narrow',
-  sideNavAriaLabel: 'Side navigation',
+  verticalPosition: 'normal' as const,
+  influencerWidth: 'narrow' as const,
+  sideNavAriaLabel: 'Side navigation' as const,
 };
+
+
+// Note that the descriptions here should be kept in sync with those for the
+// corresponding props for TearsheetNarrow and TearsheetShell components.
+interface EditTearsheetProps extends PropsWithChildren {
+  /**
+   * The cancel button text
+   */
+  cancelButtonText?: string;
+
+  /**
+   * The main content of the tearsheet
+   */
+  children: ReactNode;
+
+  /**
+   * An optional class or classes to be added to the outermost element.
+   */
+  className?: string;
+
+  /**
+   * A description of the flow, displayed in the header area of the tearsheet.
+   */
+  description: ReactNode;
+
+  /**
+   * Used to set the size of the influencer
+   */
+  influencerWidth?: 'narrow' | 'wide';
+
+  /**
+   * A label for the tearsheet, displayed in the header area of the tearsheet
+   * to maintain context for the tearsheet (e.g. as the title changes from page
+   * to page of a multi-page task).
+   */
+  label: ReactNode;
+
+  /**
+   * An optional handler that is called when the user closes the tearsheet (by
+   * clicking the close button, if enabled, or clicking outside, if enabled).
+   * Returning `false` here prevents the modal from closing.
+   */
+  onClose?: () => boolean | void;
+
+  /**
+   * An optional handler that is called when a user changes forms via clicking
+   * an influencer nav item.
+   * Returns the index of the selected form.
+   */
+  onFormChange?: ( formIndex: number ) => number;
+
+  /**
+   * Specify a handler for submitting the tearsheet.
+   */
+  onRequestSubmit: () => void;
+
+  /**
+   * Specifies whether the tearsheet is currently open.
+   */
+  open?: boolean;
+
+  /**
+   * Specifies the aria label for the SideNav from Carbon UIShell
+   */
+  sideNavAriaLabel?: string;
+
+  /**
+   * The submit button text
+   */
+  submitButtonText?: string;
+
+  /**
+   * The main title of the tearsheet, displayed in the header area.
+   */
+  title: ReactNode;
+
+  /**
+   * The position of the top of tearsheet in the viewport. The 'normal'
+   * position (the default) is a short distance down from the top of the
+   * viewport, leaving room at the top for a global header bar to show through
+   * from below. The 'lower' position provides a little extra room at the top
+   * to allow an action bar navigation or breadcrumbs to also show through.
+   */
+  verticalPosition?: 'normal' | 'lower';
+}
 
 /**
  * Use Tearsheet with medium to complex edits. See usage guidance for further information.
@@ -56,12 +142,12 @@ export let EditTearsheet = forwardRef(
 
       // Collect any other property values passed in.
       ...rest
-    },
-    ref
+    } : EditTearsheetProps,
+    ref 
   ) => {
     const [currentForm, setCurrentForm] = useState(0);
     const [formTitles, setFormTitles] = useState([]);
-    const contentRef = useRef();
+    const contentRef = useRef<HTMLDivElement>(null);
 
     const handleCurrentForm = (formIndex) => {
       setCurrentForm(formIndex);
@@ -97,37 +183,48 @@ export let EditTearsheet = forwardRef(
       );
     }
 
+    const actions= [
+      {
+        key: 'edit-action-button-submit',
+        label: submitButtonText,
+        onClick: onRequestSubmit,
+        kind: 'primary',
+      },
+      {
+        key: 'edit-action-button-cancel',
+        label: cancelButtonText,
+        onClick: onClose,
+        kind: 'ghost',
+      },
+    ];
+
     return (
       <TearsheetShell
-        {...rest}
-        {...getDevtoolsProps(componentName)}
-        actions={[
-          {
-            key: 'edit-action-button-submit',
-            label: submitButtonText,
-            onClick: onRequestSubmit,
-            kind: 'primary',
-          },
-          {
-            key: 'edit-action-button-cancel',
-            label: cancelButtonText,
-            onClick: onClose,
-            kind: 'ghost',
-          },
-        ]}
-        className={cx(blockClass, className)}
-        description={description}
-        hasCloseIcon={false}
-        influencer={defaultInfluencer()}
-        influencerPosition="left"
-        influencerWidth={influencerWidth}
-        label={label}
-        onClose={onClose}
-        open={open}
-        size="wide"
-        title={title}
-        verticalPosition={verticalPosition}
-        ref={ref}
+        {...{
+          ...getDevtoolsProps(componentName),
+          ...prepareProps(rest),
+          actions,
+          cancelButtonText,
+          children,
+          className: cx(blockClass, className),
+          description,
+          influencerPosition: "left",
+          influencerWidth,
+          label,
+          onClose,
+          open,
+          size: 'wide',
+          submitButtonText,
+          title,
+          verticalPosition,
+          onRequestSubmit,
+          onFormChange,
+          sideNavAriaLabel,
+          ...rest,
+          hasCloseIcon: false,
+          influencer: defaultInfluencer(),
+          ref
+        }}
       >
         <div className={`${blockClass}__content`} ref={contentRef} role="main">
           <Form>

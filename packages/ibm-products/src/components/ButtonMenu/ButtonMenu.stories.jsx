a9208c4d4924f64ec8c431568b46ba1b4bfa3f31
Matt Gallo <mdgallo@us.ibm.com>
2024-05-06 09:53:44 -0400

    build(storybook): update from webpack to vite to build storybook (#4987)
    * build(storybook): update from webpack to vite to build storybook
    
    * chore: update lock file
    
    * chore: add jsx as jest file extension
    
    * chore: add jsx as jest file extension
    
    * refactor: decouple jest and story dependencies
    
    * fix: pin version for addon-mdx-gfm
    
    * chore: remove default props
    
    * fix: add special regex matcher for ace-node files with suffixes
    
    * chore: use act from testing library

---
+++ packages/ibm-products/src/components/ButtonMenu/ButtonMenu.stories.jsx
@@ -0,0 +1,86 @@
+/**
+ * Copyright IBM Corp. 2021, 2024
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+
+import { action } from '@storybook/addon-actions';
+import { ActionableNotification } from '@carbon/react';
+
+import { ButtonMenu, ButtonMenuItem } from '.';
+// import mdx from './ButtonMenu.mdx';
+
+// import styles from './_storybook-styles.scss?inline';
+
+import { Add } from '@carbon/react/icons';
+
+export default {
+  title: 'IBM Products/Internal/ButtonMenu',
+  component: ButtonMenu,
+  tags: ['autodocs'],
+  // TODO: Define argTypes for props not represented by standard JS types.
+  // argTypes: {
+  //   egProp: { control: 'color' },
+  // },
+  parameters: {
+    // styles,
+    /*
+docs: {
+      page: mdx,
+    },
+*/
+  },
+};
+
+const Template = (args) => {
+  return (
+    <>
+      <ActionableNotification
+        title="Deprecation notice"
+        subtitle="This component is deprecated and will be removed in the next major version. Please migrate to Carbon’s MenuButton."
+        inline
+        kind="warning"
+        lowContrast
+        hideCloseButton
+        actionButtonLabel="See MenuButton"
+        statusIconDescription="deprecation notification"
+        onActionButtonClick={() => {
+          window.open(
+            'https://react.carbondesignsystem.com/?path=/docs/components-menubutton--overview'
+          );
+        }}
+        style={{ marginBottom: '1rem' }}
+      />
+      <ButtonMenu
+        label="Primary button"
+        menuAriaLabel="Primary button"
+        renderIcon={(props) => <Add size={16} {...props} />}
+        {...args}
+      >
+        <ButtonMenuItem
+          itemText="Option 1a"
+          onClick={action(`Click on Option 1`)}
+        />
+        <ButtonMenuItem
+          itemText="Option 2"
+          onClick={action(`Click on Option 2`)}
+        />
+        <ButtonMenuItem
+          itemText="Option 3"
+          onClick={action(`Click on Option 3`)}
+        />
+        <ButtonMenuItem
+          itemText="Option 4"
+          onClick={action(`Click on Option 4`)}
+          hasDivider
+        />
+      </ButtonMenu>
+    </>
+  );
+};
+
+export const buttonMenu = Template.bind({});
+buttonMenu.storyName = 'Button menu';

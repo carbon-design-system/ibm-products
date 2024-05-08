31aaae78eeccd08dcd18272fe16ddc2003807062
Anna Wen <54281166+annawen1@users.noreply.github.com>
2024-05-08 16:23:32 -0400

    test(NotificationsPanel): add default state avt test

---
+++ e2e/components/NotificationsPanel/NotificationsPanel-test.avt.e2e.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright IBM Corp. 2024, 2024
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+import { expect, test } from '@playwright/test';
+import { visitStory } from '../../test-utils/storybook';
+
+test.describe('NotificationsPanel @avt', () => {
+  test('@avt-default-state', async ({ page }) => {
+    await visitStory(page, {
+      component: 'NotificationsPanel',
+      id: 'ibm-products-patterns-notifications-notificationspanel--default',
+      globals: {
+        carbonTheme: 'white',
+      },
+    });
+    await expect(page).toHaveNoACViolations(
+      'NotificationsPanel @avt-default-state'
+    );
+  });
+});

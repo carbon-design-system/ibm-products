ad6010e93d5e3e7d1c8cc9344f3b250800aeec05
ariellalgilmore <ariellalgilmore@gmail.com>
2024-05-09 12:03:24 -0700

    feat(get-started): add avt test

---
+++ e2e/components/GetStarted/GetStarted-test.avt.e2e.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright IBM Corp. 2024, 2024
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+import { expect, test } from '@playwright/test';
+import { visitStory } from '../../test-utils/storybook';
+
+test.describe('GetStarted @avt', () => {
+  test('@avt-default-state', async ({ page }) => {
+    await visitStory(page, {
+      component: 'GetStarted',
+      // cspell:disable-next-line
+      id: 'ibm-products-components-cards-getstarted--default',
+      globals: {
+        carbonTheme: 'white',
+      },
+    });
+    await expect(page).toHaveNoACViolations('GetStarted @avt-default-state');
+  });
+});

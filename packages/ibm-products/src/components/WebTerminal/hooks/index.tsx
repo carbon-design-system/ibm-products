d18b79d6c707e50a44d06b4fedd9bbf2db9881c0
Anamika T S <anamika.t.s@ibm.com>
2024-05-09 13:39:41 +0530

    refactor(useWebTerminal): add typescript types

--- packages/ibm-products/src/components/WebTerminal/hooks/index.tsx
+++ packages/ibm-products/src/components/WebTerminal/hooks/index.tsx
@@ -5,15 +5,34 @@
 // LICENSE file in the root directory of this source tree.
 //
 
-import React, { useState, useCallback, createContext } from 'react';
+import React, { useState, useCallback, createContext, ReactNode } from 'react';
 import { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { pkg } from '../../../settings';
 
-export const WebTerminalContext = createContext();
+export interface WebTerminalContextType {
+  open: boolean;
+  openWebTerminal: () => void;
+  closeWebTerminal: () => void;
+  toggleWebTerminal: () => void;
+}
+
+export const WebTerminalContext = createContext<WebTerminalContextType | null>(
+  null
+);
 
 const componentName = 'WebTerminalProvider';
-export let WebTerminalProvider = ({ children }) => {
+
+interface WebTerminalProviderProps {
+  /**
+   * Provide your own terminal component as children to show up in the web terminal
+   */
+  children: ReactNode | ReactNode[];
+}
+
+export let WebTerminalProvider: React.FC<WebTerminalProviderProps> = ({
+  children,
+}) => {
   const [open, setOpen] = useState(false);
 
   const openWebTerminal = useCallback(() => setOpen(true), []);

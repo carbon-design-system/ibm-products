8c02ae05f022dcc60c1661055eb493fdf6da5e81
devadula-nandan <47176249+devadula-nandan@users.noreply.github.com>
2024-05-13 13:06:14 +0530

    test(CreateTearsheet): add default state avt test

---
+++ e2e/components/CreateFlows/CreateTearsheet-test.avt.e2e.js
@@ -0,0 +1,28 @@
+// cspell:words createtearsheet
+
+/**
+ * Copyright IBM Corp. 2024, 2024
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+import { expect, test } from '@playwright/test';
+import { visitStory } from '../../test-utils/storybook';
+
+test.describe('CreateTearsheet @avt', () => {
+  test('@avt-default-state', async ({ page }) => {
+    await visitStory(page, {
+      component: 'CreateTearsheet',
+      id: 'ibm-products-patterns-create-flows-createtearsheet--multi-step-tearsheet',
+      globals: {
+        carbonTheme: 'white',
+      },
+    });
+    await expect(page).toHaveNoACViolations(
+      'CreateTearsheet @avt-default-state'
+    );
+  });
+});

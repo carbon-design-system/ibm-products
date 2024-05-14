d96ea882e60f8a03327c6dbb155604fb17f4b56a
Kenny Lam <909118+kennylam@users.noreply.github.com>
2024-05-14 11:58:46 -0400

    test(tearsheet): add default avt test

---
+++ e2e/components/Tearsheet/Tearsheet-test.avt.e2e.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright IBM Corp. 2024
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+import { expect, test } from '@playwright/test';
+import { visitStory } from '../../test-utils/storybook';
+import { carbon } from '../../../packages/ibm-products/src/settings';
+
+test.describe('Tearsheet @avt', () => {
+  test('@avt-default-state', async ({ page }) => {
+    await visitStory(page, {
+      component: 'Tearsheet',
+      id: 'ibm-products-components-tearsheet--tearsheet',
+      globals: {
+        carbonTheme: 'white',
+      },
+    });
+
+    // const tearsheetElement = page.locator;
+    const modalElement = page.locator(`.${carbon.prefix}--modal.is-visible`);
+    await page.getByText('Open Tearsheet').click();
+    await modalElement.evaluate((element) =>
+      Promise.all(
+        element.getAnimations().map((animation) => animation.finished)
+      )
+    );
+    await expect(page).toHaveNoACViolations('Tearsheet @avt-default-state');
+  });
+});

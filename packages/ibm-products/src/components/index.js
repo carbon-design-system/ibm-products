689c0476720b45fca4266a3cc8c3fe7eae9ea0be
Matt Gallo <mdgallo@us.ibm.com>
2024-05-17 09:13:16 -0400

    fix: import feature flags file

--- packages/ibm-products/src/components/index.js
+++ packages/ibm-products/src/components/index.js
@@ -1,9 +1,11 @@
 //
-// Copyright IBM Corp. 2020, 2023
+// Copyright IBM Corp. 2020, 2024
 //
 // This source code is licensed under the Apache-2.0 license found in the
 // LICENSE file in the root directory of this source tree.
 //
+
+import '../feature-flags';
 
 export { AboutModal } from './AboutModal';
 export { APIKeyModal } from './APIKeyModal';

ebaa1c4b64a40844f54c2afbfe388ebd49823bd3
Anamika T S <47971732+anamikaanu96@users.noreply.github.com>
2024-05-21 20:13:14 +0530

    test(EditTearsheetForm): add default state AVT check (#5280)
    * test(EditTearsheetForm): add default state AVT check
    
    * test(EditTearsheetForm): revert unwanted change

---
+++ e2e/components/EditTearsheet/EditTearsheetForm-test.avt.e2e.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright IBM Corp. 2024
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+import { expect, test } from '@playwright/test';
+import { visitStory } from '../../test-utils/storybook';
+import { carbon } from '../../../packages/ibm-products/src/settings';
+
+test.describe('EditTearsheetForm @avt', () => {
+  test('@avt-default-state', async ({ page }) => {
+    await visitStory(page, {
+      component: 'EditTearsheetForm',
+      id: 'ibm-products-patterns-edit-and-update-edittearsheet--multi-form-edit-tearsheet',
+      globals: {
+        carbonTheme: 'white',
+      },
+    });
+    await page.getByText('Open EditTearsheet').click();
+    const modalElement = page.locator(`.${carbon.prefix}--modal.is-visible`);
+    await expect(modalElement).toBeVisible();
+    await modalElement.evaluate((element) =>
+      Promise.all(
+        element.getAnimations().map((animation) => animation.finished)
+      )
+    );
+    await expect(page).toHaveNoACViolations(
+      'EditTearsheetForm @avt-default-state'
+    );
+  });
+});

94ee5621e668d99d68b54de0f7b2ee4a7cc96260
Afsal K <makafsal@gmail.com>
2024-05-27 16:28:39 +0530

    fix(datagrid): remove emptyState props

--- packages/ibm-products/src/components/Datagrid/Datagrid/DatagridEmptyBody.tsx
+++ packages/ibm-products/src/components/Datagrid/Datagrid/DatagridEmptyBody.tsx
@@ -65,7 +65,7 @@ const DatagridEmptyBody = (datagridState) => {
               )}
             </>
           ) : (
-            <EmptyState {...(emptyStateProps as any)} />
+            <EmptyState />
           )}
         </TableCell>
       </TableRow>

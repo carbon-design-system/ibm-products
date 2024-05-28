73ab61a2ae772763422ff4558928730c7891c7c2
Afsal K <makafsal@gmail.com>
2024-05-28 18:06:59 +0530

    chore: revert test code

--- packages/ibm-products/src/components/Datagrid/Datagrid/Datagrid.tsx
+++ packages/ibm-products/src/components/Datagrid/Datagrid/Datagrid.tsx
@@ -5,23 +5,19 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from // ForwardedRef
-'react';
+import React, { ForwardedRef } from 'react';
 import PropTypes from 'prop-types';
-// import cx from 'classnames';
+import cx from 'classnames';
 
-// import { getDevtoolsProps } from '../../../global/js/utils/devtools';
+import { getDevtoolsProps } from '../../../global/js/utils/devtools';
 import { pkg } from '../../../settings';
-// import pconsole from '../../../global/js/utils/pconsole';
-// import { InlineEditProvider } from './addons/InlineEdit/InlineEditContext';
-// import { DatagridContent } from './DatagridContent';
-// import { FilterProvider } from './addons/Filtering/FilterProvider';
-import {
-  DataGridState,
-  // DatagridRow
-} from '../types';
+import pconsole from '../../../global/js/utils/pconsole';
+import { InlineEditProvider } from './addons/InlineEdit/InlineEditContext';
+import { DatagridContent } from './DatagridContent';
+import { FilterProvider } from './addons/Filtering/FilterProvider';
+import { DataGridState, DatagridRow } from '../types';
 
-// const blockClass = `${pkg.prefix}--datagrid`;
+const blockClass = `${pkg.prefix}--datagrid`;
 const componentName = 'Datagrid';
 
 export interface DatagridProps {
@@ -43,69 +39,69 @@ export interface DatagridProps {
 /**
  * The `Datagrid` component is an extension of Carbon's DataTable component. At the most basic level, the `Datagrid` component takes in columns and rows and renders a data table. There is a set of data table extensions which this component provides support for, these can be found [here](https://pages.github.ibm.com/cdai-design/pal/components/data-table/overview/). This component is data driven and allows you to choose what pieces will be included based on the hooks/plugins that are provided.
  */
-export let Datagrid = React.forwardRef(() =>
-  // { datagridState, title, ariaToolbarLabel, ...rest }: DatagridProps,
-  // ref: ForwardedRef<HTMLDivElement>
-  {
-    return <div>Hi</div>;
-    // if (!datagridState) {
-    //   pconsole.warn(
-    //     'Datagrid was not passed datagridState which is required to render this component.'
-    //   );
-    //   return null;
-    // }
+export let Datagrid = React.forwardRef(
+  (
+    { datagridState, title, ariaToolbarLabel, ...rest }: DatagridProps,
+    ref: ForwardedRef<HTMLDivElement>
+  ) => {
+    if (!datagridState) {
+      pconsole.warn(
+        'Datagrid was not passed datagridState which is required to render this component.'
+      );
+      return null;
+    }
 
-    // const {
-    //   withVirtualScroll,
-    //   DatagridPagination,
-    //   isFetching,
-    //   tableId,
-    //   filterProps,
-    //   className,
-    //   state: { filters },
-    // } = datagridState;
+    const {
+      withVirtualScroll,
+      DatagridPagination,
+      isFetching,
+      tableId,
+      filterProps,
+      className,
+      state: { filters },
+    } = datagridState;
 
-    // const rows = ((DatagridPagination && datagridState.page) ||
-    //   datagridState.rows) as DatagridRow[];
+    const rows = ((DatagridPagination && datagridState.page) ||
+      datagridState.rows) as DatagridRow[];
 
-    // const props = {
-    //   title,
-    //   datagridState,
-    //   ariaToolbarLabel,
-    // };
+    const props = {
+      title,
+      datagridState,
+      ariaToolbarLabel,
+    };
 
-    // return (
-    //   <FilterProvider filters={filters} filterProps={filterProps}>
-    //     <InlineEditProvider>
-    //       <div
-    //         {...rest}
-    //         id={tableId}
-    //         ref={ref}
-    //         className={cx(
-    //           className,
-    //           blockClass,
-    //           withVirtualScroll
-    //             ? `${blockClass}__datagridWrap`
-    //             : `${blockClass}__datagridWrap-simple`,
-    //           !isFetching && rows.length === 0
-    //             ? `${blockClass}__empty-state`
-    //             : ''
-    //         )}
-    //         {...getDevtoolsProps(componentName)}
-    //       >
-    //         {filterProps?.variation === 'panel' ? (
-    //           <div
-    //             className={`${blockClass}__datagridWithPanel ${blockClass}__displayFlex`}
-    //           >
-    //             <DatagridContent {...props} />
-    //           </div>
-    //         ) : (
-    //           <DatagridContent {...props} />
-    //         )}
-    //       </div>
-    //     </InlineEditProvider>
-    //   </FilterProvider>
-    // );
+    return (
+      <FilterProvider filters={filters} filterProps={filterProps}>
+        <InlineEditProvider>
+          <div
+            {...rest}
+            id={tableId}
+            ref={ref}
+            className={cx(
+              className,
+              blockClass,
+              withVirtualScroll
+                ? `${blockClass}__datagridWrap`
+                : `${blockClass}__datagridWrap-simple`,
+              !isFetching && rows.length === 0
+                ? `${blockClass}__empty-state`
+                : ''
+            )}
+            {...getDevtoolsProps(componentName)}
+          >
+            {filterProps?.variation === 'panel' ? (
+              <div
+                className={`${blockClass}__datagridWithPanel ${blockClass}__displayFlex`}
+              >
+                <DatagridContent {...props} />
+              </div>
+            ) : (
+              <DatagridContent {...props} />
+            )}
+          </div>
+        </InlineEditProvider>
+      </FilterProvider>
+    );
   }
 );
 
@@ -121,7 +117,6 @@ Datagrid.propTypes = {
    * Specify a label to be read by screen readers on the container node
    * 'aria-label' of the TableToolbar component.
    */
-  /**@ts-ignore */
   ariaToolbarLabel: PropTypes.string,
   /**
    * The data grid state, much of it being supplied by the useDatagrid hook

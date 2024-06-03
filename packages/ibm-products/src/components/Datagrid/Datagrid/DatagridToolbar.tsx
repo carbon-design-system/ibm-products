f033011464c732501533dc4112d6ce747fe19166
Afsal K <makafsal@gmail.com>
2024-06-03 17:13:05 +0530

    refactor: test deploy error

--- packages/ibm-products/src/components/Datagrid/Datagrid/DatagridToolbar.tsx
+++ packages/ibm-products/src/components/Datagrid/Datagrid/DatagridToolbar.tsx
@@ -5,28 +5,22 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, {
-  useRef, //   // MutableRefObject, // {
-  //   //  useEffect,
-  //   useRef,
-  //   //  useState
-  // }
-} from 'react';
+import React, { useRef, MutableRefObject, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import {
+  /**@ts-ignore */
   TableToolbar,
-  //   //   //   TableBatchActions,
-  //   //   //   TableBatchAction,
-  //   //   //   MenuButton,
-  //   //   //   MenuItem,
-} from '@carbon/react/lib';
+  /**@ts-ignore */
+  TableBatchActions,
+  /**@ts-ignore */
+  TableBatchAction,
+  MenuButton,
+  MenuItem,
+} from '@carbon/react';
 import { useResizeObserver } from '../../../global/js/hooks/useResizeObserver';
-import {
-  pkg,
-  // carbon
-} from '../../../settings';
+import { pkg, carbon } from '../../../settings';
 import cx from 'classnames';
-// import { handleSelectAllRowData } from './addons/stateReducer';
+import { handleSelectAllRowData } from './addons/stateReducer';
 import { DataGridState } from '../types';
 
 const blockClass = `${pkg.prefix}--datagrid__table-toolbar`;
@@ -35,179 +29,177 @@ interface DatagridToolbarProps {
   ariaToolbarLabel?: string;
 }
 
-// const DatagridBatchActionsToolbar = (
-//   datagridState: DataGridState,
-//   width: number,
-//   ref: MutableRefObject<any>
-// ) => {
-//   console.log(ref);
-//   // const [displayAllInMenu, setDisplayAllInMenu] = useState(false);
-//   // const [initialListWidth, setInitialListWidth] = useState(null);
-//   // const [receivedInitialWidth, setReceivedInitialWidth] = useState(false);
-//   // const {
-//   //   onSelectAllRows,
-//   //   state: { selectedRowIds },
-//   //   toggleAllRowsSelected,
-//   //   toolbarBatchActions,
-//   //   setGlobalFilter,
-//   //   rows,
-//   //   dispatch,
-//   //   getRowId,
-//   //   batchActionMenuButtonLabel,
-//   //   translateWithIdBatchActions,
-//   // } = datagridState;
-//   // const batchActionMenuButtonLabelText = batchActionMenuButtonLabel ?? 'More';
-//   // const selectedKeys = Object.keys(selectedRowIds || {});
-//   // const totalSelected = selectedKeys.length;
+const DatagridBatchActionsToolbar = (
+  datagridState: DataGridState,
+  width: number,
+  ref: MutableRefObject<any>
+) => {
+  const [displayAllInMenu, setDisplayAllInMenu] = useState(false);
+  const [initialListWidth, setInitialListWidth] = useState(null);
+  const [receivedInitialWidth, setReceivedInitialWidth] = useState(false);
+  const {
+    onSelectAllRows,
+    state: { selectedRowIds },
+    toggleAllRowsSelected,
+    toolbarBatchActions,
+    setGlobalFilter,
+    rows,
+    dispatch,
+    getRowId,
+    batchActionMenuButtonLabel,
+    translateWithIdBatchActions,
+  } = datagridState;
+  const batchActionMenuButtonLabelText = batchActionMenuButtonLabel ?? 'More';
+  const selectedKeys = Object.keys(selectedRowIds || {});
+  const totalSelected = selectedKeys.length;
 
-//   // Get initial width of batch actions container,
-//   // used to measure when all items are put inside
-//   // the ButtonMenu
-//   // useEffect(() => {
-//   //   if (totalSelected === 1 && !receivedInitialWidth) {
-//   //     const batchActionListWidth = ref?.current?.querySelector(
-//   //       `.${carbon.prefix}--action-list`
-//   //     ).offsetWidth;
-//   //     setInitialListWidth(batchActionListWidth);
-//   //     setReceivedInitialWidth(true);
-//   //   }
-//   // }, [totalSelected, receivedInitialWidth, ref]);
+  // Get initial width of batch actions container,
+  // used to measure when all items are put inside
+  // the ButtonMenu
+  useEffect(() => {
+    if (totalSelected === 1 && !receivedInitialWidth) {
+      const batchActionListWidth = ref?.current?.querySelector(
+        `.${carbon.prefix}--action-list`
+      ).offsetWidth;
+      setInitialListWidth(batchActionListWidth);
+      setReceivedInitialWidth(true);
+    }
+  }, [totalSelected, receivedInitialWidth, ref]);
 
-//   // useEffect(() => {
-//   //   const summaryWidth = ref?.current.querySelector(
-//   //     `.${carbon.prefix}--batch-summary`
-//   //   ).offsetWidth;
-//   //   if (width < summaryWidth + initialListWidth + 32) {
-//   //     setDisplayAllInMenu(true);
-//   //   } else {
-//   //     setDisplayAllInMenu(false);
-//   //   }
-//   // }, [width, ref, initialListWidth]);
+  useEffect(() => {
+    const summaryWidth = ref?.current.querySelector(
+      `.${carbon.prefix}--batch-summary`
+    ).offsetWidth;
+    if (width < summaryWidth + initialListWidth + 32) {
+      setDisplayAllInMenu(true);
+    } else {
+      setDisplayAllInMenu(false);
+    }
+  }, [width, ref, initialListWidth]);
 
-//   // const getSelectedRowData = () => {
-//   //   if (selectedKeys.length === 0) {
-//   //     return [];
-//   //   }
-//   //   return selectedKeys.map((rowIndex) => {
-//   //     const filteredRow = rows.filter(
-//   //       (row) => row.index === parseInt(rowIndex)
-//   //     );
-//   //     return filteredRow.length ? filteredRow[0] : [];
-//   //   });
-//   // };
+  const getSelectedRowData = () => {
+    if (selectedKeys.length === 0) {
+      return [];
+    }
+    return selectedKeys.map((rowIndex) => {
+      const filteredRow = rows.filter(
+        (row) => row.index === parseInt(rowIndex)
+      );
+      return filteredRow.length ? filteredRow[0] : [];
+    });
+  };
 
-//   // Render batch actions in ButtonMenu
-//   // const renderBatchActionOverflow = () => {
-//   //   const menuClass = `${blockClass}__button-menu`;
-//   //   const minWidthBeforeOverflowIcon = 380;
-//   //   // Do not render ButtonMenu when there are 3 or less items
-//   //   // and if there is enough available space to render all the items
-//   //   if (
-//   //     toolbarBatchActions &&
-//   //     toolbarBatchActions?.length <= 3 &&
-//   //     !displayAllInMenu
-//   //   ) {
-//   //     return;
-//   //   }
+  // Render batch actions in ButtonMenu
+  const renderBatchActionOverflow = () => {
+    const menuClass = `${blockClass}__button-menu`;
+    const minWidthBeforeOverflowIcon = 380;
+    // Do not render ButtonMenu when there are 3 or less items
+    // and if there is enough available space to render all the items
+    if (
+      toolbarBatchActions &&
+      toolbarBatchActions?.length <= 3 &&
+      !displayAllInMenu
+    ) {
+      return;
+    }
 
-//   //   const renderItem = (batchAction, index) => (
-//   //     <MenuItem
-//   //       key={`${batchAction.label}-${index}`}
-//   //       label={batchAction.label}
-//   //       onClick={(event) => onClickHandler(event, batchAction)}
-//   //     />
-//   //   );
+    const renderItem = (batchAction, index) => (
+      <MenuItem
+        key={`${batchAction.label}-${index}`}
+        label={batchAction.label}
+        onClick={(event) => onClickHandler(event, batchAction)}
+      />
+    );
 
-//   //   return (
-//   //     <MenuButton
-//   //       label={batchActionMenuButtonLabelText}
-//   //       className={cx([
-//   //         menuClass,
-//   //         {
-//   //           [`${menuClass}-icon-only`]: width <= minWidthBeforeOverflowIcon,
-//   //         },
-//   //       ])}
-//   //       tabIndex={totalSelected > 0 ? 0 : -1}
-//   //       menuAlignment="bottom"
-//   //     >
-//   //       {toolbarBatchActions?.map((batchAction, index) => {
-//   //         const hidden = index < 2 && !displayAllInMenu;
-//   //         if (!hidden) {
-//   //           return renderItem(batchAction, index);
-//   //         }
-//   //       })}
-//   //     </MenuButton>
-//   //   );
-//   // };
+    return (
+      <MenuButton
+        label={batchActionMenuButtonLabelText}
+        className={cx([
+          menuClass,
+          {
+            [`${menuClass}-icon-only`]: width <= minWidthBeforeOverflowIcon,
+          },
+        ])}
+        tabIndex={totalSelected > 0 ? 0 : -1}
+        menuAlignment="bottom"
+      >
+        {toolbarBatchActions?.map((batchAction, index) => {
+          const hidden = index < 2 && !displayAllInMenu;
+          if (!hidden) {
+            return renderItem(batchAction, index);
+          }
+        })}
+      </MenuButton>
+    );
+  };
 
-//   // const onClickHandler = (event, batchAction) => {
-//   //   batchAction.onClick(getSelectedRowData(), event);
-//   //   if (batchAction.type === 'select_all') {
-//   //     toggleAllRowsSelected(true);
-//   //   }
-//   // };
+  const onClickHandler = (event, batchAction) => {
+    batchAction.onClick(getSelectedRowData(), event);
+    if (batchAction.type === 'select_all') {
+      toggleAllRowsSelected(true);
+    }
+  };
 
-//   // const onCancelHandler = () => {
-//   //   handleSelectAllRowData({
-//   //     dispatch,
-//   //     rows: [],
-//   //     getRowId,
-//   //     isChecked: false,
-//   //     indeterminate: undefined,
-//   //   });
-//   //   toggleAllRowsSelected(false);
-//   //   setGlobalFilter?.(null);
-//   // };
+  const onCancelHandler = () => {
+    handleSelectAllRowData({
+      dispatch,
+      rows: [],
+      getRowId,
+      isChecked: false,
+      indeterminate: undefined,
+    });
+    toggleAllRowsSelected(false);
+    setGlobalFilter?.(null);
+  };
 
-//   // const onSelectAllHandler = () => {
-//   //   toggleAllRowsSelected(true);
-//   //   onSelectAllRows?.(true);
-//   //   handleSelectAllRowData({
-//   //     dispatch,
-//   //     rows,
-//   //     getRowId,
-//   //     indeterminate: undefined,
-//   //     isChecked: undefined,
-//   //   });
-//   // };
+  const onSelectAllHandler = () => {
+    toggleAllRowsSelected(true);
+    onSelectAllRows?.(true);
+    handleSelectAllRowData({
+      dispatch,
+      rows,
+      getRowId,
+      indeterminate: undefined,
+      isChecked: undefined,
+    });
+  };
 
-//   // Only display the first two batch actions, the rest are
-//   // displayed inside of the ButtonMenu if there are more than
-//   // 3 batch actions
-//   return <div>hello...</div>;
-//   // return (
-//   //   <TableBatchActions
-//   //     shouldShowBatchActions={totalSelected > 0}
-//   //     totalSelected={totalSelected}
-//   //     onCancel={onCancelHandler}
-//   //     onSelectAll={onSelectAllHandler}
-//   //     totalCount={rows && rows.length}
-//   //     translateWithId={translateWithIdBatchActions}
-//   //   >
-//   //     {!displayAllInMenu &&
-//   //       toolbarBatchActions &&
-//   //       toolbarBatchActions?.map((batchAction, index) => {
-//   //         if (
-//   //           (index < 2 && toolbarBatchActions.length > 3) ||
-//   //           (index < 3 && toolbarBatchActions.length <= 3)
-//   //         ) {
-//   //           return (
-//   //             <TableBatchAction
-//   //               key={`${batchAction.label}-${index}`}
-//   //               renderIcon={batchAction.renderIcon}
-//   //               onClick={(event) => onClickHandler(event, batchAction)}
-//   //               iconDescription={batchAction.label}
-//   //               tabIndex={totalSelected > 0 ? 0 : -1}
-//   //             >
-//   //               {batchAction.label}
-//   //             </TableBatchAction>
-//   //           );
-//   //         }
-//   //       })}
-//   //     {renderBatchActionOverflow()}
-//   //   </TableBatchActions>
-//   // );
-// };
+  // Only display the first two batch actions, the rest are
+  // displayed inside of the ButtonMenu if there are more than
+  // 3 batch actions
+  return (
+    <TableBatchActions
+      shouldShowBatchActions={totalSelected > 0}
+      totalSelected={totalSelected}
+      onCancel={onCancelHandler}
+      onSelectAll={onSelectAllHandler}
+      totalCount={rows && rows.length}
+      translateWithId={translateWithIdBatchActions}
+    >
+      {!displayAllInMenu &&
+        toolbarBatchActions &&
+        toolbarBatchActions?.map((batchAction, index) => {
+          if (
+            (index < 2 && toolbarBatchActions.length > 3) ||
+            (index < 3 && toolbarBatchActions.length <= 3)
+          ) {
+            return (
+              <TableBatchAction
+                key={`${batchAction.label}-${index}`}
+                renderIcon={batchAction.renderIcon}
+                onClick={(event) => onClickHandler(event, batchAction)}
+                iconDescription={batchAction.label}
+                tabIndex={totalSelected > 0 ? 0 : -1}
+              >
+                {batchAction.label}
+              </TableBatchAction>
+            );
+          }
+        })}
+      {renderBatchActionOverflow()}
+    </TableBatchActions>
+  );
+};
 
 const DatagridToolbar = ({
   ariaToolbarLabel,
@@ -225,17 +217,15 @@ const DatagridToolbar = ({
       className={cx([blockClass, `${blockClass}--${getRowHeight}`])}
     >
       <TableToolbar aria-label={ariaToolbarLabel}>
-        <div>hello 1</div>
-        {/* {<DatagridActions {...datagridState} />} */}
-        {/* {DatagridBatchActionsToolbar?.(datagridState, width, ref)} */}
+        {<DatagridActions {...datagridState} />}
+        {DatagridBatchActionsToolbar?.(datagridState, width, ref)}
       </TableToolbar>
     </div>
   ) : DatagridActions ? (
     <div className={blockClass}>
       <TableToolbar aria-label={ariaToolbarLabel}>
-        <div>hello 2</div>
-        {/* {<DatagridActions {...datagridState} />} */}
-        {/* {DatagridBatchActions?.(datagridState)} */}
+        {<DatagridActions {...datagridState} />}
+        {DatagridBatchActions?.(datagridState)}
       </TableToolbar>
     </div>
   ) : null;

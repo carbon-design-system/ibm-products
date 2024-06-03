f6f5571fd09fa50b4335a8a84b5ad2ee12954886
Afsal K <makafsal@gmail.com>
2024-06-03 10:59:59 +0530

    refactor: test deploy error

--- packages/ibm-products/src/components/Datagrid/Datagrid/Datagrid.tsx
+++ packages/ibm-products/src/components/Datagrid/Datagrid/Datagrid.tsx
@@ -5,23 +5,19 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from // ForwardedRef
-'react';
+import React, { ForwardedRef } from 'react';
 import PropTypes from 'prop-types';
-// import cx from 'classnames';
+import cx from 'classnames';
 
-// import { getDevtoolsProps } from '../../../global/js/utils/devtools';
+import { getDevtoolsProps } from '../../../global/js/utils/devtools';
 import { pkg } from '../../../settings';
 import pconsole from '../../../global/js/utils/pconsole';
 import { InlineEditProvider } from './addons/InlineEdit/InlineEditContext';
 // import { DatagridContent } from './DatagridContent';
 import { FilterProvider } from './addons/Filtering/FilterProvider';
-import {
-  DataGridState,
-  // DatagridRow
-} from '../types';
+import { DataGridState, DatagridRow } from '../types';
 
-// const blockClass = `${pkg.prefix}--datagrid`;
+const blockClass = `${pkg.prefix}--datagrid`;
 const componentName = 'Datagrid';
 
 export interface DatagridProps {
@@ -44,44 +40,44 @@ export interface DatagridProps {
  * The `Datagrid` component is an extension of Carbon's DataTable component. At the most basic level, the `Datagrid` component takes in columns and rows and renders a data table. There is a set of data table extensions which this component provides support for, these can be found [here](https://pages.github.ibm.com/cdai-design/pal/components/data-table/overview/). This component is data driven and allows you to choose what pieces will be included based on the hooks/plugins that are provided.
  */
 export let Datagrid = React.forwardRef(
-  ({
-    datagridState,
-  }: // title, ariaToolbarLabel,
-  //  ...rest
-  DatagridProps) =>
-    // ref: ForwardedRef<HTMLDivElement>
+  (
     {
-      if (!datagridState) {
-        pconsole.warn(
-          'Datagrid was not passed datagridState which is required to render this component.'
-        );
-        return;
-      }
+      datagridState,
+      // title, ariaToolbarLabel,
+      ...rest
+    }: DatagridProps,
+    ref: ForwardedRef<HTMLDivElement>
+  ) => {
+    if (!datagridState) {
+      pconsole.warn(
+        'Datagrid was not passed datagridState which is required to render this component.'
+      );
+      return;
+    }
 
-      const {
-        // withVirtualScroll,
-        // DatagridPagination,
-        // isFetching,
-        // tableId,
-        filterProps,
-        // className,
-        state,
-      } = datagridState;
+    const {
+      withVirtualScroll,
+      DatagridPagination,
+      isFetching,
+      tableId,
+      filterProps,
+      className,
+      state,
+    } = datagridState;
 
-      // const rows = ((DatagridPagination && datagridState.page) ||
-      //   datagridState.rows) as DatagridRow[];
+    const rows = ((DatagridPagination && datagridState.page) ||
+      datagridState.rows) as DatagridRow[];
 
-      // const props = {
-      //   title,
-      //   datagridState,
-      //   ariaToolbarLabel,
-      // };
+    // const props = {
+    //   title,
+    //   datagridState,
+    //   ariaToolbarLabel,
+    // };
 
-      return (
-        <FilterProvider filters={state?.filters} filterProps={filterProps}>
-          <InlineEditProvider>
-            <div>Hi</div>
-            {/* <div
+    return (
+      <FilterProvider filters={state?.filters} filterProps={filterProps}>
+        <InlineEditProvider>
+          <div
             {...rest}
             id={tableId}
             ref={ref}
@@ -97,7 +93,8 @@ export let Datagrid = React.forwardRef(
             )}
             {...getDevtoolsProps(componentName)}
           >
-            {filterProps?.variation === 'panel' ? (
+            Hello
+            {/* {filterProps?.variation === 'panel' ? (
               <div
                 className={`${blockClass}__datagridWithPanel ${blockClass}__displayFlex`}
               >
@@ -105,12 +102,12 @@ export let Datagrid = React.forwardRef(
               </div>
             ) : (
               <DatagridContent {...props} />
-            )}
-          </div> */}
-          </InlineEditProvider>
-        </FilterProvider>
-      );
-    }
+            )} */}
+          </div>
+        </InlineEditProvider>
+      </FilterProvider>
+    );
+  }
 );
 
 // Return a placeholder if not released and not enabled by feature flag

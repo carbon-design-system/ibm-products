25bc952d7564928f3f9391e93252cbf9102d73ee
Matt Gallo <mdgallo@us.ibm.com>
2024-06-04 15:05:06 -0400

    fix: address issues with create full page

--- packages/ibm-products/src/components/CreateFullPage/CreateFullPage.tsx
+++ packages/ibm-products/src/components/CreateFullPage/CreateFullPage.tsx
@@ -245,7 +245,8 @@ export let CreateFullPage = React.forwardRef(
     const [currentStep, setCurrentStep] = useState(1);
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [modalIsOpen, setModalIsOpen] = useState(false);
-    const previousState = usePreviousValue({ currentStep });
+    // eslint-disable-next-line ssr-friendly/no-dom-globals-in-react-fc
+    const previousState = usePreviousValue({ currentStep, open });
     const [isDisabled, setIsDisabled] = useState(false);
     const [onNext, setOnNext] = useState();
     const [onMount, setOnMount] = useState();

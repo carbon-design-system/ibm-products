578f1cfbc3b7815edb102dc1fd1f2740b32ce0d6
Matt Gallo <mdgallo@us.ibm.com>
2024-06-17 09:45:57 -0400

    fix: import feature flags to prevent them from being tree shaked away (#5516)

--- packages/ibm-products/src/global/js/hooks/usePortalTarget.js
+++ packages/ibm-products/src/global/js/hooks/usePortalTarget.js
@@ -4,6 +4,7 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import '../../../feature-flags';
 import { useState, useEffect, useCallback } from 'react';
 import { pkg } from '../../../settings';
 import { createPortal } from 'react-dom';

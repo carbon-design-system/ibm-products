1244225cd2531843542596fb332eb3355fc73c4e
Afsal K <makafsal@gmail.com>
2024-06-25 15:03:29 +0530

    test(tearsheet): implement avt for focus trap and focus return

--- e2e/components/Tearsheet/Tearsheet-test.avt.e2e.js
+++ e2e/components/Tearsheet/Tearsheet-test.avt.e2e.js
@@ -11,6 +11,8 @@ import { expect, test } from '@playwright/test';
 import { visitStory } from '../../test-utils/storybook';
 import { carbon } from '../../../packages/ibm-products/src/settings';
 
+test.describe.configure({ mode: 'parallel' });
+
 test.describe('Tearsheet @avt', () => {
   test('@avt-default-state', async ({ page }) => {
     await visitStory(page, {
@@ -29,5 +31,49 @@ test.describe('Tearsheet @avt', () => {
       )
     );
     await expect(page).toHaveNoACViolations('Tearsheet @avt-default-state');
+  });
+
+  test('@avt-focus-trap-and-return', async ({ page }) => {
+    await visitStory(page, {
+      component: 'Tearsheet',
+      id: 'ibm-products-components-tearsheet--return-focus-to-open-button',
+      globals: {
+        carbonTheme: 'white',
+      },
+    });
+
+    const modalElement = page.locator(`.${carbon.prefix}--modal.is-visible`);
+    const openButton = page.getByText('Open Tearsheet');
+
+    await page.keyboard.press('Tab');
+    await expect(openButton).toBeFocused();
+    await openButton.click();
+    await modalElement.evaluate((element) =>
+      Promise.all(
+        element.getAnimations().map((animation) => animation.finished)
+      )
+    );
+
+    await expect(page).toHaveNoACViolations(
+      'Tearsheet @avt-focus-trap-and-return'
+    );
+    await expect(page.locator('#tss-ft1')).toBeFocused();
+
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await expect(page.getByText('Replace')).toBeFocused();
+    await page.keyboard.press('Tab');
+    await expect(page.locator('#tss-ft1')).toBeFocused();
+
+    await page.keyboard.press('Shift+Tab');
+    await page.keyboard.press('Shift+Tab');
+    await page.keyboard.press('Shift+Tab');
+
+    const cancelButton = page.getByText('Cancel');
+    await expect(cancelButton).toBeFocused();
+    await cancelButton.click();
+    await expect(openButton).toBeFocused();
   });
 });

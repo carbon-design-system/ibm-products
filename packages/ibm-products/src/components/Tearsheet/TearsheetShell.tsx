0517cdbdd4e177bfb4a5cc7dea2c535aead0633d
Matt Gallo <mdgallo@us.ibm.com>
2024-06-28 16:29:21 -0400

    feat(eslint-ssr-plugin): adds `eslint-plugin-ssr-friendly` so we can catch ssr issues/bugs during development (#5409)
    * feat: add ssr eslint plugin
    
    * fix: address ssr issues discovered from eslint plugin
    
    * chore: remove console log
    
    * chore: remove more logs and add transitionend type
    
    * fix: address issues with create full page
    
    * chore: update reduced motion hook
    
    * chore: fix side panel animation errors
    
    * fix: web terminal reduced motion updates
    
    * chore: remove console log
    
    * fix: address ts issue
    
    * fix: update another ts issue
    
    * fix: address coachmark stack test
    
    * chore: merge main and resolve conflict

--- packages/ibm-products/src/components/Tearsheet/TearsheetShell.tsx
+++ packages/ibm-products/src/components/Tearsheet/TearsheetShell.tsx
@@ -561,6 +561,7 @@ TearsheetShell.displayName = componentName;
 export const portalType =
   typeof Element === 'undefined'
     ? PropTypes.object
+    // eslint-disable-next-line ssr-friendly/no-dom-globals-in-module-scope
     : PropTypes.instanceOf(Element);
 
 export const deprecatedProps = {

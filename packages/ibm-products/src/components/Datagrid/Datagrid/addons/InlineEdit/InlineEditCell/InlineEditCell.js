6221fa15fd4e76bf8e8dc277b8d41636a0260fe7
Matt Gallo <mdgallo@us.ibm.com>
2024-07-16 09:29:59 -0400

    feat: use autoAlign prop for dropdown editable cell

--- packages/ibm-products/src/components/Datagrid/Datagrid/addons/InlineEdit/InlineEditCell/InlineEditCell.js
+++ packages/ibm-products/src/components/Datagrid/Datagrid/addons/InlineEdit/InlineEditCell/InlineEditCell.js
@@ -331,8 +331,9 @@ export const InlineEditCell = ({
       <Dropdown
         id={cellId}
         label={cellLabel || 'Dropdown menu options'}
-        ariaLabel={cellLabel || 'Dropdown menu options'}
+        aria-label={cellLabel || 'Dropdown menu options'}
         {...inputProps}
+        autoAlign
         hideLabel
         style={{
           width: cell.column.totalWidth,

b719dbb5c26bcfc62c32996bddd2000b4a2afcaa
Anna Wen <54281166+annawen1@users.noreply.github.com>
2024-07-22 16:07:35 -0400

    refactor(EmptyState): implement types for EmptyState

--- packages/ibm-products/src/components/EmptyStates/EmptyState.tsx
+++ packages/ibm-products/src/components/EmptyStates/EmptyState.tsx
@@ -6,7 +6,7 @@
  */
 
 // Import portions of React that are needed.
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { EmptyStateV2 } from '.';
 
 // Other standard imports.
@@ -17,6 +17,8 @@ import { Button, Link } from '@carbon/react';
 import { getDevtoolsProps } from '../../global/js/utils/devtools';
 import '../../global/js/utils/props-helper';
 import { pkg } from '../../settings';
+import { ButtonProps } from '@carbon/react';
+import { CarbonIconType } from '@carbon/icons-react/lib/CarbonIcon';
 
 import { EmptyStateContent } from './EmptyStateContent';
 
@@ -30,58 +32,122 @@ export const defaults = {
   size: 'lg',
 };
 
+interface EmptyStateProps {
+  /**
+   * Empty state action button
+   */
+  action?: {
+    kind?: 'primary' | 'secondary' | 'tertiary';
+    renderIcon?: CarbonIconType;
+    onClick?: ButtonProps['onClick'];
+    text?: string;
+  };
+
+  /**
+   * Provide an optional class to be applied to the containing node.
+   */
+  className?: string;
+
+  /**
+   * Empty state illustration, specify the `src` for a provided illustration to be displayed. In the case of requiring a light and dark illustration of your own, simply pass the corresponding illustration based on the current theme of your application.
+   * For example: `illustration={appTheme === 'dark' ? darkIllustration : lightIllustration}`
+   */
+  illustration?: string;
+
+  /**
+   * The alt text for empty state svg images. If not provided , title will be used.
+   */
+  illustrationDescription?: string;
+
+  /**
+   * Designates the position of the illustration relative to the content
+   */
+  illustrationPosition?: 'top' | 'right' | 'bottom' | 'left';
+
+  /**
+   * Empty state link object
+   */
+  link?: {
+    text?: string | ReactNode;
+    href?: string;
+  };
+
+  /**
+   * Empty state size
+   */
+  size?: 'lg' | 'sm';
+
+  /**
+   * Empty state subtitle
+   */
+  subtitle?: string | ReactNode;
+
+  /**
+   * Empty state title
+   */
+  title: string | ReactNode;
+
+  /**
+   * Designates which version of the EmptyState component is being used.
+   * Refer to V2 documentation separately.
+   */
+  v2?: boolean;
+}
+
 /**
  * The `EmptyState` component follows the Carbon guidelines for empty states with some added specifications around illustration usage. For additional usage guidelines and documentation please refer to the links above.
  */
-export let EmptyState = React.forwardRef(({ v2 = false, ...props }, ref) => {
-  // todo: deprecate v1
-  if (v2) {
-    return <EmptyStateV2 {...props} />;
-  }
-  const {
-    action,
-    className,
-    illustration,
-    illustrationDescription,
-    illustrationPosition = defaults.position,
-    link,
-    size = defaults.size,
-    subtitle,
-    title,
-    ...rest
-  } = props;
-  return (
-    <div
-      {
-        // Pass through any other property values as HTML attributes.
-        ...rest
-      }
-      className={cx(blockClass, `${blockClass}-type--default`, className, {
-        [`${blockClass}-position--${illustrationPosition}`]: !!illustration,
-      })}
-      ref={ref}
-      {...getDevtoolsProps(componentName)}
-    >
-      {illustration && (
-        <img
-          src={illustration}
-          alt={illustrationDescription}
-          className={cx([
-            `${blockClass}__illustration`,
-            `${blockClass}__illustration--${size}`,
-          ])}
+export let EmptyState = React.forwardRef<HTMLDivElement, EmptyStateProps>(
+  ({ v2 = false, ...props }, ref) => {
+    // todo: deprecate v1
+    if (v2) {
+      return <EmptyStateV2 {...props} />;
+    }
+    const {
+      action,
+      className,
+      illustration,
+      illustrationDescription,
+      illustrationPosition = defaults.position,
+      link,
+      size = defaults.size,
+      subtitle,
+      title,
+      ...rest
+    } = props;
+    return (
+      <div
+        {
+          // Pass through any other property values as HTML attributes.
+          ...rest
+        }
+        className={cx(blockClass, `${blockClass}-type--default`, className, {
+          [`${blockClass}-position--${illustrationPosition}`]: !!illustration,
+        })}
+        ref={ref}
+        {...getDevtoolsProps(componentName)}
+      >
+        {illustration && (
+          <img
+            src={illustration}
+            alt={illustrationDescription}
+            className={cx([
+              `${blockClass}__illustration`,
+              `${blockClass}__illustration--${size}`,
+            ])}
+          />
+        )}
+        <EmptyStateContent
+          action={action}
+          link={link}
+          size={size}
+          subtitle={subtitle}
+          title={title}
         />
-      )}
-      <EmptyStateContent
-        action={action}
-        link={link}
-        size={size}
-        subtitle={subtitle}
-        title={title}
-      />
-    </div>
-  );
-});
+      </div>
+    );
+  }
+);
 
 // Return a placeholder if not released and not enabled by feature flag
 EmptyState = pkg.checkComponentEnabled(EmptyState, componentName);
@@ -112,6 +178,7 @@ EmptyState.propTypes = {
   /**
    * The alt text for custom provided illustrations
    */
+  /**@ts-ignore*/
   illustrationDescription: PropTypes.string.isRequired.if(
     ({ illustration }) => illustration
   ),

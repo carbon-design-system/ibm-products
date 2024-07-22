57f95818bc0739f934c1fb85b6c52a29fe950826
devadula-nandan <47176249+devadula-nandan@users.noreply.github.com>
2024-07-22 18:12:34 +0530

    test(pageHeader): more coverage

--- e2e/components/PageHeader/PageHeader-test.avt.e2e.js
+++ e2e/components/PageHeader/PageHeader-test.avt.e2e.js
@@ -59,20 +59,12 @@ test.describe('PageHeader @avt', () => {
     await page.locator(`.page-header-stories__dummy-content`).hover();
     await page.mouse.wheel(0, -170);
     await expect(pageTitle).not.toBeInViewport();
-    // TODO: enable this after fixing accessibility violations
-    // await expect(page).toHaveNoACViolations(
-    //   'PageHeader @avt-collapse-on-scroll'
-    // );
 
     // The header expands again when scrolled back to the top.
     await page.locator(`.page-header-stories__dummy-content`).hover();
     await page.mouse.wheel(0, -700);
     await page.waitForTimeout(100);
     await expect(pageTitle).toBeInViewport();
-    // TODO: enable this after fixing accessibility violations
-    // await expect(page).toHaveNoACViolations(
-    //   'PageHeader @avt-collapse-on-scroll'
-    // );
   });
 
   // Starts collapsed by default
@@ -95,7 +87,7 @@ test.describe('PageHeader @avt', () => {
     await expect(pageTitle).not.toBeInViewport();
     // TODO: enable this after fixing accessibility violations
     // await expect(page).toHaveNoACViolations(
-    //   'PageHeader @avt-collapse-on-scroll'
+    //   'PageHeader @avt-collapse-by-default'
     // );
 
     // The header expands when the page is scrolled to the top.
@@ -104,7 +96,7 @@ test.describe('PageHeader @avt', () => {
     await expect(pageTitle).toBeInViewport();
     // TODO: enable this after fixing accessibility violations
     // await expect(page).toHaveNoACViolations(
-    //   'PageHeader @avt-collapse-on-scroll'
+    //   'PageHeader @avt-collapse-by-default'
     // );
   });
 
@@ -127,10 +119,10 @@ test.describe('PageHeader @avt', () => {
     await expect(pageTitle).toBeInViewport();
     // TODO: enable this after fixing accessibility violations
     // await expect(page).toHaveNoACViolations(
-    //   'PageHeader @avt-collapse-on-scroll'
+    //   'PageHeader @avt-collapse-by-toggle-button'
     // );
 
-    // The header collapses when the cheveron button is toggled.
+    // The header collapses when the cheveron button is toggled close.
     await pressTabKey(page, 20);
     await expect(page.getByLabel('Collapse the page header')).toBeFocused();
     await page.keyboard.press('Enter');
@@ -138,22 +130,18 @@ test.describe('PageHeader @avt', () => {
     await expect(pageTitle).not.toBeInViewport();
     // TODO: enable this after fixing accessibility violations
     // await expect(page).toHaveNoACViolations(
-    //   'PageHeader @avt-collapse-on-scroll'
+    //   'PageHeader @avt-collapse-by-toggle-button'
     // );
 
-    // The header expands when the cheveron button is toggled.
+    // The header expands when the cheveron button is toggled open.
     await expect(page.getByLabel('Expand the page header')).toBeFocused();
     await page.keyboard.press('Enter');
     await page.waitForTimeout(300);
     await expect(pageTitle).toBeInViewport();
-    // TODO: enable this after fixing accessibility violations
-    // await expect(page).toHaveNoACViolations(
-    //   'PageHeader @avt-collapse-on-scroll'
-    // );
   });
 
-  // PageHeader buttons collapse into MenuButton on small screens
-  test('@avt-buttons-collapse-into-menubutton-on-small-screens', async ({
+  // PageHeader buttons move into MenuButton on small screens
+  test('@avt-header-buttons-move-to-menubutton-on-small-screens', async ({
     page,
   }) => {
     await visitStory(page, {
@@ -163,6 +151,10 @@ test.describe('PageHeader @avt', () => {
         carbonTheme: 'white',
       },
     });
+    // TODO: enable this after fixing accessibility violations
+    // await expect(page).toHaveNoACViolations(
+    //   'PageHeader @avt-header-buttons-move-to-menubutton-on-small-screens'
+    // );
     // renders all buttons on large screens by default
     await pressTabKey(page, 15);
     await expect(
@@ -177,10 +169,17 @@ test.describe('PageHeader @avt', () => {
       page.getByRole('button', { name: 'Primary button' })
     ).toBeFocused();
     // collapses into menu button on small screens
-    page.setViewportSize({ width: 1024, height: 768 });
-    await page.reload();
-    await page.waitForLoadState('networkidle');
-    pressTabKey(page, 7);
+    await page.setViewportSize({ width: 1024, height: 768 });
+    // reset focus to first focusable element
+    await page
+      .locator('.dev-prefix--c4p--breadcrumb-with-overflow__overflow-menu')
+      .focus();
+    // TODO: enable this after fixing accessibility violations
+    // await expect(page).toHaveNoACViolations(
+    //   'PageHeader @avt-header-buttons-move-to-menubutton-on-small-screens'
+    // );
+    await pressTabKey(page, 5);
+
     await expect(
       page.getByRole('button', { name: 'Page actions' })
     ).toBeFocused();
@@ -195,15 +194,130 @@ test.describe('PageHeader @avt', () => {
       page.getByRole('button', { name: 'Page actions' })
     ).toBeFocused();
   });
+
+  // PageHeader buttons change position when header collapsed
+  test('@avt-buttons-change-position-when-header-collapsed', async ({
+    page,
+  }) => {
+    await visitStory(page, {
+      component: 'PageHeader',
+      id: 'ibm-products-components-page-header-pageheader--fully-loaded-and-some',
+      globals: {
+        carbonTheme: 'white',
+      },
+    });
+    // TODO: enable this after fixing accessibility violations
+    // await expect(page).toHaveNoACViolations(
+    //   'PageHeader @avt-buttons-change-position-when-header-collapsed'
+    // );
+    // renders all buttons on large screens by default
+    await pressTabKey(page, 15);
+    await expect(
+      page.getByRole('button', { name: 'danger Danger button' })
+    ).toBeFocused();
+    await page.keyboard.press('Tab');
+    await expect(
+      page.getByRole('button', { name: 'Secondary button' })
+    ).toBeFocused();
+    await page.keyboard.press('Tab');
+    await expect(
+      page.getByRole('button', { name: 'Primary button' })
+    ).toBeFocused();
+    // reset focus to first focusable element
+    await page.getByRole('link', { name: 'Home page' }).focus();
+    // changes position when header collapsed
+    await page.locator(`.page-header-stories__dummy-content`).hover();
+    await page.mouse.wheel(0, 180);
+
+    // TODO: enable this after fixing accessibility violations
+    // await expect(page).toHaveNoACViolations(
+    //   'PageHeader @avt-buttons-change-position-when-header-collapsed'
+    // );
+    await pressTabKey(page, 4);
+    await expect(
+      page.getByRole('button', { name: 'Page actions...' })
+    ).toBeFocused();
+    await page.keyboard.press('Enter');
+    (await page.locator('*:focus').textContent()) === 'Primary button';
+    await page.keyboard.press('ArrowDown');
+    (await page.locator('*:focus').textContent()) === 'Secondary button';
+    await page.keyboard.press('ArrowDown');
+    (await page.locator('*:focus').textContent()) === 'Danger button';
+    await page.keyboard.press('Escape');
+    await expect(
+      page.getByRole('button', { name: 'Page actions' })
+    ).toBeFocused();
+  });
+
+  // action bar buttons move into MenuButton on small screens
+  test('@avt-action-buttons-move-to-menubutton-on-small-screens', async ({
+    page,
+  }) => {
+    await visitStory(page, {
+      component: 'PageHeader',
+      id: 'ibm-products-components-page-header-pageheader--fully-loaded-and-some',
+      globals: {
+        carbonTheme: 'white',
+      },
+    });
+    // TODO: enable this after fixing accessibility violations
+    // await expect(page).toHaveNoACViolations(
+    //   'PageHeader @avt-action-buttons-move-to-menubutton-on-small-screens'
+    // );
+    // renders all buttons on large screens by default
+    await pressTabKey(page, 5);
+    await expect(page.getByRole('tooltip').getByText('Action 1')).toBeVisible();
+    await page.keyboard.press('Tab');
+    await expect(page.getByRole('tooltip').getByText('Action 2')).toBeVisible();
+    await page.keyboard.press('Tab');
+    await expect(page.getByRole('tooltip').getByText('Action 3')).toBeVisible();
+    await pressTabKey(page, 5);
+    await expect(page.getByRole('tooltip').getByText('Action 8')).toBeVisible();
+    await page.keyboard.press('Tab');
+    await expect(page.getByRole('tooltip').getByText('Action 9')).toBeVisible();
+    await page.keyboard.press('Tab');
+    await expect(
+      page.getByRole('tooltip').getByText('Action 10')
+    ).toBeVisible();
+    // collapses into menu button on small screens
+    await page.setViewportSize({ width: 1024, height: 768 });
+    // reset focus to first focusable element
+    await page
+      .locator('.dev-prefix--c4p--breadcrumb-with-overflow__overflow-menu')
+      .focus();
+    // TODO: enable this after fixing accessibility violations
+    // await expect(page).toHaveNoACViolations(
+    //   'PageHeader @avt-action-buttons-move-to-menubutton-on-small-screens'
+    // );
+    await pressTabKey(page, 1);
+    await expect(page.getByRole('tooltip').getByText('Action 1')).toBeVisible();
+    await pressTabKey(page, 2);
+    await expect(page.getByRole('tooltip').getByText('Action 3')).toBeVisible();
+    await pressTabKey(page, 1);
+    await expect(
+      page.getByRole('button', { name: 'Show further action bar items' })
+    ).toBeFocused();
+    await page.keyboard.press('Enter');
+    (await page.locator('*:focus').textContent()) === 'Action 4';
+    await page.keyboard.press('ArrowDown');
+    (await page.locator('*:focus').textContent()) === 'Action 5';
+    await page.keyboard.press('ArrowDown');
+    (await page.locator('*:focus').textContent()) === 'Action 6';
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+    (await page.locator('*:focus').textContent()) === 'Action 10';
+    await page.keyboard.press('Escape');
+    await expect(
+      page.getByRole('button', { name: 'Show further action bar items' })
+    ).toBeFocused();
+  });
 });
 
 async function pressTabKey(page, number) {
   for (let i = 0; i < number; i++) {
     await page.keyboard.press('Tab');
+    await page.waitForTimeout(40);
   }
 }
-// async function waitForTransition(element) {
-//   await element.evaluate((el) =>
-//     Promise.all(el.getAnimations().map((animation) => animation.finished))
-//   );
-// }

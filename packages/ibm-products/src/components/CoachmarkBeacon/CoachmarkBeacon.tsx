b164de9172713458d6b6dcc97be9c61ba04bbf2b
Matt Gallo <mdgallo@us.ibm.com>
2024-08-07 21:46:32 -0400

    chore: use title instead of aria-label for coachmark beacon

--- packages/ibm-products/src/components/CoachmarkBeacon/CoachmarkBeacon.tsx
+++ packages/ibm-products/src/components/CoachmarkBeacon/CoachmarkBeacon.tsx
@@ -75,7 +75,8 @@ export let CoachmarkBeacon = React.forwardRef<
         aria-label={label}
         className={`${blockClass}__target`}
       >
-        <svg className={`${blockClass}__center`} aria-label={label}>
+        <svg className={`${blockClass}__center`}>
+          <title>{label}</title>
           <circle r={1} cx={38} cy={38} />
         </svg>
       </button>

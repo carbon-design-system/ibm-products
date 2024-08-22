13a8d28ba4cb2631a5ad5be9a94d79d5f36c77c7
Amal K Joy <153802538+amal-k-joy@users.noreply.github.com>
2024-08-22 23:21:03 +0530

    chore: storybook Organize component and patterns based on new IA (#5911)

--- packages/ibm-products/src/components/ExportModal/ExportModal.stories.jsx
+++ packages/ibm-products/src/components/ExportModal/ExportModal.stories.jsx
@@ -14,7 +14,7 @@ import wait from '../../global/js/utils/wait';
 import { StoryDocsPage } from '../../global/js/utils/StoryDocsPage';
 
 export default {
-  title: 'IBM Products/Patterns/Export/ExportModal',
+  title: 'IBM Products/Components/Export/ExportModal',
   component: ExportModal,
   tags: ['autodocs'],
   parameters: {

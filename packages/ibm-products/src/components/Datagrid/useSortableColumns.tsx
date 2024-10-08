a3232f69fe37ca0b1b8c961f880c9fe7278dd29d
Sangeetha Babu <58620134+sangeethababu9223@users.noreply.github.com>
2024-10-08 11:43:30 +0530

    fix(Datagrid): ci-check issue test

--- packages/ibm-products/src/components/Datagrid/useSortableColumns.tsx
+++ packages/ibm-products/src/components/Datagrid/useSortableColumns.tsx
@@ -11,7 +11,7 @@ import { pkg, carbon } from '../../settings';
 import { Button } from '@carbon/react';
 import { ArrowUp, ArrowDown, ArrowsVertical } from '@carbon/react/icons';
 import { SelectAll } from './Datagrid/DatagridSelectAll';
-import { DatagridAILabel } from './Datagrid/addons/AiLabel/DatagridAiLabel';
+import { DatagridAILabel } from './Datagrid/addons/AiLabel1/DatagridAiLabel1';
 import { Hooks, TableInstance } from 'react-table';
 import { DataGridState } from './types';
 

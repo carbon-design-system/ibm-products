f536eee8f851e188a41a7f3e3a86c53760d11ba5
Matt Gallo <mdgallo@us.ibm.com>
2024-10-30 16:14:56 -0400

    chore: add license

---
+++ packages/ibm-products/src/components/StepFlow/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright IBM Corp. 2024, 2024
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export { StepActions } from './StepActions';
+export { StepGroup } from './StepGroup';
+export { StepContext, useStepContext } from './stepContext';

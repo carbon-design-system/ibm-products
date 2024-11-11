48c10ccd67e2bde417fb111c67d32bbd7359a4c9
Anamika T S <anamika.t.s@ibm.com>
2024-11-11 12:48:06 +0530

    fix(useravatar): accessibility fixes and avt test

--- e2e/components/UserAvatar/UserAvatar-test.avt.e2e.js
+++ e2e/components/UserAvatar/UserAvatar-test.avt.e2e.js
@@ -23,4 +23,46 @@ test.describe('UserAvatar @avt', () => {
     });
     await expect(page).toHaveNoACViolations('UserAvatar @avt-default-state');
   });
+
+  test('@avt-focus-state', async ({ page }) => {
+    await visitStory(page, {
+      component: 'UserAvatar',
+      id: 'experimental-components-user-avatar-useravatar--default',
+      globals: {
+        carbonTheme: 'white',
+      },
+    });
+
+    await page.keyboard.press('Tab');
+    await expect(
+      page
+        .locator('section')
+        .filter({ hasText: "useTheme reveals theme: 'White" })
+        .getByLabel('Thomas J. Watson')
+    ).toBeFocused();
+  });
+
+  test('@avt-hover-state', async ({ page }) => {
+    await visitStory(page, {
+      component: 'UserAvatar',
+      id: 'experimental-components-user-avatar-useravatar--default',
+      globals: {
+        carbonTheme: 'white',
+      },
+    });
+
+    const tooltipTrigger = page
+      .locator('section')
+      .filter({ hasText: "useTheme reveals theme: 'White" })
+      .getByLabel('Thomas J. Watson');
+    await expect(tooltipTrigger).toBeVisible();
+    await tooltipTrigger.hover();
+    const tooltipContent = page
+      .getByText('TW Thomas J. Watson user profile')
+      .first();
+    await expect(tooltipContent).toBeVisible();
+    // Press ESCAPE key while hover is active
+    await page.keyboard.press('Escape');
+    await expect(tooltipContent).toBeHidden();
+  });
 });

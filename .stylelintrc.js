9dbce79e41301e19f82969872117b9f16426ef67
David Menendez <damenend@gmail.com>
2024-11-27 12:51:04 -0600

    chore: update stylelint-plugin-carbon-tokens

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['stylelint-config-carbon'],
+  plugins: ['stylelint-plugin-carbon-tokens'],
   rules: {
     'max-nesting-depth': null,
     'scss/no-global-function-names': null,
@@ -7,5 +8,11 @@ module.exports = {
     'scss/load-no-partial-leading-underscore': null,
     'scss/double-slash-comment-inline': null,
     'no-duplicate-selectors': null,
+
+    'carbon/layout-use': null,
+    'carbon/theme-use': null,
+    'carbon/type-use': null,
+    'carbon/motion-duration-use': null,
+    'carbon/motion-easing-use': null,
   },
 };

3ba71291b9caef34921419d7c253e14ee8168e02
Alexander Melo <Alexander.Melo@ibm.com>
2025-01-09 13:51:40 -0500

    fix: reverts framer-motion to v6

--- packages/ibm-products/src/components/FilterSummary/FilterSummary.tsx
+++ packages/ibm-products/src/components/FilterSummary/FilterSummary.tsx
@@ -158,7 +158,7 @@ const FilterSummary = React.forwardRef(
           [`${blockClass}__expanded`]: multiline,
         })}
       >
-        <AnimatePresence mode="wait">
+        <AnimatePresence exitBeforeEnter>
           {!multiline && renderTagSet('single')}
           {multiline && renderTagSet('multiline')}
         </AnimatePresence>

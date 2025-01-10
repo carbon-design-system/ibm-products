1fc958bebde0c011bc1a4c213dd65b348a5e159e
Alexander Melo <Alexander.Melo@ibm.com>
2025-01-10 11:00:57 -0500

    fix: type declarations

--- packages/ibm-products/src/custom-typings/index.d.ts
+++ packages/ibm-products/src/custom-typings/index.d.ts
@@ -212,13 +212,6 @@ declare module '@carbon/react' {
     usePrefix,
   } from '@carbon/react';
 }
-
-declare module '@carbon/type' {
-  export { ButtonProps } from '@carbon/type';
-}
-declare module '@carbon/react/icons' {
-  export { CarbonIconType } from '@carbon/react/icons';
-}
 declare module '@carbon/colors';
 declare module '@carbon/motion';
 declare module '@carbon/feature-flags';

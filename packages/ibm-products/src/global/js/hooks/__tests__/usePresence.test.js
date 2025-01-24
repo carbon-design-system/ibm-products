bf2ef0bd54689b5d2effee479de59db85e165ca5
Matt Gallo <mdgallo@us.ibm.com>
2025-01-24 14:10:45 -0500

    chore: add license

--- packages/ibm-products/src/global/js/hooks/__tests__/usePresence.test.js
+++ packages/ibm-products/src/global/js/hooks/__tests__/usePresence.test.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright IBM Corp. 2025, 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import React, { act } from 'react';
 import { usePresence } from '../usePresence';
 import { render, screen } from '@testing-library/react';

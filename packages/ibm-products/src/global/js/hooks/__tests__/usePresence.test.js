7e89644a55a74cd803160ad362933a9fa57b37c8
Matt Gallo <mdgallo@us.ibm.com>
2025-01-24 14:05:07 -0500

    test(usePresence): add test coverage for hook

---
+++ packages/ibm-products/src/global/js/hooks/__tests__/usePresence.test.js
@@ -0,0 +1,34 @@
+import React, { act } from 'react';
+import { usePresence } from '../usePresence';
+import { render, screen } from '@testing-library/react';
+
+const content = 'Component content';
+const defaultDuration = 1000; // specified in `usePresence.ts`
+// eslint-disable-next-line react/prop-types
+const TestComponent = ({ open }) => {
+  const { shouldRender } = usePresence(open);
+  return shouldRender ? <div>{content}</div> : null;
+};
+
+describe('usePresence', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it('should immediately render component', () => {
+    render(<TestComponent open />);
+    expect(screen.getByText(content)).toBeInTheDocument();
+  });
+  it('should wait to unmount component to account for animation duration', () => {
+    const { rerender, container } = render(<TestComponent open />);
+    rerender(<TestComponent open={false} />);
+    act(() => {
+      jest.advanceTimersByTime(defaultDuration);
+    });
+    expect(container.firstChild).toBeNull();
+  });
+});

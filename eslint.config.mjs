b86efb151272198bb497009ae857ccd0562ad6a2
Matt Gallo <mdgallo@us.ibm.com>
2025-01-29 22:49:09 -0500

    fix(eslint): filter out repeated ts-eslint plugin definitions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,24 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import eslint from "@eslint/js";
+import { fixupPluginRules } from "@eslint/compat";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import react from "eslint-plugin-react";
 import pluginSsrFriendly from 'eslint-plugin-ssr-friendly';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
+import tslintPlugin from '@typescript-eslint/eslint-plugin';
 
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
 import { FlatCompat } from '@eslint/eslintrc'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+  recommendedConfig: eslint.configs.recommended,
+  allConfig: eslint.configs.all,
 })
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   {
@@ -31,12 +30,15 @@ export default [
       }
     }
   },
-  pluginJs.configs.recommended,
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  // ...compat.extends("eslint-config-carbon").map(c => {
-  //   console.log(c.plugins)
-  // }),
-  pluginReact.configs.flat.recommended,
+  ...compat.extends("eslint-config-carbon").map(c => {
+    if (c.plugins?.['@typescript-eslint']) {
+      return {};
+    }
+    return c;
+  }),
+  react.configs.flat.recommended,
   {
     ignores: [
       'build',
@@ -59,15 +61,14 @@ export default [
   },
   {
     plugins: {
-      pluginSsrFriendly,
-      pluginReactHooks
+      'ssr-friendly': fixupPluginRules(pluginSsrFriendly),
+      'react-hooks': pluginReactHooks,
+      tslintPlugin
     },
     rules: {
       '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/no-unused-expressions': ['error', { allowTernary: true }],
-      'ssr-friendly/no-dom-globals-in-module-scope': [
-        0
-      ],
+      'ssr-friendly/no-dom-globals-in-module-scope': 'error',
       'react/display-name': [
         0
       ],

960a502be043256a4ef8b34f7f4293ab6ef3f9f4
Matt Gallo <mdgallo@us.ibm.com>
2025-01-31 10:35:54 -0500

    fix(prettier): add mjs extension to prettier list

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import globals from 'globals';
+import globals from "globals";
 import { fixupPluginRules } from '@eslint/compat';
 import { FlatCompat } from '@eslint/eslintrc'
 import eslint from '@eslint/js';

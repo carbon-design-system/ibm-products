2905420c4dae45f8b641aa8058c221cc969e8569
Matt Gallo <mdgallo@us.ibm.com>
2025-02-13 08:52:24 -0500

    refactor: remove lodash dependency (#6809)
    * refactor: remove lodash dependency
    
    * refactor(ScrollGradient): use intersection observer instead of onscroll
    
    * chore: mock intersection observer
    
    * test: mock intersection observer in index-all-enabled test
    
    * test(utils): add coverage for new utils, remove coachmark clamp
    
    * chore: copyright headers
    
    * test: update clamp fn
    
    * refactor(throttle): add default value for delay
    
    * fix: add type assertions
    
    ---------
    
    Co-authored-by: elysia <elysia.hwang@gmail.com>

--- packages/ibm-products/src/components/Datagrid/useStickyColumn.ts
+++ packages/ibm-products/src/components/Datagrid/useStickyColumn.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2021, 2023
+ * Copyright IBM Corp. 2021, 2025
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -12,7 +12,7 @@ import {
   useState,
   MutableRefObject,
 } from 'react';
-import debounce from 'lodash/debounce';
+import { debounce } from '../../global/js/utils/debounce';
 import cx from 'classnames';
 import { pkg } from '../../settings';
 import {

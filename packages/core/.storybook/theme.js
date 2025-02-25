ed22028d28e7e875d40b2dedcd7ac939cf6bf07c
Anamika T S <anamika.t.s@ibm.com>
2025-02-25 12:26:15 +0530

    fix(storybook): remove console statement

--- packages/core/.storybook/theme.js
+++ packages/core/.storybook/theme.js
@@ -3,8 +3,6 @@ import packageInfo from '../../ibm-products/package.json';
 
 const { description, version } = packageInfo;
 
-console.log(description, version);
-
 export default create({
   brandTitle: `${description} v${version}`,
   fontBase: "'IBM Plex Sans', 'Helvetica Neue', Arial, sans-serif",

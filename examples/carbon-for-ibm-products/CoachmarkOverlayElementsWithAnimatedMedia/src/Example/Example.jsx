7d14d3db9bc5647fc3218f44c3bf4be6f10c79ad
Amal K Joy <amal.joy.k@gmail.com>
2025-02-28 14:05:48 +0530

    chore: fix

--- examples/carbon-for-ibm-products/CoachmarkOverlayElementsWithAnimatedMedia/src/Example/Example.jsx
+++ examples/carbon-for-ibm-products/CoachmarkOverlayElementsWithAnimatedMedia/src/Example/Example.jsx
@@ -8,17 +8,51 @@ import React from 'react';
 // from the @carbon/ibm-products component library.
 // ----------------------------------------------------
 
-import { CoachmarkOverlayElements } from '@carbon/ibm-products';
+import {
+  CoachmarkOverlayElements,
+  Coachmark,
+  CoachmarkOverlayElement,
+  CoachmarkBeacon,
+} from '@carbon/ibm-products';
 
 import './_example.scss';
 import { SteppedAnimatedMedia } from '../SteppedAnimatedMedia/SteppedAnimatedMedia';
-const Anim1 = new URL('../SteppedAnimatedMedia/assets/illustrations/anim1.json', import.meta.url).pathname;
-const Anim2 = new URL('../SteppedAnimatedMedia/assets/illustrations/anim2.json', import.meta.url).pathname;
+const Anim1 = new URL(
+  '../SteppedAnimatedMedia/assets/illustrations/anim1.json',
+  import.meta.url
+).pathname;
+const Anim2 = new URL(
+  '../SteppedAnimatedMedia/assets/illustrations/anim2.json',
+  import.meta.url
+).pathname;
 
 export const Example = () => {
-  return <CoachmarkOverlayElements renderMedia= {({ playStep }) => (
-    <SteppedAnimatedMedia filePaths={[Anim1, Anim2]} playStep={playStep} />
-  )} />;
+  return (
+    <Coachmark
+      align={'bottom'}
+      positionTune={{ x: 0, y: 0 }}
+      target={<CoachmarkBeacon label="Show information" kind={'default'} />}
+      //theme={theme}
+    >
+      <CoachmarkOverlayElements
+        renderMedia={({ playStep }) => (
+          <SteppedAnimatedMedia
+            filePaths={[Anim1, Anim2]}
+            playStep={playStep}
+          />
+        )}
+      >
+        <CoachmarkOverlayElement
+          title="Example 1"
+          description="This is an example description."
+        />
+        <CoachmarkOverlayElement
+          title="Example 2"
+          description="This is another example description."
+        />
+      </CoachmarkOverlayElements>
+    </Coachmark>
+  );
 };
 
 export default Example;

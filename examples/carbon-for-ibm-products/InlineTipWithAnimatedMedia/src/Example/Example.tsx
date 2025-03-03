3ff32f479531e69beaec92703766cb835f1c4454
Amal K Joy <amal.joy.k@gmail.com>
2025-03-03 14:23:47 +0530

    chore: issue fix 2

--- examples/carbon-for-ibm-products/InlineTipWithAnimatedMedia/src/Example/Example.tsx
+++ examples/carbon-for-ibm-products/InlineTipWithAnimatedMedia/src/Example/Example.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-  InlineTip
-} from '@carbon/ibm-products';
+import { InlineTip } from '@carbon/ibm-products';
 
 import './_example.scss';
 import { SteppedAnimatedMedia } from '../SteppedAnimatedMedia/SteppedAnimatedMedia';
@@ -13,13 +11,25 @@ const InlineTipAnimation = new URL(
 export const Example = () => {
   return (
     <InlineTip
-    renderMedia={() => (
-      <SteppedAnimatedMedia
-        filePaths={[InlineTipAnimation]}
-        playStep={1}
-      />
-    )}
-  />
+      title="Use case-specific heading"
+      renderMedia={() => (
+        <SteppedAnimatedMedia filePaths={[InlineTipAnimation]} playStep={1} />
+      )}
+    >
+      <ul>
+        <li>
+          Use <b>case-specific</b> content that explains the concept or adds
+          context.
+        </li>
+        <li>
+          Use case-specific <i>content that</i> explains the concept or adds
+          context.
+        </li>
+        <li>
+          Use case-specific content that explains the concept or adds context.
+        </li>
+      </ul>
+    </InlineTip>
   );
 };
 

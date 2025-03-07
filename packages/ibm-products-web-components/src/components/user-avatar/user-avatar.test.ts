7b66b4644d21da1c0d4fd3a2a9ca9e7ab9c6ff4f
Anamika T S <anamika.t.s@ibm.com>
2025-03-07 16:21:13 +0530

    test(useravatar): resolve test-c4p

--- packages/ibm-products-web-components/src/components/user-avatar/user-avatar.test.ts
+++ packages/ibm-products-web-components/src/components/user-avatar/user-avatar.test.ts
@@ -8,7 +8,7 @@ import { expect, describe, it, vi } from 'vitest';
 import { fixture, html } from '@open-wc/testing';
 import CDSUseravatar from './user-avatar';
 import { prefix } from '../../globals/settings';
-import './index';
+import '@carbon/ibm-products-web-components/es/components/user-avatar/index';
 const mockedUserIcon = vi.fn().mockReturnValue({});
 
 vi.mock('@carbon/web-components/es/icons/user/16.js', () => mockedUserIcon);

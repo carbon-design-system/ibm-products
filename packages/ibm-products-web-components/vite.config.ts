ea4e691f8cd050ae9543004e86d057f2f10a465e
Afsal K <makafsal@gmail.com>
2025-03-12 17:09:57 +0530

    fix: change vite config

--- packages/ibm-products-web-components/vite.config.ts
+++ packages/ibm-products-web-components/vite.config.ts
@@ -30,7 +30,11 @@ export default defineConfig({
       provider: 'playwright',
       enabled: true,
       headless: true,
-      name: 'chromium',
+      instances: [
+        {
+          browser: 'chromium',
+        },
+      ],
     },
     coverage: {
       provider: 'v8',

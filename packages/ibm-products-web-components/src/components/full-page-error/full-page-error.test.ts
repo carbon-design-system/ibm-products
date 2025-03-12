6084309c1edad5fd350932662e7cd989b9545fb4
Alexander Melo <Alexander.Melo@ibm.com>
2025-03-12 11:23:59 -0400

    fix: removes import

--- packages/ibm-products-web-components/src/components/full-page-error/full-page-error.test.ts
+++ packages/ibm-products-web-components/src/components/full-page-error/full-page-error.test.ts
@@ -10,7 +10,7 @@ import { describe, it, vi, expect } from 'vitest';
 import { fixture, html, expect as owcExpect } from '@open-wc/testing';
 import { render } from 'lit';
 import { Kind } from './types';
-import '@carbon/ibm-products-web-components/es/components/full-page-error/full-page-error';
+// import '@carbon/ibm-products-web-components/es/components/full-page-error/full-page-error';
 import CDSFullPageError, { blockClass } from './full-page-error';
 
 const defaultProps = {

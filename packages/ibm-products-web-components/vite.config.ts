1200d18a10107d8236c294723312ae547a8584da
Afsal K <makafsal@gmail.com>
2025-03-12 14:52:31 +0530

    test: revert previous changes

--- packages/ibm-products-web-components/vite.config.ts
+++ packages/ibm-products-web-components/vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       include: ['src/**/*'],
-      exclude: ['src/**/*.stories.ts'],
+      exclude: ['src/**/*.stories.{js,ts}'],
       reporter: ['text', 'html'],
     },
   },

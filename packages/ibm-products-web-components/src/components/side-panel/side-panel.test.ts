a8290b0d21c7f80a09be9d297b6b705a6da192b1
Afsal K <makafsal@gmail.com>
2025-03-13 18:12:31 +0530

    test: remove vi import

--- packages/ibm-products-web-components/src/components/side-panel/side-panel.test.ts
+++ packages/ibm-products-web-components/src/components/side-panel/side-panel.test.ts
@@ -4,9 +4,7 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
-vi.mock('@carbon/icons/lib/close/20', () => vi.fn().mockReturnValue({}));
-import { expect, describe, it, vi, afterEach } from 'vitest';
+import { expect, describe, it, afterEach } from 'vitest';
 import { fixture, html, oneEvent } from '@open-wc/testing';
 import { SIDE_PANEL_PLACEMENT, SIDE_PANEL_SIZE } from './defs';
 import { prefix } from '../../globals/settings';

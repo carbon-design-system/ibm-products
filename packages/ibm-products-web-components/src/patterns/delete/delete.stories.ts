8c4e812971293c59dcabeafc6ed042b164c06742
Sinta Augustine <sinta@Sintas-MacBook-Pro.local>
2025-04-29 16:25:49 +0530

    feat(deletePattern): placeholder stories

---
+++ packages/ibm-products-web-components/src/patterns/delete/delete.stories.ts
@@ -0,0 +1,22 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2025, 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { html } from 'lit';
+
+export default {
+  title: 'Patterns/Delete',
+};
+
+export const highImpactDeletion = () => html`
+  <!-- <iframe
+    title="Deletion Pattern"
+    src="/examples/delete-pattern/index.html"
+  ></iframe> -->
+  <div>Render delete pattern here</div>
+`;

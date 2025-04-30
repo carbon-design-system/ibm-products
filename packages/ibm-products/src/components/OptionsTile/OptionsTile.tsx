2896fc6fcbe3142a5fc43bfbef029bcd0b33453e
Afsal K <makafsal@gmail.com>
2025-04-30 17:43:29 +0530

    fix(OptionsTile): change type of title prop

--- packages/ibm-products/src/components/OptionsTile/OptionsTile.tsx
+++ packages/ibm-products/src/components/OptionsTile/OptionsTile.tsx
@@ -96,7 +96,7 @@ export interface OptionsTileProps {
   /**
    * Provide the title for this OptionsTile.
    */
-  title: string;
+  title: string | object | ReactNode;
 
   /**
    * Optionally provide an id which should be used for the title.
@@ -424,7 +424,11 @@ OptionsTile.propTypes = {
   /**
    * Provide the title for this OptionsTile.
    */
-  title: PropTypes.string.isRequired,
+  title: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+    PropTypes.node,
+  ]).isRequired,
 
   /**
    * Optionally provide an id which should be used for the title.

259cb8be8147bc2e3c1e1bd2186c0db81e00f05b
Nandan Devadula <47176249+devadula-nandan@users.noreply.github.com>
2025-05-05 15:12:07 +0530

    feat: string formatter height overflow, re-use in tearsheet (#7345)
    * feat: string formatter height overflow, re-use in tearsheet
    
    * test: add test for the feature, and use it in narrow tearsheet
    
    * revert: remove non working test
    
    * fix: add dependency on container width
    
    * fix: merge refs and observe outer ref
    
    * chore: add more details and controls to the documentation
    
    * chore: update default control for tearsheet narrow
    
    * chore: revert default control for tearsheet narrow
    
    * chore: re-update default control for tearsheet narrow
    
    * fix: use refs instead of query selecting
    
    * chore: string formatter initial commit
    
    * Revert "chore: string formatter initial commit"
    intended for web component
    This reverts commit d44a58dd71a6579302dfa78d1b1103d83051797b.

--- packages/ibm-products/src/components/CreateTearsheetNarrow/CreateTearsheetNarrow.stories.jsx
+++ packages/ibm-products/src/components/CreateTearsheetNarrow/CreateTearsheetNarrow.stories.jsx
@@ -16,6 +16,7 @@ import {
   FormGroup,
 } from '@carbon/react';
 import { pkg } from '../../settings';
+import { StringFormatter } from '../StringFormatter/StringFormatter.js';
 import { CreateTearsheetNarrow } from '.';
 import styles from './_storybook-styles.scss?inline';
 import { StoryDocsPage } from '../../global/js/utils/StoryDocsPage';
@@ -30,6 +31,39 @@ export default {
   component: CreateTearsheetNarrow,
   tags: ['autodocs'],
   argTypes: {
+    description: {
+      control: {
+        type: 'select',
+        labels: {
+          0: 'With plain String',
+          1: 'With StringFormatter and 1 line',
+          2: 'With StringFormatter and 2 lines',
+        },
+        default: 0,
+      },
+      description:
+        'A description of the flow, displayed in the header area of the tearsheet.\n Note: `StringFormatter` can be passed as a React node to apply custom text formatting, including ellipsis truncation and a definition tooltip when the content is too long.',
+      options: [0, 1, 2],
+      mapping: {
+        0: 'Select the number of partitions you want to create',
+        1: (
+          <StringFormatter
+            lines={1}
+            truncate={true}
+            value="This is a description for the tearsheet, providing an opportunity to describe the flow over a couple of lines in the header of the tearsheet."
+            tooltipDirection="bottom"
+          />
+        ),
+        2: (
+          <StringFormatter
+            lines={2}
+            truncate={true}
+            value="This is a description for the tearsheet, providing an opportunity to describe the flow over a couple of lines in the header of the tearsheet."
+            tooltipDirection="bottom"
+          />
+        ),
+      },
+    },
     children: { control: { disable: true } },
     ...slugArgTypes(),
     ...decoratorArgTypes(),
@@ -49,7 +83,7 @@ const createTearsheetNarrowBlockClass = `${pkg.prefix}--create-tearsheet-narrow-
 const defaultStoryProps = {
   title: 'Create partition',
   className: 'test-class-name',
-  description: 'Select the number of partitions you want to create',
+  description: 0,
   formTitle: 'Core configuration',
   formDescription:
     'We recommend you fill out and evaluate these details at a minimum before deploying your topic.',

0cadf7cf5c40a62c1e55bbcf1822a7ac35d96d7e
David Menendez <damenend@gmail.com>
2025-05-14 15:12:18 -0500

    chore: move products storybook config from core to products (#7415) (#7505)
    * chore: move products storybook config from core to products
    
    * fix: update core to products in ci scripts
    
    * fix: add public
    
    * fix: storybook import paths
    
    * fix: add internal extension to internal stories
    
    * fix: simpleheader path
    
    * fix: path
    
    * fix: ci
    
    * fix: exclude internal from percy

--- packages/ibm-products/src/components/FilterPanel/FilterPanelAccordion/FilterPanelAccordion.stories.jsx
+++ packages/ibm-products/src/components/FilterPanel/FilterPanelAccordion/FilterPanelAccordion.stories.jsx
@@ -14,7 +14,7 @@ import mdx from './FilterPanelAccordion.mdx';
 import uuidv4 from '../../../global/js/utils/uuidv4';
 
 import styles from '../_storybook-styles.scss?inline';
-import { Annotation } from '../../../../../core/.storybook/Annotation';
+import { Annotation } from '../../../../.storybook/Annotation';
 
 const storyClass = 'filter-panel-stories';
 

a374f41ad9bcc1f65e20fcf5c749aee1bf0f9e64
David Menendez <damenend@gmail.com>
2025-05-22 12:30:33 -0500

    chore: overflow handler web component stories

--- packages/ibm-products-web-components/src/utilities/overflow-handler/overflow-handler.stories.ts
+++ packages/ibm-products-web-components/src/utilities/overflow-handler/overflow-handler.stories.ts
@@ -8,17 +8,18 @@
  */
 
 import { html } from 'lit';
+import { render } from 'lit-html';
 import styles from './story-styles.scss?lit';
 import '@carbon/web-components/es/components/tag/index.js';
 import '@carbon/web-components/es/components/slider/index.js';
 
 const storyPrefix = 'overflow-handler-stories__';
 
-const getTags = (n: number = 5) => {
+const getItems = (n: number = 5) => {
   return Array(n)
     .fill(null)
     .map((_, idx) => ({
-      label: `tag-${idx + 1}`,
+      label: `item-${idx + 1}`,
     }));
 };
 
@@ -26,20 +27,47 @@ const tagTemplate = {
   args: {},
   argTypes: {},
   render: () => {
-    const width = 500;
-    const tags = 5;
-    const widthHandler = (evt) => {
-      // todo
+    let tags = 5;
+    let width = 500;
+    let items = getItems(tags);
+
+    const getTags = () => {
+      return items.map(
+        (tag) => html`<cds-tag title=${tag.label}>${tag.label}</cds-tag>`
+      );
     };
-    const tagsHandler = (evt) => {
-      // todo
+
+    const widthHandler = (evt: CustomEvent) => {
+      const { value } = evt.detail;
+      width = value;
+      const slider = document.querySelector('#width-slider');
+      slider?.setAttribute('value', value);
+      const container = document.querySelector('.annotation') as HTMLElement;
+      if (container) {
+        container.style.maxWidth = `${width}px`;
+      }
     };
+
+    const tagsHandler = (evt: CustomEvent) => {
+      const { value } = evt.detail;
+      tags = value;
+      items = getItems(value);
+      const slider = document.querySelector('#tag-slider');
+      slider?.setAttribute('value', value);
+      const container = document.querySelector('#tag-container') as HTMLElement;
+      const newTags = getTags();
+      if (container) {
+        render(newTags, container);
+      }
+    };
+
     return html`
       <style>
         ${styles}
       </style>
       <div class="example">
         <cds-slider
+          id="width-slider"
           label-text="Parent container width"
           class="slider"
           max="1000"
@@ -47,8 +75,11 @@ const tagTemplate = {
           step="1"
           @cds-slider-changed="${widthHandler}"
           value="${width}"
-        ></cds-slider>
+        >
+          <cds-slider-input aria-label="Width" type="number"></cds-slider-input>
+        </cds-slider>
         <cds-slider
+          id="tag-slider"
           label-text="Number of total tags"
           class="slider"
           max="50"
@@ -56,16 +87,15 @@ const tagTemplate = {
           step="1"
           @cds-slider-changed="${tagsHandler}"
           value="${tags}"
-        ></cds-slider>
+        >
+          <cds-slider-input aria-label="Tags" type="number"></cds-slider-input>
+        </cds-slider>
         <div class="parent">
           <div class="annotation">
             <div class="annotation__label">Parent container</div>
             <div class="annotation__content">
               <p>Visible items:</p>
-              ${getTags().map(
-                (tag) =>
-                  html`<cds-tag title=${tag.label}>${tag.label}</cds-tag>`
-              )}
+              <div id="tag-container">${getTags()}</div>
             </div>
           </div>
         </div>
@@ -75,6 +105,16 @@ const tagTemplate = {
 };
 
 export const WithTags = {
+  ...tagTemplate,
+  args: {},
+};
+
+export const WithUserAvatar = {
+  ...tagTemplate,
+  args: {},
+};
+
+export const WithButtons = {
   ...tagTemplate,
   args: {},
 };

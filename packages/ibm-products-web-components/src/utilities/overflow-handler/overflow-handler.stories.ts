923d72eb04ca0ba557ed7fbeb33743bbf1115b1f
David Menendez <damenend@gmail.com>
2025-05-28 13:58:45 -0500

    fix: implementation

--- packages/ibm-products-web-components/src/utilities/overflow-handler/overflow-handler.stories.ts
+++ packages/ibm-products-web-components/src/utilities/overflow-handler/overflow-handler.stories.ts
@@ -1,55 +1,72 @@
-/**
- * @license
- *
- * Copyright IBM Corp. 2025, 2025
- *
- * This source code is licensed under the Apache-2.0 license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import { html } from 'lit';
-import { render } from 'lit-html';
 import styles from './story-styles.scss?lit';
 import '@carbon/web-components/es/components/tag/index.js';
 import '@carbon/web-components/es/components/slider/index.js';
+import '@carbon/web-components/es/components/button/index.js';
+import '../../components/user-avatar/index.js';
 import { createOverflowHandler } from '@carbon/utilities';
 
 const storyPrefix = 'overflow-handler-stories__';
 
-const getItems = (n: number = 5) => {
+const makeItems = (n: number = 5) => {
   return Array(n)
     .fill(null)
     .map((_, idx) => ({
-      label: `item-${idx + 1}`,
+      label: `item ${idx + 1}`,
     }));
 };
 
-const tagTemplate = {
+const template = {
   args: {},
   argTypes: {},
-  render: () => {
-    let width = 750;
+  render: (args) => {
+    const { variant } = args;
+    let width = 1000;
     let handler;
 
-    document.addEventListener('DOMContentLoaded', () => {
+    const initializeHandler = () => {
+      if (handler) {
+        console.log('handler found. removing ...');
+        document.removeEventListener('DOMContentLoaded', initializeHandler);
+        handler.disconnect();
+        return;
+      }
       const visibleContainer = document.querySelector(
-        '#visible-tags'
+        '#visible-items'
       ) as HTMLElement;
-      const hiddenContainer = document.querySelector(
-        '#hidden-tags'
-      ) as HTMLElement;
-      handler = createOverflowHandler({
-        container: visibleContainer,
-        onChange: (visible, hidden) => {
-          render(visible, visibleContainer);
-          render(hidden, hiddenContainer);
-        },
+      // initiate the handler only when the dom settles and is stable, so that the items are at right dimensions before initialization.
+      requestAnimationFrame(() => {
+        handler = createOverflowHandler({
+          container: visibleContainer,
+          onChange: (visible, hidden) => {
+            console.log(visible, hidden);
+          },
+        });
       });
-    });
+    };
 
-    const getTags = () => {
-      return getItems(10).map(
-        (tag) => html`<cds-tag title=${tag.label}>${tag.label}</cds-tag>`
+    document.addEventListener('DOMContentLoaded', initializeHandler);
+
+    const makeTags = () => {
+      return makeItems(10).map(
+        (item) =>
+          html`<span
+            ><cds-tag type="blue" title=${item.label}
+              >${item.label}</cds-tag
+            ></span
+          >`
+      );
+    };
+
+    const makeButtons = () => {
+      return makeItems(10).map(
+        (item) => html`<span><cds-button>${item.label}</cds-button></span>`
+      );
+    };
+
+    const makeUserAvatars = () => {
+      return makeItems(10).map(
+        (item) => html`<span><c4p-user-avatar></c4p-user-avatar></span>`
       );
     };
 
@@ -58,10 +75,20 @@ const tagTemplate = {
       width = value;
       const slider = document.querySelector('#width-slider');
       slider?.setAttribute('value', value);
-      const container = document.querySelector('#visible-tags') as HTMLElement;
+      const container = document.querySelector('.parent') as HTMLElement;
       if (container) {
         container.style.width = `${width}px`;
       }
+    };
+
+    const getBody = () => {
+      if (variant === 'button') {
+        return makeButtons();
+      }
+      if (variant === 'user avatar') {
+        return makeUserAvatars();
+      }
+      return makeTags();
     };
 
     return html`
@@ -81,18 +108,13 @@ const tagTemplate = {
         >
           <cds-slider-input aria-label="Width" type="number"></cds-slider-input>
         </cds-slider>
-        <div class="parent" style="width:${width}px">
-          <div id="visible-tags">
-            <cds-tag title="1">1</cds-tag>
-            <cds-tag title="2">2</cds-tag>
-            <cds-tag title="3">3</cds-tag>
-            <cds-tag title="4">4</cds-tag>
-            <cds-tag title="5">5</cds-tag>
-            <cds-tag title="6">6</cds-tag>
-            <cds-tag title="7">7</cds-tag>
-            <cds-tag title="8">8</cds-tag>
-            <cds-tag title="9">9</cds-tag>
-            <cds-tag title="10000">10000</cds-tag>
+        <div class="parent">
+          <div class="annotation">
+            <div class="annotation__label">Parent container</div>
+            <div class="annotation__content">
+              <p>Visible items:</p>
+              <div id="visible-items">${getBody()}</div>
+            </div>
           </div>
         </div>
       </div>
@@ -101,18 +123,24 @@ const tagTemplate = {
 };
 
 export const WithTags = {
-  ...tagTemplate,
-  args: {},
+  ...template,
+  args: {
+    variant: 'tag',
+  },
 };
 
 export const WithUserAvatar = {
-  ...tagTemplate,
-  args: {},
+  ...template,
+  args: {
+    variant: 'user avatar',
+  },
 };
 
 export const WithButtons = {
-  ...tagTemplate,
-  args: {},
+  ...template,
+  args: {
+    variant: 'button',
+  },
 };
 
 const meta = {

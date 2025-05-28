20abdaa1ee1f1376256d649289543cf51c1d38df
David Menendez <damenend@gmail.com>
2025-05-28 14:38:33 -0500

    fix: documentation

--- packages/ibm-products-web-components/src/utilities/overflow-handler/overflow-handler.stories.ts
+++ packages/ibm-products-web-components/src/utilities/overflow-handler/overflow-handler.stories.ts
@@ -8,6 +8,18 @@ import { createOverflowHandler } from '@carbon/utilities';
 
 const storyPrefix = 'overflow-handler-stories__';
 
+const argTypes = {
+  dimension: {
+    control: 'select',
+    options: ['width', 'height'],
+  },
+  variant: {
+    table: {
+      disable: true,
+    },
+  },
+};
+
 const makeItems = (n: number = 5) => {
   return Array(n)
     .fill(null)
@@ -16,131 +28,140 @@ const makeItems = (n: number = 5) => {
     }));
 };
 
-const template = {
-  args: {},
-  argTypes: {},
-  render: (args) => {
-    const { variant } = args;
-    let width = 1000;
-    let handler;
+const renderTemplate = (args) => {
+  const { variant, dimension, maxVisibleItems } = args;
+  let width = 1000;
+  let handler;
 
-    const initializeHandler = () => {
-      if (handler) {
-        console.log('handler found. removing ...');
-        document.removeEventListener('DOMContentLoaded', initializeHandler);
-        handler.disconnect();
-        return;
-      }
-      const visibleContainer = document.querySelector(
-        '#visible-items'
-      ) as HTMLElement;
-      // initiate the handler only when the dom settles and is stable, so that the items are at right dimensions before initialization.
-      requestAnimationFrame(() => {
-        handler = createOverflowHandler({
-          container: visibleContainer,
-          onChange: (visible, hidden) => {
-            console.log(visible, hidden);
-          },
-        });
+  const initializeHandler = () => {
+    if (handler) {
+      console.log('handler found. removing ...');
+      document.removeEventListener('DOMContentLoaded', initializeHandler);
+      handler.disconnect();
+      return;
+    }
+    const visibleContainer = document.querySelector(
+      '#visible-items'
+    ) as HTMLElement;
+    // initiate the handler only when the dom settles and is stable, so that the items are at right dimensions before initialization.
+    requestAnimationFrame(() => {
+      handler = createOverflowHandler({
+        container: visibleContainer,
+        onChange: (visible, hidden) => {
+          console.log(visible, hidden);
+        },
+        dimension,
+        maxVisibleItems,
       });
-    };
+    });
+  };
 
-    document.addEventListener('DOMContentLoaded', initializeHandler);
+  document.addEventListener('DOMContentLoaded', initializeHandler);
 
-    const makeTags = () => {
-      return makeItems(10).map(
-        (item) =>
-          html`<span
-            ><cds-tag type="blue" title=${item.label}
-              >${item.label}</cds-tag
-            ></span
-          >`
-      );
-    };
+  const makeTags = () => {
+    return makeItems(10).map(
+      (item) =>
+        html`<span
+          ><cds-tag type="blue" title=${item.label}
+            >${item.label}</cds-tag
+          ></span
+        >`
+    );
+  };
 
-    const makeButtons = () => {
-      return makeItems(10).map(
-        (item) => html`<span><cds-button>${item.label}</cds-button></span>`
-      );
-    };
+  const makeButtons = () => {
+    return makeItems(10).map(
+      (item) => html`<span><cds-button>${item.label}</cds-button></span>`
+    );
+  };
 
-    const makeUserAvatars = () => {
-      return makeItems(10).map(
-        (item) => html`<span><c4p-user-avatar></c4p-user-avatar></span>`
-      );
-    };
+  const makeUserAvatars = () => {
+    return makeItems(10).map(
+      (item) => html`<span><c4p-user-avatar></c4p-user-avatar></span>`
+    );
+  };
 
-    const widthHandler = (evt: CustomEvent) => {
-      const { value } = evt.detail;
-      width = value;
-      const slider = document.querySelector('#width-slider');
-      slider?.setAttribute('value', value);
-      const container = document.querySelector('.parent') as HTMLElement;
-      if (container) {
-        container.style.width = `${width}px`;
-      }
-    };
+  const widthHandler = (evt: CustomEvent) => {
+    const { value } = evt.detail;
+    width = value;
+    const slider = document.querySelector('#width-slider');
+    slider?.setAttribute('value', value);
+    const container = document.querySelector('.parent') as HTMLElement;
+    if (container) {
+      container.style.width = `${width}px`;
+    }
+  };
 
-    const getBody = () => {
-      if (variant === 'button') {
-        return makeButtons();
-      }
-      if (variant === 'user avatar') {
-        return makeUserAvatars();
-      }
-      return makeTags();
-    };
+  const getBody = () => {
+    if (variant === 'button') {
+      return makeButtons();
+    }
+    if (variant === 'user avatar') {
+      return makeUserAvatars();
+    }
+    return makeTags();
+  };
 
-    return html`
-      <style>
-        ${styles}
-      </style>
-      <div class="example">
-        <cds-slider
-          id="width-slider"
-          label-text="Parent container width"
-          class="slider"
-          max="1000"
-          min="200"
-          step="1"
-          @cds-slider-changed="${widthHandler}"
-          value="${width}"
-        >
-          <cds-slider-input aria-label="Width" type="number"></cds-slider-input>
-        </cds-slider>
-        <div class="parent">
-          <div class="annotation">
-            <div class="annotation__label">Parent container</div>
-            <div class="annotation__content">
-              <p>Visible items:</p>
-              <div id="visible-items">${getBody()}</div>
-            </div>
+  return html`
+    <style>
+      ${styles}
+    </style>
+    <div class="example">
+      <cds-slider
+        id="width-slider"
+        label-text="Parent container width"
+        class="slider"
+        max="1000"
+        min="200"
+        step="1"
+        @cds-slider-changed="${widthHandler}"
+        value="${width}"
+      >
+        <cds-slider-input aria-label="Width" type="number"></cds-slider-input>
+      </cds-slider>
+      <div class="parent">
+        <div class="annotation">
+          <div class="annotation__label">Parent container</div>
+          <div class="annotation__content">
+            <p>Visible items:</p>
+            <div id="visible-items">${getBody()}</div>
           </div>
         </div>
       </div>
-    `;
-  },
+    </div>
+  `;
+};
+
+const defaultProps = {
+  maxVisibleItems: 10,
+  dimension: 'width',
 };
 
 export const WithTags = {
-  ...template,
   args: {
+    ...defaultProps,
     variant: 'tag',
   },
+  argTypes,
+  render: renderTemplate,
 };
 
 export const WithUserAvatar = {
-  ...template,
   args: {
+    ...defaultProps,
     variant: 'user avatar',
   },
+  argTypes,
+  render: renderTemplate,
 };
 
 export const WithButtons = {
-  ...template,
   args: {
+    ...defaultProps,
     variant: 'button',
   },
+  argTypes,
+  render: renderTemplate,
 };
 
 const meta = {

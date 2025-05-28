487e863b32373ffc497a1d7780f6b289f3dd117b
Anamika T S <anamika.t.s@ibm.com>
2025-05-28 14:25:04 +0530

    feat(ExportModal): added as WC Pattern

---
+++ packages/ibm-products-web-components/src/patterns/export-modal/export-modal.stories.ts
@@ -0,0 +1,39 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2025, 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { html } from 'lit';
+import '../../../examples/export-modal/src/export-modal';
+export default {
+  title: 'Patterns/ExportModal',
+};
+
+export const standard = {
+  render: () => {
+    return html` <standard-export-modal> </standard-export-modal> `;
+  },
+};
+
+export const WithExtension = {
+  render: () => {
+    const extension = ['pdf'];
+    return html`
+      <standard-export-modal .validExtensions=${extension}>
+      </standard-export-modal>
+    `;
+  },
+};
+
+export const WithPreformattedExtension = {
+  render: () => {
+    return html`
+      <export-modal-preformatted-extension>
+      </export-modal-preformatted-extension>
+    `;
+  },
+};

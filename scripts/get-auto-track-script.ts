ca36644272e7432548542655a94e76b0ea83c3f4
Matt Gallo <mdgallo@us.ibm.com>
2025-06-19 10:30:48 -0400

    refactor(instrumentation): replace amplitude with autotrack

---
+++ scripts/get-auto-track-script.ts
@@ -0,0 +1,56 @@
+/**
+ * Copyright IBM Corp. 2025, 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//cspell: disable
+export const getAutoTrack = (source: string) => `
+  <script>
+    window._ibmAnalytics = {
+      settings: {
+        name: '${source}',
+        isSpa: false,
+        tealiumProfileName: 'in-app-usage-growth',
+      },
+      onLoad: [['ibmStats.pageview', []]],
+    };
+
+    window.digitalData = {
+      page: {
+        pageInfo: {
+          ibm: {
+            siteId: ${`IBM_${(window as any)._ibmAnalytics.settings.name}`},
+          },
+          segment: {
+            enabled: true,
+            env: 'test',
+            key: 'aWg1ug3bDNcSTnde9iwvg1OcvVRzf0JC',
+            coremetrics: false,
+            commonProperties: {
+              UT30: 'C4IP',
+              productCode: 'C4IP',
+              productCodeType: 'C4IP',
+              productTitle: 'carbon-for-ibm-products',
+              instanceId: 'carbon-for-ibm-products',
+            },
+            pageProperties: {
+              // NO REQUIRED PROPERTIES
+            },
+          },
+        },
+        category: {
+          primaryCategory: 'PC110',
+        },
+      },
+    };
+  </script>
+  <script
+    src="//1.www.s81c.com/common/stats/ibm-common.js"
+    type="text/javascript"
+    async="async"></script>
+  <script
+    type="text/javascript"
+    async="async"
+    src="//1.www.s81c.com/common/carbon/autotrack.min.js"></script>`;

273ca03d5c784fa51bfa65993525d55e58184b34
Matt Gallo <mdgallo@us.ibm.com>
2025-06-20 13:18:10 -0400

    refactor(instrumentation): replace amplitude with autotrack (#7710)
    * refactor(instrumentation): replace amplitude with autotrack
    
    * chore: remove typings directory
    
    * refactor(instrumentation): update how siteId is set
    
    * refactor(Instrumentation): remove extaneous fields
    
    * refactor(Instrumentation): add page properties

--- packages/ibm-products/.storybook/main.js
+++ packages/ibm-products/.storybook/main.js
@@ -6,6 +6,7 @@
  */
 import { dirname, join, resolve } from 'path';
 import remarkGfm from 'remark-gfm';
+import { getAutoTrack } from '../../../scripts/get-auto-track-script';
 
 const stories = [
   '../src/**/!(*.internal).stories.*',
@@ -62,15 +63,7 @@ export default {
       ${head}
       ${
         process.env.NODE_ENV !== 'development'
-          ? `
-          <script src="https://cdn.amplitude.com/script/f6f1d9025934f04f5a2a8bebb74abf2f.js"></script>
-          <script>
-            window.amplitude.add(window.sessionReplay.plugin({sampleRate: 1}));
-            window.amplitude.init('f6f1d9025934f04f5a2a8bebb74abf2f', {
-              "fetchRemoteConfig":true,
-              "autocapture":true
-            });
-          </script>`
+          ? getAutoTrack('ibm-products-react-storybook')
           : ''
       }
     `;

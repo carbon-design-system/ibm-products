b19d9632d9ff1b0b012a951503a506caec0037cf
David Menendez <damenend@gmail.com>
2025-06-25 09:22:07 -0500

    fix: additional v19 changes

--- packages/ibm-products/src/components/Tearsheet/Tearsheet.stories.jsx
+++ packages/ibm-products/src/components/Tearsheet/Tearsheet.stories.jsx
@@ -582,9 +582,9 @@ const StackedTemplate = (
   const [open2, setOpen2] = useState(false);
   const [open3, setOpen3] = useState(false);
   const ref = useRef(undefined);
-  const openButton1 = useRef();
-  const openButton2 = useRef();
-  const openButton3 = useRef();
+  const openButton1 = useRef(undefined);
+  const openButton2 = useRef(undefined);
+  const openButton3 = useRef(undefined);
 
   const wiredActions1 = Array.prototype.map.call(actions, (action) => {
     if (action.label === 'Cancel') {

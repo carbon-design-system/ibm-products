9a8956c104dab44b68c36f7f50e5bb318bb77fd2
devadula-nandan <devadula.nandan@gmail.com>
2025-06-30 18:16:01 +0530

    fix: ci checks

--- packages/ibm-products-web-components/examples/set-of-users/src/example-data.ts
+++ packages/ibm-products-web-components/examples/set-of-users/src/example-data.ts
@@ -1,3 +1,12 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2025, 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const namesString =
   'Alice Bob Charlie Diana Ethan Fiona George Hannah Ian Julia Kevin Laura Mike Nina Oscar Paula Quentin Rachel Steve Tina Uma Victor Wendy Xavier Yvonne Zach';
 
--- packages/ibm-products-web-components/examples/set-of-users/src/set-of-users.ts
+++ packages/ibm-products-web-components/examples/set-of-users/src/set-of-users.ts
@@ -1,3 +1,12 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2025, 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { LitElement, html, nothing } from 'lit';
 import { customElement, property, query, state } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';

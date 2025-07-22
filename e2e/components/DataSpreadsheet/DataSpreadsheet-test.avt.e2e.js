5f81975e32c07e235fc69e0119745d07a4852d1d
Matt Gallo <mdgallo@us.ibm.com>
2025-07-22 11:11:23 -0400

    chore: update e2e story urls

--- e2e/components/DataSpreadsheet/DataSpreadsheet-test.avt.e2e.js
+++ e2e/components/DataSpreadsheet/DataSpreadsheet-test.avt.e2e.js
@@ -14,7 +14,7 @@ test.describe('DataSpreadsheet @avt', () => {
   test('@avt-default-state', async ({ page }) => {
     await visitStory(page, {
       component: 'DataSpreadsheet',
-      id: 'experimental-dataspreadsheet--data-spreadsheet',
+      id: 'preview-dataspreadsheet--data-spreadsheet',
       globals: {
         carbonTheme: 'white',
       },

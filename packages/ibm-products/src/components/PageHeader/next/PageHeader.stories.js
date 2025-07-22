7d6ad5bf57adc095ecaccbeb90f0ae9fad97de76
Matt Gallo <mdgallo@us.ibm.com>
2025-07-22 16:00:28 -0400

    chore: remove useEffect import

--- packages/ibm-products/src/components/PageHeader/next/PageHeader.stories.js
+++ packages/ibm-products/src/components/PageHeader/next/PageHeader.stories.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Add } from '@carbon/icons-react';
 import { preview__PageHeader as PageHeader } from '../../';
 import {

a2a4f2699eb3a004a075f13f7478ed54e030169e
Sinta Augustine <sintaaugustine@gmail.com>
2025-07-23 17:11:36 +0530

    chore: rename bignumbers to bignumber

--- packages/ibm-products/src/components/index.js
+++ packages/ibm-products/src/components/index.js
@@ -1,5 +1,5 @@
 //
-// Copyright IBM Corp. 2020, 2024
+// Copyright IBM Corp. 2020, 2025
 //
 // This source code is licensed under the Apache-2.0 license found in the
 // LICENSE file in the root directory of this source tree.
@@ -101,7 +101,8 @@ export {
 } from './Guidebanner';
 export { InlineTip, InlineTipButton, InlineTipLink } from './InlineTip';
 export { NonLinearReading } from './NonLinearReading';
-export { BigNumbers } from './BigNumbers';
+export { BigNumber } from './BigNumber';
+export { BigNumber as BigNumbers } from './BigNumber';
 export { TruncatedList } from './TruncatedList';
 export { TruncatedText } from './TruncatedText';
 export { InterstitialScreen } from './InterstitialScreen';

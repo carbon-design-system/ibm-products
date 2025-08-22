9323ec610d74c0608cdaba8b10fd8228b6f5266b
David Menendez <damenend@gmail.com>
2025-08-22 08:41:08 -0500

    fix: update format script

--- scripts/example-gallery-builder/update-example/templates/src/App.jsx
+++ scripts/example-gallery-builder/update-example/templates/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from './ThemeSelector/ThemeContext';
 import { ThemeDropdown } from './ThemeSelector/ThemeDropdown';
-IMPORT_IBM_PRODUCTS_CONFIGimport Example from './Example/Example';
+import Example from './Example/Example';
 
 function App() {
   return (

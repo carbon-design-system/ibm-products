afe482cfcc78156c818f42a39956d23a09e1dac4
Matt Gallo <mdgallo@us.ibm.com>
2025-09-23 14:31:31 -0400

    chore(step-flow): cleanup wrapper step group component

--- packages/ibm-products-web-components/src/components/step-flow/step-group.ts
+++ packages/ibm-products-web-components/src/components/step-flow/step-group.ts
@@ -1,12 +1,8 @@
 import { LitElement, html } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
-import { StepInstance } from './step-flow-signal';
+import { customElement } from 'lit/decorators.js';
 
 @customElement('step-group')
 export class StepGroup extends LitElement {
-  @property({ type: Object })
-  stepInstance = {} as StepInstance; // Default value
-
   render() {
     return html`<slot></slot>`;
   }

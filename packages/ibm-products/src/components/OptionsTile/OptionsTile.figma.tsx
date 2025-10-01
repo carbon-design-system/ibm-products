0519d2db5b730077df1c45f489256a387db5ca0f
Jeff Longshore <Jeff.Longshore@ibm.com>
2025-10-01 08:49:59 -0500

    refactor: continued refinement of code connect file

--- packages/ibm-products/src/components/OptionsTile/OptionsTile.figma.tsx
+++ packages/ibm-products/src/components/OptionsTile/OptionsTile.figma.tsx
@@ -11,20 +11,77 @@ import figma from '@figma/code-connect';
 
 figma.connect(
   OptionsTile,
-  'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=36228-14503&t=AL5QssM8KsTELzh9-4',
+  'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=36228%3A14503',
   {
     props: {
-	    title: figma.string('Heading text'),
-	    summary: figma.string('Summary text'),
+      title: figma.string('Heading text'),
       open: figma.boolean('Expanded'),
-      OptionsTile: figma.nestedProps('Summary + Validation message', {
-        locked: figma.boolean('Locked'),
-      }),
       size: figma.enum('Size', {
         Large: 'lg',
-        XL: 'xl',
+        'Extra large': 'xl',
+      }),
+
+      children: figma.enum('Type', {
+        Static: undefined,
+        Expandable: figma.instance('Swap slot'),
+      }),
+      summaryProps: figma.boolean('Summary + Validation', {
+        true: figma.nestedProps('Summary + Validation message', {
+          locked: figma.enum('Type', {
+            Summary: figma.boolean('Locked'),
+            Warning: false,
+            Error: false,
+          }),
+          summary: figma.enum('Type', {
+            Summary: figma.string('Summary text'),
+            Warning: undefined,
+            Error: undefined,
+          }),
+          invalid: figma.enum('Type', {
+            Summary: undefined,
+            Warning: undefined,
+            Error: true,
+          }),
+          invalidText: figma.enum('Type', {
+            Summary: undefined,
+            Warning: undefined,
+            Error: figma.string('Error text'),
+          }),
+          warn: figma.enum('Type', {
+            Summary: undefined,
+            Warning: true,
+            Error: undefined,
+          }),
+          warnText: figma.enum('Type', {
+            Summary: undefined,
+            Warning: figma.string('Warning text'),
+            Error: undefined,
+          }),
+        }),
+        false: {
+          locked: undefined,
+          summary: undefined,
+          invalid: undefined,
+          invalidText: undefined,
+          warn: undefined,
+          warnText: undefined,
+        },
       }),
     },
-    example: (props) => <OptionsTile {...props} />,
+    example: (props) => (
+      <OptionsTile
+        title={props.title}
+        open={props.open}
+        size={props.size}
+        locked={props.summaryProps.locked}
+        summary={props.summaryProps.summary}
+        invalid={props.summaryProps.invalid}
+        invalidText={props.summaryProps.invalidText}
+        warn={props.summaryProps.warn}
+        warnText={props.summaryProps.warnText}
+      >
+        {props.children}
+      </OptionsTile>
+    ),
   }
 );

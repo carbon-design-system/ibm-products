890f2ea2151a182d9d873a69efff04a1d54b9bdb
Jeff Longshore <Jeff.Longshore@ibm.com>
2025-10-06 14:10:50 -0500

    refactor: added support for tooltip data and background color

--- packages/ibm-products/src/components/UserAvatar/UserAvatar.figma.tsx
+++ packages/ibm-products/src/components/UserAvatar/UserAvatar.figma.tsx
@@ -18,72 +18,105 @@ const sharedProps = {
     Medium: 'md',
     Small: 'sm',
   }),
+
+  tooltipProps: figma.nestedProps('Tooltip', {
+    text: figma.string('Tooltip text'),
+    tooltipAlignment: figma.enum('🚫 Position', {
+      Top: 'top',
+      Right: 'right',
+      Bottom: 'bottom',
+      Left: 'left',
+    }),
+  }),
+
+  bgProps: figma.nestedProps('Background', {
+    color: figma.enum('Color', {
+      Cyan: figma.enum('Sequence', {
+        Primary: 'order-1-cyan',
+        Secondary: 'order-7-cyan',
+      }),
+      Gray: figma.enum('Sequence', {
+        Primary: 'order-2-gray',
+        Secondary: 'order-8-gray',
+      }),
+      Green: figma.enum('Sequence', {
+        Primary: 'order-3-green',
+        Secondary: 'order-9-green',
+      }),
+      Magenta: figma.enum('Sequence', {
+        Primary: 'order-4-magenta',
+        Secondary: 'order-10-magenta',
+      }),
+      Purple: figma.enum('Sequence', {
+        Primary: 'order-5-purple',
+        Secondary: 'order-11-purple',
+      }),
+      Teal: figma.enum('Sequence', {
+        Primary: 'order-6-teal',
+        Secondary: 'order-12-teal',
+      }),
+    }),
+  }),
 };
 
-figma.connect(
-  UserAvatar,
-  'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=15368-59379&t=lbewdWdJ4JB5izcw-4',
-  {
-    variant: { Type: 'Image' },
-    props: sharedProps,
-    example: (props) => (
-      <UserAvatar
-        image="/path/to/image"
-        imageDescription="Alt text for image"
-        tooltipAlignment="bottom"
-        tooltipText={`${props.name}, more info here`}
-        {...props}
-      />
-    ),
-  }
-);
+const connectionURL =
+  'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=15368-59379&t=lbewdWdJ4JB5izcw-4';
 
-figma.connect(
-  UserAvatar,
-  'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=15368-59379&t=lbewdWdJ4JB5izcw-4',
-  {
-    variant: { Type: 'Initials' },
-    props: sharedProps,
-    example: (props) => (
-      <UserAvatar
-        tooltipAlignment="bottom"
-        tooltipText={`${props.name}, more info here`}
-        {...props}
-      />
-    ),
-  }
-);
+figma.connect(UserAvatar, connectionURL, {
+  variant: { Type: 'Image' },
+  props: sharedProps,
 
-figma.connect(
-  UserAvatar,
-  'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=15368-59379&t=lbewdWdJ4JB5izcw-4',
-  {
-    variant: { Type: 'Single user' },
-    props: sharedProps,
-    example: (props) => (
-      <UserAvatar
-        renderIcon={User}
-        tooltipAlignment="bottom"
-        tooltipText={`${props.name}, more info here`}
-        {...props}
-      />
-    ),
-  }
-);
+  example: (props) => (
+    <UserAvatar
+      image="/path/to/image"
+      imageDescription="Alt text for image"
+      tooltipAlignment={props.tooltipProps.tooltipAlignment}
+      tooltipText={props.tooltipProps.text}
+      size={props.size}
+    />
+  ),
+});
 
-figma.connect(
-  UserAvatar,
-  'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=15368-59379&t=lbewdWdJ4JB5izcw-4',
-  {
-    variant: { Type: 'User group' },
-    props: sharedProps,
-    example: (props) => (
-      <UserAvatar
-        renderIcon={Group}
-        tooltipAlignment="bottom"
-        tooltipText={`${props.name}, more info here`}
-        {...props}
-      />
-    ),
-  }
-);
+figma.connect(UserAvatar, connectionURL, {
+  variant: { Type: 'Initials' },
+  props: sharedProps,
+  example: (props) => (
+    <UserAvatar
+      tooltipAlignment={props.tooltipProps.tooltipAlignment}
+      tooltipText={props.tooltipProps.text}
+      name={props.name}
+      size={props.size}
+      backgroundColor={props.bgProps.color}
+    />
+  ),
+});
+
+figma.connect(UserAvatar, connectionURL, {
+  variant: { Type: 'Single user' },
+  props: sharedProps,
+  example: (props) => (
+    <UserAvatar
+      renderIcon={User}
+      tooltipAlignment={props.tooltipProps.tooltipAlignment}
+      tooltipText={props.tooltipProps.text}
+      name={props.name}
+      size={props.size}
+      backgroundColor={props.bgProps.color}
+    />
+  ),
+});
+
+figma.connect(UserAvatar, connectionURL, {
+  variant: { Type: 'User group' },
+  props: sharedProps,
+  example: (props) => (
+    <UserAvatar
+      renderIcon={Group}
+      tooltipAlignment={props.tooltipProps.tooltipAlignment}
+      tooltipText={props.tooltipProps.text}
+      name={props.name}
+      size={props.size}
+      backgroundColor={props.bgProps.color}
+    />
+  ),
+});

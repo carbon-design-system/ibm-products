dcc77bb535680dfdb6b30b3f5b710330d5d576d8
Jeff Longshore <Jeff.Longshore@ibm.com>
2025-10-10 16:19:48 -0500

    fix: bug with cards code connect files

--- packages/ibm-products/src/components/ExpressiveCard/ExpressiveCard.figma.tsx
+++ packages/ibm-products/src/components/ExpressiveCard/ExpressiveCard.figma.tsx
@@ -6,7 +6,10 @@
  */
 
 import React from 'react';
+import { CarbonIconType } from '@carbon/icons-react/lib/CarbonIcon';
 import { ExpressiveCard } from './ExpressiveCard';
+import { AspectRatio } from '@carbon/react';
+import { ArrowRight, Cloud } from '@carbon/react/icons';
 import figma from '@figma/code-connect';
 
 figma.connect(
@@ -14,31 +17,166 @@ figma.connect(
   'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=9053-414333',
   {
     props: {
-      actionIcons: figma.instance('Action icons'),
-      decorator: figma.instance('Decorator'),
-      description: figma.string('Description'),
-      label: figma.string('Label'),
-      media: figma.instance('Media'),
-      mediaPosition: figma.enum('Options', {
-        top: 'Top',
-        left: 'Left',
+      pictogram: figma.enum('Media', {
+        None: undefined,
+        Top: undefined,
+        Left: undefined,
+        Pictogram: Cloud,
       }),
-      pictogram: figma.instance('Pictogram'),
-      primaryButtonIcon: figma.instance('Primary button icon'),
-      primaryButtonKind: figma.enum('Primary button kind', {
-        primary: 'Primary',
-        ghost: 'Ghost',
+      r2Props: figma.nestedProps('R:2 | Title bar', {
+        title: figma.boolean('Title', {
+          true: figma.string('Title text'),
+          false: undefined,
+        }),
+        description: figma.boolean('Caption', {
+          true: figma.string('Caption text'),
+          false: undefined,
+        }),
+        label: figma.boolean('Label', {
+          true: figma.string('Label text'),
+          false: undefined,
+        }),
       }),
-      primaryButtonText: figma.string('Primary button text'),
-      secondaryButtonIcon: figma.instance('Secondary button icon'),
-      secondaryButtonKind: figma.enum('Secondary button kind', {
-        secondary: 'Secondary',
-        ghost: 'Ghost',
+      descProps: figma.boolean('Description', {
+        true: figma.nestedProps('_Card description base', {
+          description: figma.string('Description text'),
+        }),
+        false: { description: undefined },
       }),
-      secondaryButtonText: figma.string('Secondary button text'),
-      slug: figma.instance('Slug'),
-      title: figma.string('Title'),
+
+      slotProps: figma.boolean('Slot', {
+        true: figma.nestedProps('_Card slot base', {
+          slotChildren: figma.children('Slot'),
+        }),
+        false: {
+          slotChildren: undefined,
+        },
+      }),
+      mediaPosition: figma.enum('Media', {
+        None: undefined,
+        Top: 'top',
+        Left: 'left',
+        Pictogram: undefined,
+      }),
+      mediaProps: figma.enum('Media', {
+        None: { media: undefined },
+        Top: figma.nestedProps('Aspect ratio', {
+          media: figma.enum('Aspect ratio', {
+            '1:1': <AspectRatio ratio="1x1">1:1</AspectRatio>,
+            '4:3': <AspectRatio ratio="4x3">4:3</AspectRatio>,
+            '3:2': <AspectRatio ratio="3x2">3:2</AspectRatio>,
+            '16:9': <AspectRatio ratio="16x9">16:9</AspectRatio>,
+            '2:1': <AspectRatio ratio="2x1">2:1</AspectRatio>,
+          }),
+        }),
+        Left: figma.nestedProps('Aspect ratio', {
+          media: figma.enum('Aspect ratio', {
+            '1:1': <AspectRatio ratio="1x1">1:1</AspectRatio>,
+            '4:3': <AspectRatio ratio="4x3">4:3</AspectRatio>,
+            '3:2': <AspectRatio ratio="3x2">3:2</AspectRatio>,
+            '16:9': <AspectRatio ratio="16x9">16:9</AspectRatio>,
+            '2:1': <AspectRatio ratio="2x1">2:1</AspectRatio>,
+          }),
+        }),
+        Pictogram: { media: undefined },
+      }),
+
+      aspectRatioProps: figma.enum('Media', {
+        None: {
+          media: undefined,
+        },
+        Top: {
+          media: <AspectRatio ratio="1x1">1:1</AspectRatio>,
+        },
+        Left: {
+          media: <AspectRatio ratio="1x1">1:1</AspectRatio>,
+        },
+        Pictogram: {
+          mediaProps: { media: undefined },
+        },
+      }),
+
+      actionBarProps: figma.nestedProps('_Expressive card action bar base', {
+        actionIcons: figma.enum('Type', {
+          'Primary + ghost': undefined,
+          'Primary only': undefined,
+          'Primary full bleed': undefined,
+          'Icon only': [
+            {
+              id: '1',
+              icon: () => <ArrowRight size={18} />,
+              iconDescription: 'Visit carbon official site',
+            },
+          ],
+        }),
+        primaryButtonKind: figma.enum('Type', {
+          'Primary + ghost': 'primary',
+          'Primary only': 'primary',
+          'Primary full bleed': 'primary',
+          'Icon only': undefined,
+        }),
+        primaryButtonText: figma.enum('Type', {
+          'Primary + ghost': 'Button',
+          'Primary only': 'Button',
+          'Primary full bleed': 'Button',
+          'Icon only': undefined,
+        }),
+        secondaryButtonKind: figma.enum('Type', {
+          'Primary + ghost': 'ghost',
+          'Primary only': undefined,
+          'Primary full bleed': undefined,
+          'Icon only': undefined,
+        }),
+        secondaryButtonText: figma.enum('Type', {
+          'Primary + ghost': 'Button',
+          'Primary only': undefined,
+          'Primary full bleed': undefined,
+          'Icon only': undefined,
+        }),
+      }),
+
+      // actionIcons: figma.instance('Action icons'),
+      // decorator: figma.instance('Decorator'),
+      // description: figma.string('Description'),
+      // label: figma.string('Label'),
+      // media: figma.instance('Media'),
+      // mediaPosition: figma.enum('Options', {
+      //   top: 'Top',
+      //   left: 'Left',
+      // }),
+      // pictogram: figma.instance('Pictogram'),
+      // primaryButtonIcon: figma.instance('Primary button icon'),
+      // primaryButtonKind: figma.enum('Primary button kind', {
+      //   primary: 'Primary',
+      //   ghost: 'Ghost',
+      // }),
+      // primaryButtonText: figma.string('Primary button text'),
+      // secondaryButtonIcon: figma.instance('Secondary button icon'),
+      // secondaryButtonKind: figma.enum('Secondary button kind', {
+      //   secondary: 'Secondary',
+      //   ghost: 'Ghost',
+      // }),
+      // secondaryButtonText: figma.string('Secondary button text'),
+      // slug: figma.instance('Slug'),
+      // title: figma.string('Title'),
     },
-    example: (props) => <ExpressiveCard {...props} />,
+    example: (props) => (
+      <ExpressiveCard
+        pictogram={props.pictogram}
+        mediaPosition={props.mediaPosition}
+        media={props.mediaProps.media}
+        title={props.r2Props.title}
+        description={props.r2Props.description}
+        label={props.r2Props.label}
+        primaryButtonKind={props.actionBarProps.primaryButtonKind}
+        primaryButtonText={props.actionBarProps.primaryButtonText}
+        secondaryButtonKind={props.actionBarProps.secondaryButtonKind}
+        secondaryButtonText={props.actionBarProps.secondaryButtonText}
+        actionIcons={props.actionBarProps.actionIcons}
+      >
+        {props.descProps.description}
+        {props.slotProps.slotChildren}
+      </ExpressiveCard>
+    ),
   }
 );

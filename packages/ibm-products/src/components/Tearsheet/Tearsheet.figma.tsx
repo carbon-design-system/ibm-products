0fe374e0cec1252720b007d0d432e9c32813ad0d
Jeff Longshore <Jeff.Longshore@ibm.com>
2025-10-14 17:14:15 -0500

    feat: added tearsheet and narrowtearsheet code connect files

--- packages/ibm-products/src/components/Tearsheet/Tearsheet.figma.tsx
+++ packages/ibm-products/src/components/Tearsheet/Tearsheet.figma.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import { Tearsheet } from './Tearsheet';
+import { Tearsheet, TearsheetProps } from './Tearsheet';
 import figma from '@figma/code-connect';
 
 figma.connect(
@@ -14,17 +14,156 @@ figma.connect(
   'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=9837%3A338113',
   {
     props: {
-      // No matching props could be found for these Figma properties:
-      // "buttons": figma.boolean('Buttons'),
-      // "rightPanel": figma.boolean('Right panel'),
-      // "leftPanel": figma.boolean('Left panel'),
-      // "layers": figma.boolean('Layers'),
-      // "size": figma.enum('Size', {
-      //   "Max (1584px)": "max--1584px-",
-      //   "X-Large (1312px)": "x-large--1312px-",
-      //   "Large (1056px)": "large--1056px-"
-      // })
+      title: figma.nestedProps('_Tearsheet header', {
+        value: figma.textContent('Title'),
+      }),
+      description: figma.nestedProps('_Tearsheet header', {
+        value: figma.textContent('Description'),
+      }),
+      children: figma.children('_Tearsheet content - Wide'),
+      actions: figma.boolean('Buttons', {
+        true: figma.nestedProps('R:2 | Tearsheet footer item', {
+          value: figma.enum('Buttons', {
+            '1': [
+              {
+                key: 1,
+                kind: 'primary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked primary'),
+              },
+            ],
+            '2': [
+              {
+                key: 1,
+                kind: 'primary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked primary'),
+              },
+              {
+                key: 2,
+                kind: 'secondary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked secondary'),
+              },
+            ],
+            '3': [
+              {
+                key: 1,
+                kind: 'primary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked primary'),
+              },
+              {
+                key: 2,
+                kind: 'secondary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked secondary'),
+              },
+              {
+                key: 3,
+                kind: 'ghost',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked ghost'),
+              },
+            ],
+            '4': [
+              {
+                key: 1,
+                kind: 'primary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked primary'),
+              },
+              {
+                key: 2,
+                kind: 'secondary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked secondary'),
+              },
+              {
+                key: 3,
+                kind: 'tertiary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked tertiary'),
+              },
+              {
+                key: 4,
+                kind: 'ghost',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked ghost'),
+              },
+            ],
+          }),
+        }),
+        false: { value: undefined },
+      }),
+      influencer: figma.boolean('Left panel', {
+        true: figma.boolean('Right panel', {
+          true: figma.children('_Tearsheet right panel'),
+          false: figma.children('_Tearsheet left panel'),
+        }),
+        false: figma.boolean('Right panel', {
+          true: figma.children('_Tearsheet right panel'),
+          false: undefined,
+        }),
+      }),
+
+      influencerPosition: figma.boolean('Left panel', {
+        true: figma.boolean('Right panel', {
+          true: 'right',
+          false: 'left',
+        }),
+        false: figma.boolean('Right panel', {
+          true: 'right',
+          false: undefined,
+        }),
+      }),
+      influencerWidth: figma.boolean('Left panel', {
+        true: figma.boolean('Right panel', {
+          true: figma.nestedProps('_Tearsheet right panel', {
+            width: figma.enum('Width', {
+              '256px': 'narrow',
+              '320px': 'wide',
+            }),
+          }),
+          false: figma.nestedProps('_Tearsheet left panel', {
+            width: figma.enum('Width', {
+              '256px': 'narrow',
+              '320px': 'wide',
+            }),
+          }),
+        }),
+        false: figma.boolean('Right panel', {
+          true: figma.nestedProps('_Tearsheet right panel', {
+            width: figma.enum('Width', {
+              '256px': 'narrow',
+              '320px': 'wide',
+            }),
+          }),
+          false: { width: undefined },
+        }),
+      }),
     },
-    example: (props) => <Tearsheet />,
+    example: (props) => (
+      <Tearsheet
+        title={props.title.value}
+        description={props.description.value}
+        actions={props.actions.value}
+        influencer={props.influencer}
+        influencerPosition={props.influencerPosition}
+        influencerWidth={props.influencerWidth.width}
+      >
+        {props.children}
+      </Tearsheet>
+    ),
   }
 );
--- packages/ibm-products/src/components/Tearsheet/TearsheetNarrow.figma.tsx
+++ packages/ibm-products/src/components/Tearsheet/TearsheetNarrow.figma.tsx
@@ -14,13 +14,107 @@ figma.connect(
   'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=9837%3A338231',
   {
     props: {
-      // No matching props could be found for these Figma properties:
-      // "buttons": figma.boolean('Buttons'),
-      // "layers": figma.boolean('Layers'),
-      // "type": figma.enum('Type', {
-      //   "Default": "default"
-      // })
+      title: figma.nestedProps('R:2 | Tearsheet header item', {
+        value: figma.textContent('Title'),
+      }),
+      description: figma.nestedProps('R:2 | Tearsheet header item', {
+        value: figma.textContent('Description'),
+      }),
+      children: figma.children('_Tearsheet content - Narrow'),
+      actions: figma.boolean('Buttons', {
+        true: figma.nestedProps('R:2 | Tearsheet footer item', {
+          value: figma.enum('Buttons', {
+            '1': [
+              {
+                key: 1,
+                kind: 'primary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked primary'),
+              },
+            ],
+            '2': [
+              {
+                key: 1,
+                kind: 'primary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked primary'),
+              },
+              {
+                key: 2,
+                kind: 'secondary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked secondary'),
+              },
+            ],
+            '3': [
+              {
+                key: 1,
+                kind: 'primary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked primary'),
+              },
+              {
+                key: 2,
+                kind: 'secondary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked secondary'),
+              },
+              {
+                key: 3,
+                kind: 'ghost',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked ghost'),
+              },
+            ],
+            '4': [
+              {
+                key: 1,
+                kind: 'primary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked primary'),
+              },
+              {
+                key: 2,
+                kind: 'secondary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked secondary'),
+              },
+              {
+                key: 3,
+                kind: 'tertiary',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked tertiary'),
+              },
+              {
+                key: 4,
+                kind: 'ghost',
+                label: 'Button',
+                loading: false,
+                onClick: () => console.log('clicked ghost'),
+              },
+            ],
+          }),
+        }),
+        false: { value: undefined },
+      }),
     },
-    example: (props) => <TearsheetNarrow />,
+    example: (props) => (
+      <TearsheetNarrow
+        title={props.title.value}
+        description={props.description.value}
+        actions={props.actions.value}
+      >
+        {props.children}
+      </TearsheetNarrow>
+    ),
   }
 );

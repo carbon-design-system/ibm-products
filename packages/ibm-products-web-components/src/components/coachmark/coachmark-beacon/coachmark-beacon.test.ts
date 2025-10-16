aee7cd0b5d2a36b9fa680fe4e620cabddbb25ca9
Anamika T S <anamika.t.s@ibm.com>
2025-10-16 17:54:08 +0530

    test(coachmarkbeacon): add test

--- packages/ibm-products-web-components/src/components/coachmark/coachmark-beacon/coachmark-beacon.test.ts
+++ packages/ibm-products-web-components/src/components/coachmark/coachmark-beacon/coachmark-beacon.test.ts
@@ -6,3 +6,73 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+import { expect, describe, it } from 'vitest';
+import { fixture, html, oneEvent } from '@open-wc/testing';
+import './index';
+import { prefix } from '../../../globals/settings';
+
+const blockClass = `${prefix}--coachmark-beacon`;
+
+const defaultProps = {
+  label: 'show information',
+  id: 'coachmarkBeacon',
+} as any;
+
+const template = (props = defaultProps) => html`
+  <c4p-coachmark-beacon
+    label=${props.label}
+    @c4p-coachmark-beacon-clicked=${(e: CustomEvent) => {
+      console.log('Beacon clicked!', e.detail.expanded);
+    }}
+  >
+  </c4p-coachmark-beacon>
+`;
+
+describe('c4p-coachmark-beacon', () => {
+  it('should render a beacon', async () => {
+    const beacon = await fixture(template({ ...defaultProps }));
+    expect(beacon).toBeDefined();
+  });
+
+  it('has correct host classes', async () => {
+    const el = await fixture(template({ ...defaultProps }));
+
+    expect(el.classList.contains(blockClass)).to.be.true;
+    expect(el.classList.contains(`${blockClass}-default`)).to.be.true;
+  });
+
+  it('applies className to the containing node', async () => {
+    const customClass = 'test';
+    const beacon = await fixture(template({ ...defaultProps }));
+    beacon.classList.add(customClass);
+    expect(beacon.getAttribute('class')).to.include(customClass);
+  });
+
+  it('should render icon for the beacon', async () => {
+    const beacon = await fixture(template({ ...defaultProps }));
+    const renderedSVG = beacon?.shadowRoot?.querySelector(
+      'slot[name="icon"]'
+    ) as HTMLSlotElement;
+    expect(renderedSVG).to.exist;
+    const assignedNodes = renderedSVG.assignedNodes({ flatten: true });
+    const svg = assignedNodes.find(
+      (node) => node.nodeName.toLowerCase() === 'svg'
+    ) as SVGElement;
+    expect(svg).to.exist;
+  });
+
+  it('toggles expanded state and dispatches event on click', async () => {
+    const el = await fixture(template({ ...defaultProps }));
+
+    const button = el.shadowRoot!.querySelector<HTMLElement>('cds-button')!;
+
+    const eventPromise = oneEvent(el, 'c4p-coachmark-beacon-clicked');
+
+    button.click();
+
+    const event = await eventPromise;
+    expect(event).to.exist;
+    expect(event.detail.expanded).to.be.true;
+  });
+});

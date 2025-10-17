5abd7e616a61bf439450b32fa39a265dd2a02f19
Rich Kummer <rckummer@gmail.com>
2025-10-17 14:23:50 -0400

    feat(empty-state): sharedProps

--- packages/ibm-products/src/components/EmptyStates/EmptyState.figma.tsx
+++ packages/ibm-products/src/components/EmptyStates/EmptyState.figma.tsx
@@ -10,54 +10,65 @@ import { EmptyState } from './EmptyState';
 import figma from '@figma/code-connect';
 import { Add } from '@carbon/icons-react';
 
+const sharedProps = {
+  action: figma.boolean('Button', {
+    true: {
+      text: 'Button',
+      onClick: () => console.log("clicked empty state action button'),"),
+      renderIcon: (props) => <Add size={20} {...props} />,
+      iconDescription: 'Add icon',
+    },
+    false: undefined,
+  }),
+  link: figma.boolean('Link', {
+    true: {
+      text: 'Link',
+      href: 'https://www.carbondesignsystem.com',
+      target: '_blank',
+    },
+    false: undefined,
+  }),
+  size: figma.enum('Size', {
+    Large: 'lg',
+    Small: 'sm',
+  }),
+  illustrationPosition: figma.enum('Illustration position', {
+    Top: 'top',
+    Right: 'right',
+    Left: 'left',
+    Bottom: 'bottom',
+  }),
+  illustration: figma.boolean('Illustration', {
+    true: './path/to/illustration.svg',
+    false: undefined,
+  }),
+  title: figma.string('Title text'),
+  subtitle: figma.string('Subtitle text'),
+};
+
+// Custom
 figma.connect(
   EmptyState,
   'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=420-2434&t=lbewdWdJ4JB5izcw-4',
   {
-    props: {
-      action: figma.boolean('Button', {
-        true: {
-          text: 'Button',
-          onClick: () => console.log("clicked empty state action button'),"),
-          renderIcon: (props) => <Add size={20} {...props} />,
-          iconDescription: 'Add icon',
-        },
-        false: undefined,
-      }),
-      link: figma.boolean('Link', {
-        true: {
-          text: 'Link',
-          href: 'https://www.carbondesignsystem.com',
-          target: '_blank',
-        },
-        false: undefined,
-      }),
-      size: figma.enum('Size', {
-        Large: 'lg',
-        Small: 'sm',
-      }),
-      illustrationPosition: figma.enum('Illustration position', {
-        Top: 'top',
-        Right: 'right',
-        Left: 'left',
-        Bottom: 'bottom',
-      }),
-      illustration: figma.boolean('Illustration', {
-        true: './path/to/illustration.svg',
-        false: undefined,
-      }),
-      title: figma.string('Title text'),
-      subtitle: figma.string('Subtitle text'),
-    },
-    example: (props) => (
+    props: sharedProps,
+    example: ({
+      title,
+      subtitle,
+      size,
+      illustrationPosition,
+      illustration,
+      action,
+      link,
+    }) => (
       <EmptyState
-        title={props.title}
-        subtitle={props.subtitle}
-        size={props.size}
-        illustrationPosition={props.illustrationPosition}
-        illustration={props.illustration}
-        action={props.action}
-        link={props.link}
+        title={title}
+        subtitle={subtitle}
+        size={size}
+        illustrationPosition={illustrationPosition}
+        illustration={illustration}
+        action={action}
+        link={link}
       ></EmptyState>
     ),
   }

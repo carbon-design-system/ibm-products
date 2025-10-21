aedc62af5dd56931528b4bb71f3eeee8b4f88edc
David Menendez <damenend@gmail.com>
2025-10-21 13:19:19 -0500

    fix: review changes

--- packages/ibm-products-web-components/src/components/options-tile/options-file.figma.ts
+++ packages/ibm-products-web-components/src/components/options-tile/options-file.figma.ts
@@ -12,25 +12,72 @@ figma.connect(
   'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=36228%3A14503',
   {
     props: {
-      body: figma.instance('Body slot'),
-      defaultOpen: figma.boolean('Expanded'),
+      title: figma.string('Heading text'),
+      open: figma.boolean('Expanded'),
       size: figma.enum('Size', {
         Large: 'lg',
         'Extra large': 'xl',
       }),
-      summary: figma.instance('Summary slot'),
-      titleText: figma.string('Title text'),
-      toggle: figma.instance('Toggle slot'),
+      toggle: figma.boolean('Toggle', {
+        true: '<cds-toggle id="my-toggle" size="sm" hideLabel></cds-toggle>',
+        false: undefined,
+      }),
+      children: figma.enum('Type', {
+        Static: undefined,
+        Expandable: figma.instance('Swap slot'),
+      }),
+      summaryProps: figma.boolean('Summary + Validation', {
+        true: figma.nestedProps('Summary + Validation message', {
+          locked: figma.enum('Type', {
+            Summary: figma.boolean('Locked'),
+            Warning: false,
+            Error: false,
+          }),
+          summary: figma.enum('Type', {
+            Summary: figma.string('Summary text'),
+            Warning: undefined,
+            Error: undefined,
+          }),
+          invalid: figma.enum('Type', {
+            Summary: undefined,
+            Warning: undefined,
+            Error: true,
+          }),
+          invalidText: figma.enum('Type', {
+            Summary: undefined,
+            Warning: undefined,
+            Error: figma.string('Error text'),
+          }),
+          warn: figma.enum('Type', {
+            Summary: undefined,
+            Warning: true,
+            Error: undefined,
+          }),
+          warnText: figma.enum('Type', {
+            Summary: undefined,
+            Warning: figma.string('Warning text'),
+            Error: undefined,
+          }),
+        }),
+        false: {
+          locked: undefined,
+          summary: undefined,
+          invalid: undefined,
+          invalidText: undefined,
+          warn: undefined,
+          warnText: undefined,
+        },
+      }),
     },
-    example: ({ body, defaultOpen, size, summary, titleText, toggle }) => html`
+    example: (props) => html`
       <c4p-options-tile
-        ?defaultOpen="${defaultOpen}"
-        size="${size}"
-        titleText="${titleText}"
+        ?defaultOpen="${props.open}"
+        size="${props.size}"
+        titleText="${props.title}"
       >
-        <div slot="summary">${summary}</div>
-        <div slot="toggle">${toggle}</div>
-        <div slot="body">${body}</div>
+        <div slot="summary">${props.summaryProps.summary}</div>
+        <div slot="toggle">${props.toggle}</div>
+        <div slot="body">${props.children}</div>
       </c4p-options-tile>
     `,
     imports: [

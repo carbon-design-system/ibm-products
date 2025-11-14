b81b772320c0fa902ce32ccc13c45e8a8d19036d
David Menendez <damenend@gmail.com>
2025-11-14 08:54:18 -0600

    fix: changes

--- packages/ibm-products/src/components/EditInPlace/EditInPlace.figma.tsx
+++ packages/ibm-products/src/components/EditInPlace/EditInPlace.figma.tsx
@@ -4,41 +4,52 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
 import React from 'react';
 import { EditInPlace } from './EditInPlace';
 import figma from '@figma/code-connect';
-
 figma.connect(
   EditInPlace,
-  'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=15713-9770&t=A1DwBD80AwWFp5NR-0',
+  'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=15713%3A9775',
   {
     props: {
-      cancelLabel: figma.string('Cancel label'),
-      editLabel: figma.string('Edit label'),
-      invalid: figma.boolean('Invalid'),
-      invalidText: figma.string('Invalid text'),
-      labelText: figma.string('Label text'),
-      placeholder: figma.string('Placeholder'),
-      saveLabel: figma.string('Save label'),
+      // These props were automatically mapped based on your linked code:
       size: figma.enum('Size', {
-        small: 'sm',
-        large: 'lg',
-        medium: 'md',
+        Small: 'sm',
       }),
-      value: figma.string('Value'),
+      invalid: figma.enum('State', {
+        Active: false,
+        Disabled: false,
+        Enabled: false,
+        Error: true,
+        Focus: false,
+        Hover: false,
+      }),
+      invalidText: figma.enum('State', {
+        Active: undefined,
+        Disabled: undefined,
+        Enabled: undefined,
+        Error: figma.textContent('Error message goes here'),
+        Focus: undefined,
+        Hover: undefined,
+      }),
+      value: figma.textContent('Input text'),
     },
     example: (props) => (
       <EditInPlace
-        cancelLabel={props.cancelLabel}
-        editLabel={props.editLabel}
-        invalid={props.invalid}
-        invalidText={props.invalidText}
-        labelText={props.labelText}
-        placeholder={props.placeholder}
-        saveLabel={props.saveLabel}
+        cancelLabel="Cancel"
+        editLabel="Edit"
+        id="Example-ID"
+        labelText="Example label text"
+        onCancel={() => console.log('Cancel clicked')}
+        onChange={(val) => {
+          console.log('Changed to: ' + val);
+        }}
+        onSave={() => console.log('Save clicked')}
+        saveLabel="Save"
         size={props.size}
         value={props.value}
+        invalid={props.invalid}
+        invalidText={props.invalidText}
       />
     ),
   }

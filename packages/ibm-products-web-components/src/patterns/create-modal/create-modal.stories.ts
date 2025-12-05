5048aa0af67b90eae559f84f659d384d44b06245
Afsal K <makafsal@gmail.com>
2025-12-05 16:13:45 +0530

    feat(create-modal): init create-modal example

---
+++ packages/ibm-products-web-components/src/patterns/create-modal/create-modal.stories.ts
@@ -0,0 +1,35 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2025, 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { html } from 'lit';
+import '../../../examples/create-modal/src/create-modal';
+export default {
+  title: 'Patterns/Step flows/CreateModal',
+};
+
+export const CreateModal = {
+  render: () => {
+    return html` <div>hmm</div>`;
+  },
+};
+
+// export const WithExtension = {
+//   render: () => {
+//     return html` <export-modal-with-extension> </export-modal-with-extension> `;
+//   },
+// };
+
+// export const WithPreformattedExtension = {
+//   render: () => {
+//     return html`
+//       <export-modal-preformatted-extension>
+//       </export-modal-preformatted-extension>
+//     `;
+//   },
+// };

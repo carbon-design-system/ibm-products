0cbc86e6a7d6e53a3b6d3cff153fca520f7c0306
David Menendez <damenend@gmail.com>
2026-01-14 15:40:55 -0600

    fix: init testing

--- packages/ibm-products-web-components/src/components/guide-banner/guide-banner-element.test.ts
+++ packages/ibm-products-web-components/src/components/guide-banner/guide-banner-element.test.ts
@@ -4,6 +4,7 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 import { describe, expect, it } from 'vitest';
 import { fixture, html } from '@open-wc/testing';
 import CDSGuideBannerElement from './guide-banner-element';

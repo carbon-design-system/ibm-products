283b8262eee76d97e7d244a75a5c263f1f712cf5
Sinta Augustine <sintaaugustine@gmail.com>
2026-01-22 16:40:21 +0530

    feat(pageheader): code connect integration for pageheader v2

--- packages/ibm-products/src/components/PageHeader/next/PageHeader.figma.tsx
+++ packages/ibm-products/src/components/PageHeader/next/PageHeader.figma.tsx
@@ -11,214 +11,208 @@ import {
   Button,
   Grid,
   Column,
-  Breadcrumb,
   BreadcrumbItem,
   Tag,
-  OperationalTag,
   TabList,
   Tab,
+  OverflowMenu,
+  OverflowMenuItem,
 } from '@carbon/react';
 import { Activity, Bee } from '@carbon/react/icons';
 
+// Breadcrumb bar
+figma.connect(
+  PageHeader.BreadcrumbBar,
+  'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=41243-6410',
+  {
+    props: {
+      icon: figma.boolean('Icon', {
+        true: () => <Bee size={16} />,
+        false: undefined,
+      }),
+      contentActions: figma.boolean('Buttons', {
+        true: (
+          <Button className="breadcrumb-bar-action-button" size="sm">
+            Button
+          </Button>
+        ),
+        false: undefined,
+      }),
+      pageActions: figma.boolean('Actions', {
+        true: (
+          <>
+            <Button
+              renderIcon={Activity}
+              iconDescription="Icon Description 1"
+              hasIconOnly
+              size="md"
+              kind="ghost"
+            />
+            <Button
+              renderIcon={Activity}
+              iconDescription="Icon Description 2"
+              hasIconOnly
+              size="md"
+              kind="ghost"
+            />
+            <Button
+              renderIcon={Activity}
+              iconDescription="Icon Description 3"
+              hasIconOnly
+              size="md"
+              kind="ghost"
+            />
+          </>
+        ),
+        false: undefined,
+      }),
+      border: figma.boolean('Bottom border'),
+    },
+    example: (props) => {
+      return (
+        <PageHeader.BreadcrumbBar
+          border={props.border}
+          renderIcon={props.icon}
+          pageActions={props.pageActions}
+          contentActions={props.contentActions}
+        >
+          <PageHeader.BreadcrumbOverflow
+            renderOverflowBreadcrumb={(hiddenItems) => (
+              <BreadcrumbItem data-floating-menu-container>
+                <OverflowMenu
+                  align="bottom"
+                  aria-label="Overflow menu in a breadcrumb"
+                >
+                  {hiddenItems.map((el, i) => (
+                    <OverflowMenuItem itemText={el.innerText} key={i} />
+                  ))}
+                </OverflowMenu>
+              </BreadcrumbItem>
+            )}
+          >
+            <BreadcrumbItem href="/#">Breadcrumb</BreadcrumbItem>
+            <BreadcrumbItem href="/#">Breadcrumb</BreadcrumbItem>
+            <BreadcrumbItem href="/#">Breadcrumb</BreadcrumbItem>
+            <BreadcrumbItem href="/#">Breadcrumb</BreadcrumbItem>
+          </PageHeader.BreadcrumbOverflow>
+        </PageHeader.BreadcrumbBar>
+      );
+    },
+    imports: [
+      "import { preview__PageHeader as PageHeader } from '@carbon/ibm-products';",
+    ],
+  }
+);
+
+// Tab bar
+figma.connect(
+  PageHeader.TabBar,
+  'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=41241-4083',
+  {
+    props: {
+      tabs: figma.boolean('Tabs', {
+        true: (
+          <TabList>
+            <Tab>Tab label</Tab>
+            <Tab>Tab label</Tab>
+          </TabList>
+        ),
+        false: undefined,
+      }),
+      tags: figma.boolean('Tags', {
+        true: (
+          <>
+            <Tag type="blue" id="example-tag-1" key="example-tag-1">
+              Tag
+            </Tag>
+            <Tag type="blue" id="example-tag-2" key="example-tag-2">
+              Tag
+            </Tag>
+          </>
+        ),
+        false: undefined,
+      }),
+      scroller: figma.boolean('Collapsable button', {
+        true: <PageHeader.ScrollButton />,
+        false: undefined,
+      }),
+    },
+    example: (props) => {
+      return (
+        <PageHeader.TabBar tags={props.tags} scroller={props.scroller}>
+          {props.tabs}
+        </PageHeader.TabBar>
+      );
+    },
+    imports: [
+      "import { preview__PageHeader as PageHeader } from '@carbon/ibm-products';",
+    ],
+  }
+);
+
 const sharedProps = {
-  titleText: figma.string('Title text'),
-  subtitle: figma.boolean('Subtitle', {
-    true: figma.string('Subtitle text'),
-    false: undefined,
+  title: figma.nestedProps('_Title', {
+    text: figma.string('Title text'),
+    icon: figma.boolean('Leading icon', {
+      true: () => <Bee size={32} />,
+      false: undefined,
+    }),
+    tags: figma.boolean('Slot', {
+      true: (
+        <>
+          <Tag className="tag" type="blue" size="lg">
+            Tag
+          </Tag>
+          <Tag className="tag" type="blue" size="lg">
+            Tag
+          </Tag>
+        </>
+      ),
+      false: undefined,
+    }),
   }),
-  summary: figma.boolean('Summary', {
-    true: figma.string('Summary text'),
-    false: undefined,
-  }),
-  titleIcon: figma.boolean('Title icon (image)', {
-    true: () => <Bee size={32} />,
-    false: undefined,
+  summary: figma.nestedProps('_Summary', {
+    text: figma.string('Summary text'),
   }),
 };
 
-const connectionURL =
+const pageHeaderConnectionURL =
   'https://www.figma.com/design/0F9dKH2abAd7gSfvnacfWf/-v11--IBM-Products-%E2%80%93-Carbon-Design-System?node-id=31080%3A23323';
 
-figma.connect(PageHeader, connectionURL, {
-  variant: { Variant: 'With actions' },
+figma.connect(PageHeader, pageHeaderConnectionURL, {
+  variant: { Style: 'Action' },
   props: {
     ...sharedProps,
     breadcrumbBar: figma.boolean('Breadcrumb bar', {
-      true: figma.boolean('Toolbar buttons', {
+      true: figma.children('_Breadcrumb bar'),
+      false: undefined,
+    }),
+    header: figma.nestedProps('_Content', {
+      actions: figma.boolean('Actions', {
         true: (
-          <PageHeader.BreadcrumbBar
-            border={true}
-            renderIcon={() => <Bee size={16} />}
-            pageActions={
-              <>
-                <Button
-                  renderIcon={Activity}
-                  iconDescription="Icon Description 1"
-                  hasIconOnly
-                  size="md"
-                  kind="ghost"
-                />
-                <Button
-                  renderIcon={Activity}
-                  iconDescription="Icon Description 2"
-                  hasIconOnly
-                  size="md"
-                  kind="ghost"
-                />
-                <Button
-                  renderIcon={Activity}
-                  iconDescription="Icon Description 3"
-                  hasIconOnly
-                  size="md"
-                  kind="ghost"
-                />
-              </>
-            }
-            contentActions={
-              <Button className="breadcrumb-bar-action-button" size="sm">
-                Primary action
-              </Button>
-            }
-          >
-            <Breadcrumb>
-              <BreadcrumbItem href="#">Breadcrumb</BreadcrumbItem>
-              <BreadcrumbItem href="#">Breadcrumb</BreadcrumbItem>
-              <BreadcrumbItem href="#">Breadcrumb</BreadcrumbItem>
-            </Breadcrumb>
-          </PageHeader.BreadcrumbBar>
-        ),
-        false: (
-          <PageHeader.BreadcrumbBar
-            border={true}
-            renderIcon={() => <Bee size={16} />}
-            pageActions={
-              <>
-                <Button
-                  renderIcon={Activity}
-                  iconDescription="Icon Description 1"
-                  hasIconOnly
-                  size="md"
-                  kind="ghost"
-                />
-                <Button
-                  renderIcon={Activity}
-                  iconDescription="Icon Description 2"
-                  hasIconOnly
-                  size="md"
-                  kind="ghost"
-                />
-                <Button
-                  renderIcon={Activity}
-                  iconDescription="Icon Description 3"
-                  hasIconOnly
-                  size="md"
-                  kind="ghost"
-                />
-              </>
-            }
-          >
-            <Breadcrumb>
-              <BreadcrumbItem href="#">Breadcrumb</BreadcrumbItem>
-              <BreadcrumbItem href="#">Breadcrumb</BreadcrumbItem>
-              <BreadcrumbItem href="#">Breadcrumb</BreadcrumbItem>
-            </Breadcrumb>
-          </PageHeader.BreadcrumbBar>
-        ),
-      }),
-      false: undefined,
-    }),
-    pageActions: figma.boolean('Primary button', {
-      true: (
-        <PageHeader.ContentPageActions
-          menuButtonLabel="Actions"
-          actions={[
-            {
-              // props used for both collapse menu item and non-collapsed action form
-              id: 'primary-action',
-              onClick: () => console.log('Primary action'),
-              body: (
-                // component to render when non-collapsed
-                <Button kind="primary" size="md">
-                  Primary action
-                </Button>
-              ),
-              // props to pass to the corresponding collapsed menu item
-              menuItem: {
-                label: 'Primary action',
+          <PageHeader.ContentPageActions
+            actions={[
+              {
+                id: 'action1',
+                onClick: () => console.log('Action 1'),
+                body: (
+                  <Button size="md" kind="primary">
+                    Button
+                  </Button>
+                ),
               },
-            },
-          ]}
-        ></PageHeader.ContentPageActions>
-      ),
-      false: undefined,
-    }),
-    tagTitle: figma.boolean('Tag (title)', {
-      true: (
-        <Tag className="tag" type="blue" size="lg">
-          Tag
-        </Tag>
-      ),
-      false: undefined,
+            ]}
+          ></PageHeader.ContentPageActions>
+        ),
+        false: undefined,
+      }),
+      subtitle: figma.boolean('Subtitle', {
+        true: figma.textContent('Subtitle'),
+        false: undefined,
+      }),
     }),
     tabBar: figma.boolean('Tab bar', {
-      true: figma.boolean('Tag (tab)', {
-        true: (
-          <PageHeader.TabBar
-            tags={
-              <PageHeader.TagOverflow
-                renderOverflowTag={(
-                  hiddenItems,
-                  handleOverflowClick,
-                  openPopover
-                ) => (
-                  <OperationalTag
-                    onClick={handleOverflowClick}
-                    aria-expanded={openPopover}
-                    text={`+${hiddenItems.length}`}
-                  />
-                )}
-                renderPopoverContent={(hiddenItems) => {
-                  return hiddenItems.map((i, index) => {
-                    const foundJSXTag = [
-                      <Tag type="blue" id="example-tag-1" key="example-tag-1">
-                        Tag 1
-                      </Tag>,
-                      <Tag type="purple" id="example-tag-2" key="example-tag-2">
-                        Tag 2
-                      </Tag>,
-                    ].find((c) => c.props.id === i.id);
-                    return React.cloneElement(foundJSXTag, {
-                      id: `cloned-tag-node-id-${index}`,
-                      key: `cloned-tag-key-${index}`,
-                    });
-                  });
-                }}
-              >
-                <Tag type="blue" id="example-tag-1" key="example-tag-1">
-                  Tag 1
-                </Tag>
-                ,
-                <Tag type="purple" id="example-tag-2" key="example-tag-2">
-                  Tag 2
-                </Tag>
-              </PageHeader.TagOverflow>
-            }
-          >
-            <TabList>
-              <Tab>Tab label</Tab>
-              <Tab>Tab label</Tab>
-            </TabList>
-          </PageHeader.TabBar>
-        ),
-        false: (
-          <PageHeader.TabBar>
-            <TabList>
-              <Tab>Tab label</Tab>
-              <Tab>Tab label</Tab>
-            </TabList>
-          </PageHeader.TabBar>
-        ),
-      }),
+      true: figma.children('_Tab bar'),
       false: undefined,
     }),
   },
@@ -227,13 +221,13 @@ figma.connect(PageHeader, connectionURL, {
       <PageHeader.Root>
         {props.breadcrumbBar}
         <PageHeader.Content
-          title={props.titleText}
-          renderIcon={props.titleIcon}
-          pageActions={props.pageActions}
-          contextualActions={props.tagTitle}
+          title={props.title.text}
+          renderIcon={props.title.icon}
+          pageActions={props.header.actions}
+          contextualActions={props.title.tags}
         >
-          <PageHeader.ContentText subtitle={props.subtitle}>
-            {props.summary}
+          <PageHeader.ContentText subtitle={props.header.subtitle}>
+            {props.summary.text}
           </PageHeader.ContentText>
         </PageHeader.Content>
         {props.tabBar}
@@ -245,24 +239,19 @@ figma.connect(PageHeader, connectionURL, {
   ],
 });
 
-figma.connect(PageHeader, connectionURL, {
-  variant: { Variant: 'With image' },
+figma.connect(PageHeader, pageHeaderConnectionURL, {
+  variant: { Style: 'Image' },
   props: {
     ...sharedProps,
     breadcrumbBar: figma.boolean('Breadcrumb bar', {
-      true: (
-        <PageHeader.BreadcrumbBar
-          border={false}
-          renderIcon={() => <Bee size={16} />}
-        >
-          <Breadcrumb>
-            <BreadcrumbItem href="#">Breadcrumb</BreadcrumbItem>
-            <BreadcrumbItem href="#">Breadcrumb</BreadcrumbItem>
-            <BreadcrumbItem href="#">Breadcrumb</BreadcrumbItem>
-          </Breadcrumb>
-        </PageHeader.BreadcrumbBar>
-      ),
+      true: figma.children('_Breadcrumb bar'),
       false: undefined,
+    }),
+    subtitle: figma.nestedProps('_Content expressive', {
+      text: figma.boolean('Subtitle', {
+        true: figma.textContent('Subtitle'),
+        false: undefined,
+      }),
     }),
   },
   example: (props) => {
@@ -272,11 +261,12 @@ figma.connect(PageHeader, connectionURL, {
           <Column lg={8} md={4} sm={4}>
             {props.breadcrumbBar}
             <PageHeader.Content
-              title={props.titleText}
-              renderIcon={props.titleIcon}
+              title={props.title.text}
+              renderIcon={props.title.icon}
+              contextualActions={props.title.tags}
             >
-              <PageHeader.ContentText subtitle={props.subtitle}>
-                {props.summary}
+              <PageHeader.ContentText subtitle={props.subtitle.text}>
+                {props.summary.text}
               </PageHeader.ContentText>
             </PageHeader.Content>
           </Column>

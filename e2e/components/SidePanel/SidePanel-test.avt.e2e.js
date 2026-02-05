09c6b8b05e66473901d7fcc235a26de12807efcd
Afsal K <makafsal@gmail.com>
2026-02-05 16:21:56 +0530

    test(SidePanel): unskip 1 test

--- e2e/components/SidePanel/SidePanel-test.avt.e2e.js
+++ e2e/components/SidePanel/SidePanel-test.avt.e2e.js
@@ -11,7 +11,7 @@ import { expect, test } from '@playwright/test';
 import { visitStory } from '../../test-utils/storybook';
 
 test.describe('SidePanel @avt', () => {
-  test.skip('@avt-default-state', async ({ page }) => {
+  test('@avt-default-state', async ({ page }) => {
     await visitStory(page, {
       component: 'SidePanel',
       id: 'components-sidepanel--slide-over',

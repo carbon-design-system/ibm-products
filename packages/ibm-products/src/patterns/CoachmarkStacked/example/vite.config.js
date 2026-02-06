6ac5639b179082d23bedcdc71f49227f24086f43
Anamika T S <anamika.t.s@ibm.com>
2026-02-06 14:29:57 +0530

    fix: build failure

--- packages/ibm-products/src/patterns/CoachmarkStacked/example/vite.config.js
+++ packages/ibm-products/src/patterns/CoachmarkStacked/example/vite.config.js
@@ -12,7 +12,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'classic',
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {

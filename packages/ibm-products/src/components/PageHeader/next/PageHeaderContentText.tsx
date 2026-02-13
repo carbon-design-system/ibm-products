60bcc4f08be8a8dbe0f7943427668320b665d059
Afsal K <makafsal@gmail.com>
2026-02-13 19:30:52 +0530

    refactor(PageHeader): separate child components from PageHeader.tsx file
    refactor(useFocus): refactor repeated useEffect code
    
    chore: update license date
    
    chore: remove unwanted change
    
    chore: remove unwanted change
    
    chore: remove unwanted change 3
    
    chore: remove unwanted change 4
    
    chore: remove unwanted change 4
    
    refactor(useFocus): refactor repeated useEffect code
    
    chore: update license date
    
    chore: remove unwanted change
    
    chore: remove unwanted change
    
    chore: remove unwanted change 5

---
+++ packages/ibm-products/src/components/PageHeader/next/PageHeaderContentText.tsx
@@ -0,0 +1,73 @@
+/**
+ * Copyright IBM Corp. 2026
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { unstable_Text as Text } from '@carbon/react';
+import { blockClass } from '../PageHeaderUtils';
+
+/**
+ * ----------------
+ * PageHeaderContentText
+ * ----------------
+ */
+export interface PageHeaderContentTextProps {
+  /**
+   * Provide child elements to be rendered inside PageHeaderContentText.
+   */
+  children?: React.ReactNode;
+  /**
+   * Specify an optional className to be added to your PageHeaderContentText
+   */
+  className?: string;
+  /**
+   * The PageHeaderContent's subtitle
+   */
+  subtitle?: string;
+}
+
+export const PageHeaderContentText = ({
+  className,
+  children,
+  subtitle,
+  ...other
+}: PageHeaderContentTextProps) => {
+  const classNames = classnames(
+    {
+      [`${blockClass}__content__body`]: true,
+    },
+    className
+  );
+
+  return (
+    <div className={classNames} {...other}>
+      {subtitle && (
+        <Text as="h3" className={`${blockClass}__content__subtitle`}>
+          {subtitle}
+        </Text>
+      )}
+      {children}
+    </div>
+  );
+};
+
+PageHeaderContentText.displayName = 'PageHeaderContentText';
+
+PageHeaderContentText.propTypes = {
+  /**
+   * Provide child elements to be rendered inside PageHeaderContentText.
+   */
+  children: PropTypes.node,
+  /**
+   * Specify an optional className to be added to your PageHeaderContentText
+   */
+  className: PropTypes.string,
+  /**
+   * The PageHeaderContent's subtitle
+   */
+  subtitle: PropTypes.string,
+};

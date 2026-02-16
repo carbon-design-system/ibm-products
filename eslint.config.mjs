7a072b8bb79ad6609d56a9dc5ffb9809141e4a71
Amal K Joy <153802538+amal-k-joy@users.noreply.github.com>
2026-02-16 11:25:34 +0530

    Update eslint.config.mjs
    Co-authored-by: Sinta Augustine <sintaaugustine@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -84,10 +84,8 @@ export default tsEslint.config(
       'packages/ibm-products/scripts/generate/templates/**/*.js*',
       'scripts/example-gallery-builder/update-example/**/*.js*',
       // Pattern example files (StackBlitz examples)
-      'packages/ibm-products/src/patterns/*/example/main.tsx',
-      'packages/ibm-products/src/patterns/*/example/App.tsx',
-      'packages/ibm-products/src/patterns/*/example/index.html',
-      'packages/ibm-products/src/patterns/*/example/preview-components/**',
+      "packages/ibm-products/src/patterns/*/example/**",
+      "!packages/ibm-products/src/patterns/*/example/components/**",
     ],
   },
   {

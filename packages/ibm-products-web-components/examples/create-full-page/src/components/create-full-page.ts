c29f824e5b7ea061cab6cb6b229dbb441baf6900
Anamika T S <anamika.t.s@ibm.com>
2026-05-08 15:06:20 +0530

    fix: review and checks failure

--- packages/ibm-products-web-components/examples/create-full-page/src/components/create-full-page.ts
+++ packages/ibm-products-web-components/examples/create-full-page/src/components/create-full-page.ts
@@ -11,7 +11,6 @@ import { carbonElement as customElement } from '@carbon/web-components/es/global
 import '@carbon/web-components/es/components/button/index.js';
 import '@carbon/web-components/es/components/inline-loading/index.js';
 import '@carbon/web-components/es/components/modal/index.js';
-import '@carbon/ibm-products-web-components/es/components/page-header/index.js';
 import type { Breadcrumb } from './simple-header';
 import type { StepData } from './create-influencer';
 import type { ActionButton } from '@carbon/ibm-products-web-components/es/components/action-set/index.js';
--- packages/ibm-products-web-components/examples/create-full-page/src/components/index.ts
+++ packages/ibm-products-web-components/examples/create-full-page/src/components/index.ts
@@ -4,15 +4,14 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
 export { CreateFullPage } from './create-full-page';
 export { CreateFullPageStep } from './create-full-page-step';
 export { CreateInfluencer } from './create-influencer';
-export { BreadcrumbWithOverflow } from './breadcrumb-with-overflow';
 
 // Import all components to register them
+// Import page-header components once to avoid duplicate registrations
+import '@carbon/ibm-products-web-components/es/components/page-header/index.js';
 import './create-full-page';
 import './create-full-page-step';
 import './create-influencer';
-import './breadcrumb-with-overflow';
 

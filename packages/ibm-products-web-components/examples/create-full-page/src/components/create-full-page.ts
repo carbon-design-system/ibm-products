4e4ced6b26e765e0bf2f2ebd3d106e68c3025685
Afsal K <makafsal@gmail.com>
2026-05-20 11:11:27 +0530

    chore: change import for production

--- packages/ibm-products-web-components/examples/create-full-page/src/components/create-full-page.ts
+++ packages/ibm-products-web-components/examples/create-full-page/src/components/create-full-page.ts
@@ -12,10 +12,6 @@ import '@carbon/web-components/es/components/button/index.js';
 import '@carbon/web-components/es/components/inline-loading/index.js';
 import '@carbon/web-components/es/components/modal/index.js';
 import '@carbon/ibm-products-web-components/es/components/page-header/index.js';
-import '@carbon/ibm-products-web-components/es/components/page-header/page-header-breadcrumb.ts'
-import '@carbon/ibm-products-web-components/es/components/page-header/page-header-breadcrumbs-set/page-header-breadcrumbs-set.ts'
-import '@carbon/ibm-products-web-components/es/components/page-header/page-header-content.ts'
-import '@carbon/ibm-products-web-components/es/components/page-header/page-header-content-text.ts'
 import type { StepData } from './create-influencer';
 import type { ActionButton } from '@carbon/ibm-products-web-components/es/components/action-set/index.js';
 import '@carbon/ibm-products-web-components/es/components/action-set/index.js';

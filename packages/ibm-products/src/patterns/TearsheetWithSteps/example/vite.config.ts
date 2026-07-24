1f1199397e661763c0394e7c6738c6e7ad3f3387
Amal K Joy <153802538+amal-k-joy@users.noreply.github.com>
2026-07-24 11:12:38 +0530

    fix: add example for tearsheet with steps (#9635)
    * fix: add example for tearsheet with steps
    
    * fix: define matchMedia and provider
    
    * fix: use canvas additionalActions
    
    * fix: fix the examples errors
    
    * fix: corrected path and styles

---
+++ packages/ibm-products/src/patterns/TearsheetWithSteps/example/vite.config.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright IBM Corp. 2025, 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+});

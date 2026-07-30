35e03e013322e2d87ec8114f9d80c3312f4595d8
Rucha Sohoni <rucha.sohoni@gmail.com>
2026-07-30 12:19:50 +0100

    fix(expressivecard): use reactnode as type for pictogram prop

--- packages/ibm-products/src/components/ExpressiveCard/ExpressiveCard.tsx
+++ packages/ibm-products/src/components/ExpressiveCard/ExpressiveCard.tsx
@@ -7,7 +7,6 @@
 
 import React, { PropsWithChildren, ReactNode, forwardRef } from 'react';
 
-import { CarbonIconType } from '@carbon/icons-react/lib/CarbonIcon';
 import { Card } from '../Card';
 import PropTypes from 'prop-types';
 import { getDevtoolsProps } from '../../global/js/utils/devtools';
@@ -63,7 +62,7 @@ export interface ExpressiveCardProps extends PropsWithChildren {
   /**
    * Provides the icon that's displayed at the top of the card
    */
-  pictogram?: CarbonIconType;
+  pictogram?: ReactNode;
   /**
    * Optionally specify an href for your Button to become an <a> element
    */

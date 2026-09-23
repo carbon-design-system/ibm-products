6fe9dfab90faeadd76035fc12d0678f097278482
Rucha Sohoni <145022372+ruchas31@users.noreply.github.com>
2026-09-23 10:47:44 +0000

    chore: deprecate saving prebuilt pattern (#9888)
    * chore: deprecate saving prebuilt pattern
    
    * fix: update copyright dates
    
    * fix(saving): fix failing test

--- packages/ibm-products/src/components/Saving/Saving.test.jsx
+++ packages/ibm-products/src/components/Saving/Saving.test.jsx
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2021, 2021
+ * Copyright IBM Corp. 2021, 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -28,6 +28,9 @@ const defaultProps = {
 };
 
 describe(componentName, () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'warn').mockImplementation(() => {});
+  });
   it('should render', async () => {
     render(<Saving {...defaultProps} />);
   });
